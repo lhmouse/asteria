74fc0b905a65dbf331ec3074664dc5b3aae0c4bf
Liu Hao <lh_mouse@126.com>
2018-03-23 17:31:37 +0800

    Hello world!

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// This file is part of asteria.
+// Copyleft 2018, LH_Mouse. All wrongs reserved.
+
+#include <iostream>
+
+int main(){
+	std::cerr <<"hello world!" <<std::endl;
+}
---
+++ src/parser.cpp
@@ -0,0 +1,3 @@
+// This file is part of asteria.
+// Copyleft 2018, LH_Mouse. All wrongs reserved.
+
