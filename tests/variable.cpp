cc43cfbc6d77f6c74ec0fad1edd90e2306d3ab60
Liu Hao <lh_mouse@126.com>
2018-03-25 22:08:37 +0800

    Fix test errors with GCC 4.8.

--- tests/variable.cpp
+++ tests/variable.cpp
@@ -46,19 +46,19 @@ int main(){
 	ASTERIA_TEST_CHECK(var->get_type() == Variable::type_opaque);
 	ASTERIA_TEST_CHECK(var->get<Opaque>() == opaque);
 
-	Array array = {
+	Array array({
 		std::make_shared<Variable>(true, true),
 		std::make_shared<Variable>(std::string("world"), true),
-	};
+	});
 	var->set(std::move(array));
 	ASTERIA_TEST_CHECK(var->get_type() == Variable::type_array);
 	ASTERIA_TEST_CHECK(var->get<Array>().at(0)->get<Boolean>() == true);
 	ASTERIA_TEST_CHECK(var->get<Array>().at(1)->get<String>() == "world");
 
-	Object object = {
+	Object object({
 		std::make_pair("one", std::make_shared<Variable>(true, true)),
 		std::make_pair("two", std::make_shared<Variable>(std::string("world"), true)),
-	};
+	});
 	var->set(std::move(object));
 	ASTERIA_TEST_CHECK(var->get_type() == Variable::type_object);
 	ASTERIA_TEST_CHECK(var->get<Object>().at("one")->get<Boolean>() == true);
@@ -71,9 +71,9 @@ int main(){
 	};
 	var->set(std::move(function));
 	ASTERIA_TEST_CHECK(var->get_type() == Variable::type_function);
-	Array parameters = {
+	Array parameters({
 		std::make_shared<Variable>(std::int64_t(12), true),
 		std::make_shared<Variable>(std::int64_t(15), true),
-	};
+	});
 	ASTERIA_TEST_CHECK(var->get<Function>()(std::move(parameters))->get<Integer>() == 180);
 }
