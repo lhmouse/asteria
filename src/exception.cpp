9f0832d3b704b559a120a00351a92fbcbadf955c
Liu Hao <lh_mouse@126.com>
2018-03-27 19:45:39 +0800

    Make `Exception` carry a pointer to `Variable` directly.

--- src/exception.cpp
+++ src/exception.cpp
@@ -10,22 +10,4 @@ Exception::~Exception(){
 	//
 }
 
-const char *Exception::what() const noexcept {
-	return "Asteria::Exception";
-}
-
-Reference Exception::get_reference() const {
-	if(!m_reference_opt){
-		Reference::Direct_reference null_ref = { nullptr };
-		return std::move(null_ref);
-	}
-	return *m_reference_opt;
-}
-void Exception::set_reference(Reference reference){
-	m_reference_opt = std::make_shared<Reference>(std::move(reference));
-}
-void Exception::clear_reference() noexcept {
-	m_reference_opt = nullptr;
-}
-
 }
--- src/exception.hpp
+++ src/exception.hpp
@@ -4,28 +4,32 @@
 #ifndef ASTERIA_EXCEPTION_HPP_
 #define ASTERIA_EXCEPTION_HPP_
 
-#include <exception>
 #include "fwd.hpp"
-#include "reference.hpp"
+#include <exception>
 
 namespace Asteria {
 
 class Exception : public std::exception {
 private:
-	std::shared_ptr<Reference> m_reference_opt;
+	std::shared_ptr<Variable> m_variable_opt;
 
 public:
-	explicit Exception(std::shared_ptr<Reference> reference_opt)
-		: m_reference_opt(std::move(reference_opt))
+	explicit Exception(std::shared_ptr<Variable> variable_opt)
+		: m_variable_opt(std::move(variable_opt))
 	{ }
 	~Exception() override;
 
 public:
-	const char *what() const noexcept override;
+	const char *what() const noexcept override {
+		return "Asteria::Exception";
+	}
 
-	Reference get_reference() const;
-	void set_reference(Reference reference);
-	void clear_reference() noexcept;
+	const std::shared_ptr<Variable> &get_variable_opt() const noexcept {
+		return m_variable_opt;
+	}
+	void set_variable(std::shared_ptr<Variable> variable_opt){
+		m_variable_opt = std::move(variable_opt);
+	}
 };
 
 }
