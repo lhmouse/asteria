e6fddd852f827ccb5b4f1c85822ae53c4f253929
Liu Hao <lh_mouse@126.com>
2018-03-27 16:29:39 +0800

    Update `Expression`.

--- src/expression.hpp
+++ src/expression.hpp
@@ -54,22 +54,77 @@ public:
 	struct Trailer;
 
 	enum Type : unsigned {
-		type_prefix_expression                  = 0,
-		type_id_expression_with_trailer_opt     = 1,
-		type_lambda_expression_with_trailer_opt = 2,
-		type_nested_expression_with_trailer_opt = 3,
+		type_prefix_operator                     = 0,
+		type_id_expression_with_trailer_opt      = 1,
+		type_lambda_expression_with_trailer_opt  = 2,
+		type_nested_expression_with_trailer_opt  = 3,
 	};
-
-	using Prefix_expression                  = std::tuple<Operator, Value_ptr<Expression>>;
-	using Id_expression_with_trailer_opt     = std::tuple<boost::variant<std::string, Value_ptr<Initializer>>, Value_ptr<Trailer>>;
-	using Lambda_expression_with_trailer_opt = std::tuple<boost::container::deque<std::string>, Value_ptr_deque<Statement>, Value_ptr<Trailer>>;
-	using Nested_expression_with_trailer_opt = std::tuple<Value_ptr<Expression>, Value_ptr<Trailer>>;
-
-	using Types = Type_tuple< Prefix_expression                   // 0
+	struct Prefix_operator {
+		Operator prefix_operator;
+		Value_ptr<Expression> next;
+	};
+	struct Id_expression_with_trailer_opt {
+		boost::variant<std::string, Value_ptr<Initializer>> id_or_initializer;
+		Value_ptr<Trailer> trailer_opt;
+	};
+	struct Lambda_expression_with_trailer_opt {
+		boost::container::vector<std::string> parameter_list;
+		Value_ptr_vector<Statement> function_body;
+		Value_ptr<Trailer> trailer_opt;
+	};
+	struct Nested_expression_with_trailer_opt {
+		Value_ptr<Expression> nested_expression;
+		Value_ptr<Trailer> trailer_opt;
+	};
+	using Types = Type_tuple< Prefix_operator                     // 0
 	                        , Id_expression_with_trailer_opt      // 1
 	                        , Lambda_expression_with_trailer_opt  // 2
 	                        , Nested_expression_with_trailer_opt  // 3
 		>;
+
+	enum Trailer_type : unsigned {
+		trailer_type_infix_operator     = 0,
+		trailer_type_ternary_operator   = 1,
+		trailer_type_postfix_operator   = 2,
+		trailer_type_function_call      = 3,
+		trailer_type_subscripting       = 4,
+		trailer_type_member_access      = 5,
+	};
+	struct Infix_operator {
+		Operator infix_operator;
+		Value_ptr<Expression> next;
+	};
+	struct Ternary_operator {
+		Value_ptr<Expression> true_branch_opt;
+		Value_ptr<Expression> false_branch;
+	};
+	struct Postfix_operator {
+		Operator postfix_operator;
+		Value_ptr<Trailer> next_trailer_opt;
+	};
+	struct Function_call {
+		Value_ptr_vector<Expression> argument_list;
+		Value_ptr<Trailer> next_trailer_opt;
+	};
+	struct Subscripting {
+		Value_ptr<Expression> subscript;
+		Value_ptr<Trailer> next_trailer_opt;
+	};
+	struct Member_access {
+		std::string key;
+		Value_ptr<Trailer> next_trailer_opt;
+	};
+	using Trailer_types = Type_tuple< Infix_operator     // 0
+	                                , Ternary_operator   // 1
+	                                , Postfix_operator   // 2
+	                                , Function_call      // 3
+	                                , Subscripting       // 4
+	                                , Member_access      // 5
+		>;
+
+	struct Trailer : Trailer_types::rebound_variant {
+		using Trailer_types::rebound_variant::variant;
+	};
 
 private:
 	Types::rebound_variant m_value;
