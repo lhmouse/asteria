70005ae474cd39988b576cd1ad5cdd25f3df21b5
Liu Hao <lh_mouse@126.com>
2018-03-27 19:50:22 +0800

    Add 'tests/exception.cpp'.

---
+++ tests/exception.cpp
@@ -0,0 +1,23 @@
+// This file is part of asteria.
+// Copyleft 2018, LH_Mouse. All wrongs reserved.
+
+#include "test_init.hpp"
+#include "../src/exception.hpp"
+#include "../src/variable.hpp"
+
+using namespace Asteria;
+
+int main(){
+	auto var = make_value<Variable>(Integer(42));
+	ASTERIA_TEST_CHECK(var->get_type() == Variable::type_integer);
+	ASTERIA_TEST_CHECK(var->get<Integer>() == 42);
+	try {
+		throw Exception(var.share());
+	} catch(Exception &e){
+		const auto ptr = e.get_variable_opt();
+		ASTERIA_TEST_CHECK(ptr);
+		ptr->set(String("hello"));
+	}
+	ASTERIA_TEST_CHECK(var->get_type() == Variable::type_string);
+	ASTERIA_TEST_CHECK(var->get<String>() == "hello");
+}
