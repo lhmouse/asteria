c25385d5842012178fe7109238cfc31b6a411cb4
Liu Hao <lh_mouse@126.com>
2018-03-31 18:01:38 +0800

    Update 'tests/run_one.sh'.

--- tests/run_one.sh
+++ tests/run_one.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ "$#" -lt 1 ]]; then
-	printf '\e[31;1mNo source file provided.  FAILED\e[0m\n' >&2
+	printf '\e[31;1mFAILED   No source file provided.\e[0m\n' >&2
 	exit 2;
 fi
 
 _file="$(readlink -e "$1")"
 if [[ -z "${_file}" ]]; then
-	printf '\e[31;1mSource file '"'$1'"' not found.  FAILED\e[0m\n' >&2
+	printf '\e[31;1mFAILED   Source file '"'$1'"' not found.\e[0m\n' >&2
 	exit 3;
 fi
 shift
@@ -20,9 +20,9 @@ _libs="-lasteria"
 _output="test.out~"
 _errlog="stderr.log~"
 
-printf '\e[33;1mRUNNING\e[0m  %s\n' "${_file}"
+printf '\e[33;1mRUNNING  %s\e[0m\n' "${_file}"
 if ! ("${_cxx}" ${_cxxflags} ${_ldflags} "${_file}" ${_libs} -o "${_output}" $* && LD_LIBRARY_PATH="${_runpath}" "./${_output}") >&2; then
-	printf '\e[31;1mFAILED\e[0m   %s\n' "${_file}"
+	printf '\e[31;1mFAILED   %s\e[0m\n' "${_file}"
 	exit 1
 fi
-printf '\e[32;1mPASSED\e[0m   %s\n' "${_file}"
+printf '\e[32;1mPASSED   %s\e[0m\n' "${_file}"
