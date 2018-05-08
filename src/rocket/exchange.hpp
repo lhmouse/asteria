fc182fc1ef8ed91ed9919c4e4f7d047e9d23959f
Liu Hao <lh_mouse@126.com>
2018-05-08 17:54:26 +0800

    Remove useless `inline` specifiers.

--- src/rocket/exchange.hpp
+++ src/rocket/exchange.hpp
@@ -12,19 +12,19 @@ namespace rocket {
 using ::std::decay;
 
 template<typename elementT>
-inline typename decay<elementT>::type exchange(elementT &element_old, const elementT &element_new){
+typename decay<elementT>::type exchange(elementT &element_old, const elementT &element_new){
 	auto old = ::std::move(element_old);
 	element_old = element_new;
 	return old;
 }
 template<typename elementT>
-inline typename decay<elementT>::type exchange(elementT &element_old, elementT &&element_new){
+typename decay<elementT>::type exchange(elementT &element_old, elementT &&element_new){
 	auto old = ::std::move(element_old);
 	element_old = ::std::move(element_new);
 	return old;
 }
 template<typename elementT, typename valueT>
-inline typename decay<elementT>::type exchange(elementT &element_old, valueT &&value_new){
+typename decay<elementT>::type exchange(elementT &element_old, valueT &&value_new){
 	auto old = ::std::move(element_old);
 	element_old = std::forward<valueT>(value_new);
 	return old;
