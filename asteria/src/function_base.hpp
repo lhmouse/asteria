6d9891eda39e73241471b275541453ada7edfd4e
Liu Hao <lh_mouse@126.com>
2018-05-18 22:53:54 +0800

    Make `Function_base` derive from `Opaque_base`.

--- asteria/src/function_base.hpp
+++ asteria/src/function_base.hpp
@@ -4,17 +4,14 @@
 #ifndef ASTERIA_FUNCTION_BASE_HPP_
 #define ASTERIA_FUNCTION_BASE_HPP_
 
-#include "fwd.hpp"
+#include "opaque_base.hpp"
 
 namespace Asteria {
 
-class Function_base {
-private:
-	const std::string m_description;
-
+class Function_base : public Opaque_base {
 public:
 	explicit Function_base(std::string description)
-		: m_description(std::move(description))
+		: Opaque_base(std::move(description))
 	{ }
 	virtual ~Function_base();
 
@@ -22,10 +19,6 @@ public:
 	Function_base & operator=(const Function_base &) = delete;
 
 public:
-	const std::string & describe() const noexcept {
-		return m_description;
-	}
-
 	virtual void invoke(Xptr<Reference> &result_out, Sparg<Recycler> recycler, Xptr<Reference> &&this_opt, Xptr_vector<Reference> &&arguments_opt) const = 0;
 };
 
