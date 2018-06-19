c4e8443847539009a46ac1f6e439f41a95768292
Liu Hao <lh_mouse@126.com>
2018-06-19 19:46:17 +0800

    Cleanup.

--- asteria/src/rocket/throw.cpp
+++ asteria/src/rocket/throw.cpp
@@ -2,7 +2,6 @@
 // Copyleft 2018, LH_Mouse. All wrongs reserved.
 
 #include "throw.hpp"
-#include <array>
 #include <stdexcept>
 #include <cstdarg>
 #include <cstdio>
@@ -13,24 +12,24 @@ namespace rocket {
 	do {	\
 	  ::std::va_list ap_;	\
 	  va_start(ap_, fmt_);	\
-	  ::std::vsnprintf((buf_).data(), (buf_).size(), (fmt_), ap_);	\
+	  ::std::vsnprintf((buf_), sizeof(buf_), (fmt_), ap_);	\
 	  va_end(ap_);	\
 	} while(false)
 
 void throw_invalid_argument(const char *fmt, ...){
-	::std::array<char, 1024> str;
+	char str[1024];
 	VSNPRINTF(str, fmt);
-	throw ::std::invalid_argument(str.data());
+	throw ::std::invalid_argument(str);
 }
 void throw_out_of_range(const char *fmt, ...){
-	::std::array<char, 1024> str;
+	char str[1024];
 	VSNPRINTF(str, fmt);
-	throw ::std::out_of_range(str.data());
+	throw ::std::out_of_range(str);
 }
 void throw_length_error(const char *fmt, ...){
-	::std::array<char, 1024> str;
+	char str[1024];
 	VSNPRINTF(str, fmt);
-	throw ::std::length_error(str.data());
+	throw ::std::length_error(str);
 }
 
 void rethrow_current_exception(){
