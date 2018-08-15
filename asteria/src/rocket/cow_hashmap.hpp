3dec8792c8c36840fb9d3246f1752f0cd2837f08
Liu Hao <lh_mouse@126.com>
2018-08-15 20:06:16 +0800

    Optimize `rocket::cow_hashmap` for i686.

--- asteria/src/rocket/cow_hashmap.hpp
+++ asteria/src/rocket/cow_hashmap.hpp
@@ -181,12 +181,12 @@ namespace details_cow_hashmap
 			ROCKET_ASSERT(nbkt != 0);
 			// Conversion between an unsigned integer type and a floating point type results in performance penalty.
 			// For a value known to be non-negative, an intermediate cast to some signed integer type will mitigate this.
-			const auto seed = static_cast<::std::uint64_t>(static_cast<::std::uint_fast64_t>(hval) * 0xA17870F5D4F51B49);
-			const auto ratio = static_cast<double>(static_cast<::std::int_fast64_t>(seed / 2)) / 0x1p63;
+			const auto seed = hval * static_cast<size_t>(0xA17870F5D4F51B49);
+			const auto ratio = static_cast<double>(static_cast<ptrdiff_t>(seed / 2)) / static_cast<double>(static_cast<size_t>(-1) / 2 + 1);
 			ROCKET_ASSERT((0.0 <= ratio) && (ratio < 1.0));
-			const auto rorigin = static_cast<double>(static_cast<::std::int_fast64_t>(nbkt)) * ratio;
+			const auto rorigin = static_cast<double>(static_cast<ptrdiff_t>(nbkt)) * ratio;
 			ROCKET_ASSERT((0 <= rorigin) && (rorigin < static_cast<double>(nbkt)));
-			return static_cast<size_type>(static_cast<::std::int_fast64_t>(rorigin));
+			return static_cast<size_type>(static_cast<ptrdiff_t>(rorigin));
 		}
 
 		template<typename xpointerT, typename predT>
