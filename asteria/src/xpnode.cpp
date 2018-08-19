070afbe81e3a8ea790228e6456346476ff7814a4
Liu Hao <lh_mouse@126.com>
2018-08-19 23:48:07 +0800

    Minor.

--- asteria/src/xpnode.cpp
+++ asteria/src/xpnode.cpp
@@ -1166,16 +1166,13 @@ void evaluate_xpnode_partial(Vector<Reference> &stack_inout, const Xpnode &node,
 
 Vector<Xpnode> bind_expression(const Vector<Xpnode> &expr, Spref<Context> ctx)
   {
-    if(expr.empty()) {
-      return { };
-    }
     Vector<Xpnode> expr_bnd;
     expr_bnd.reserve(expr.size());
     for(const auto &node : expr) {
       auto node_bnd = bind_xpnode_partial(node, ctx);
       expr_bnd.emplace_back(std::move(node_bnd));
     }
-    return std::move(expr_bnd);
+    return expr_bnd;
   }
 Reference evaluate_expression(const Vector<Xpnode> &expr, Spref<Context> ctx)
   {
