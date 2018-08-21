d9fca0867f7c70b610f4d8f145498b77bbf66849
Liu Hao <lh_mouse@126.com>
2018-08-21 10:29:31 +0800

    Make empty names a special case for named references.

--- asteria/src/context.hpp
+++ asteria/src/context.hpp
@@ -42,16 +42,26 @@ class Context
 
     const Reference * get_named_reference_opt(const String &name) const noexcept
       {
+        if(name.empty()) {
+          return nullptr;
+        }
         return m_named_refs.get(name);
       }
     Reference * get_named_reference_opt(const String &name) noexcept
       {
+        if(name.empty()) {
+          return nullptr;
+        }
         return m_named_refs.get_mut(name);
       }
-    Reference & set_named_reference(const String &name, Reference ref)
-      {
-        return *(m_named_refs.set(name, std::move(ref)).first);
-      }
+    template<typename ...ParamsT, typename std::enable_if<std::is_constructible<Reference, ParamsT &&...>::value>::type * = nullptr>
+      std::pair<Reference *, bool> set_named_reference(const String &name, ParamsT &&...params)
+        {
+          if(name.empty()) {
+            return std::make_pair(nullptr, false);
+          }
+          return m_named_refs.set(name, std::forward<ParamsT>(params)...);
+        }
   };
 
 extern void initialize_function_context(Spref<Context> ctx_out, const Vector<String> &params, const String &file, Unsigned line, Reference self, Vector<Reference> args);
