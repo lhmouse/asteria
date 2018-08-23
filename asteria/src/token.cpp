17c5a180bfef70812001f4a7607c2c75792ad150
Liu Hao <lh_mouse@126.com>
2018-08-23 16:20:01 +0800

    Rename `tokenize_line()` to `tokenize_line_no_comment()`.

--- asteria/src/token.cpp
+++ asteria/src/token.cpp
@@ -640,7 +640,7 @@ namespace {
     }
 }
 
-Parser_result tokenize_line(Vector<Token> &tokens_out, std::size_t line, const String &str)
+Parser_result tokenize_line_no_comment(Vector<Token> &tokens_out, std::size_t line, const String &str)
   {
     // Ensure the source string is valid UTF-8.
     std::size_t column = 0;
--- asteria/src/token.hpp
+++ asteria/src/token.hpp
@@ -184,7 +184,7 @@ class Token
         }
   };
 
-extern Parser_result tokenize_line(Vector<Token> &tokens_out, std::size_t line, const String &str);
+extern Parser_result tokenize_line_no_comment(Vector<Token> &tokens_out, std::size_t line, const String &str);
 
 }
 
