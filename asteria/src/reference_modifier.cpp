a71861367b67c0d92f3a8c5ccc1d3fc208b58da1
Liu Hao <lh_mouse@126.com>
2018-08-27 11:47:25 +0800

    Cleanup.

--- asteria/src/reference_modifier.cpp
+++ asteria/src/reference_modifier.cpp
@@ -28,24 +28,24 @@ const Value * apply_reference_modifier_readonly_partial_opt(const Reference_modi
         if(parent.type() == Value::type_null) {
           return nullptr;
         }
-        if(parent.type() != Value::type_array) {
+        const auto qarr = parent.opt<D_array>();
+        if(!qarr) {
           ASTERIA_THROW_RUNTIME_ERROR("Index `", cand.index, "` cannot be applied to `", parent, "` because it is not an array.");
         }
-        const auto &array = parent.as<D_array>();
         auto rindex = cand.index;
         if(rindex < 0) {
           // Wrap negative indices.
-          rindex += static_cast<Signed>(array.size());
+          rindex += static_cast<Signed>(qarr->size());
         }
         if(rindex < 0) {
-          ASTERIA_DEBUG_LOG("Array index fell before the front: index = ", cand.index, ", array = ", array);
+          ASTERIA_DEBUG_LOG("Array index fell before the front: index = ", cand.index, ", array = ", *qarr);
           return nullptr;
         }
-        if(rindex >= static_cast<Signed>(array.size())) {
-          ASTERIA_DEBUG_LOG("Array index fell after the back: index = ", cand.index, ", array = ", array);
+        if(rindex >= static_cast<Signed>(qarr->size())) {
+          ASTERIA_DEBUG_LOG("Array index fell after the back: index = ", cand.index, ", array = ", *qarr);
           return nullptr;
         }
-        const auto rit = array.begin() + static_cast<std::ptrdiff_t>(rindex);
+        const auto rit = qarr->begin() + static_cast<std::ptrdiff_t>(rindex);
         return &(rit[0]);
       }
     case Reference_modifier::index_object_key:
@@ -54,13 +54,13 @@ const Value * apply_reference_modifier_readonly_partial_opt(const Reference_modi
         if(parent.type() == Value::type_null) {
           return nullptr;
         }
-        if(parent.type() != Value::type_object) {
+        const auto qobj = parent.opt<D_object>();
+        if(!qobj) {
           ASTERIA_THROW_RUNTIME_ERROR("Key `", cand.key, "` cannot be applied to `", parent, "` because it is not an object.");
         }
-        const auto &object = parent.as<D_object>();
-        const auto rit = object.find(cand.key);
-        if(rit == object.end()) {
-          ASTERIA_DEBUG_LOG("Object key was not found: key = ", cand.key, ", object = ", object);
+        const auto rit = qobj->find(cand.key);
+        if(rit == qobj->end()) {
+          ASTERIA_DEBUG_LOG("Object key was not found: key = ", cand.key, ", object = ", *qobj);
           return nullptr;
         }
         return &(rit->second);
@@ -81,42 +81,42 @@ Value * apply_reference_modifier_mutable_partial_opt(const Reference_modifier &m
           }
           parent.set(D_array());
         }
-        if(parent.type() != Value::type_array) {
+        const auto qarr = parent.opt<D_array>();
+        if(!qarr) {
           ASTERIA_THROW_RUNTIME_ERROR("Index `", cand.index, "` cannot be applied to `", parent, "` because it is not an array.");
         }
-        auto &array = parent.as<D_array>();
         auto rindex = cand.index;
         if(rindex < 0) {
           // Wrap negative indices.
-          rindex += static_cast<Signed>(array.size());
+          rindex += static_cast<Signed>(qarr->size());
         }
         if(rindex < 0) {
-          ASTERIA_DEBUG_LOG("Array index fell before the front: index = ", cand.index, ", array = ", array);
+          ASTERIA_DEBUG_LOG("Array index fell before the front: index = ", cand.index, ", array = ", *qarr);
           if(!creates) {
             return nullptr;
           }
           const auto size_add = static_cast<Unsigned>(0) - static_cast<Unsigned>(rindex);
-          if(size_add >= array.max_size() - array.size()) {
-            ASTERIA_THROW_RUNTIME_ERROR("Extending the array of size `", array.size(), "` by `", size_add, "` would exceed system resource limits.");
+          if(size_add >= qarr->max_size() - qarr->size()) {
+            ASTERIA_THROW_RUNTIME_ERROR("Extending the array of size `", qarr->size(), "` by `", size_add, "` would exceed system resource limits.");
           }
-          array.insert(array.begin(), static_cast<std::size_t>(size_add));
+          qarr->insert(qarr->begin(), static_cast<std::size_t>(size_add));
           rindex = 0;
         }
-        if(rindex >= static_cast<Signed>(array.size())) {
-          ASTERIA_DEBUG_LOG("Array index fell after the back: index = ", cand.index, ", array = ", array);
+        if(rindex >= static_cast<Signed>(qarr->size())) {
+          ASTERIA_DEBUG_LOG("Array index fell after the back: index = ", cand.index, ", array = ", *qarr);
           if(!creates) {
             return nullptr;
           }
-          const auto size_add = static_cast<Unsigned>(rindex) + 1 - array.size();
-          if(size_add >= array.max_size() - array.size()) {
-            ASTERIA_THROW_RUNTIME_ERROR("Extending the array of size `", array.size(), "` by `", size_add, "` would exceed system resource limits.");
+          const auto size_add = static_cast<Unsigned>(rindex) + 1 - qarr->size();
+          if(size_add >= qarr->max_size() - qarr->size()) {
+            ASTERIA_THROW_RUNTIME_ERROR("Extending the array of size `", qarr->size(), "` by `", size_add, "` would exceed system resource limits.");
           }
-          array.insert(array.end(), static_cast<std::size_t>(size_add));
+          qarr->insert(qarr->end(), static_cast<std::size_t>(size_add));
         }
-        const auto rit = array.mut_begin() + static_cast<std::ptrdiff_t>(rindex);
+        const auto rit = qarr->mut_begin() + static_cast<std::ptrdiff_t>(rindex);
         if(erased_out_opt){
           *erased_out_opt = std::move(rit[0]);
-          array.erase(rit);
+          qarr->erase(rit);
           return erased_out_opt;
         }
         return &(rit[0]);
@@ -130,22 +130,27 @@ Value * apply_reference_modifier_mutable_partial_opt(const Reference_modifier &m
           }
           parent.set(D_object());
         }
-        if(parent.type() != Value::type_object) {
+        const auto qobj = parent.opt<D_object>();
+        if(!qobj) {
           ASTERIA_THROW_RUNTIME_ERROR("Key `", cand.key, "` cannot be applied to `", parent, "` because it is not an object.");
         }
-        auto &object = parent.as<D_object>();
         auto rit = D_object::iterator();
         if(!creates) {
-          rit = object.find_mut(cand.key);
-          if(rit == object.end()) {
+          rit = qobj->find_mut(cand.key);
+          if(rit == qobj->end()) {
+            ASTERIA_DEBUG_LOG("Object key was not found: key = ", cand.key, ", object = ", *qobj);
             return nullptr;
           }
         } else {
-          rit = object.try_emplace(cand.key).first;
+          const auto result = qobj->try_emplace(cand.key);
+          if(result.second) {
+            ASTERIA_DEBUG_LOG("New object key has been created: key = ", cand.key, ", object = ", *qobj);
+          }
+          rit = result.first;
         }
         if(erased_out_opt){
           *erased_out_opt = std::move(rit->second);
-          object.erase(rit);
+          qobj->erase(rit);
           return erased_out_opt;
         }
         return &(rit->second);
