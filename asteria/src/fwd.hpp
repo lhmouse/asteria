41d5df05a3d3408f177e417be0a060977f2f11cb
Liu Hao <lh_mouse@126.com>
2018-09-05 00:37:32 +0800

    Add aliases for <cstdint>.

--- asteria/src/fwd.hpp
+++ asteria/src/fwd.hpp
@@ -24,6 +24,22 @@ using Unsigned  = std::uint64_t;
 using Double    = double;
 using String    = rocket::cow_string;
 
+using Sint8     = std::int8_t;
+using Uint8     = std::uint8_t;
+using Sint16    = std::int16_t;
+using Uint16    = std::uint16_t;
+using Sint32    = std::int32_t;
+using Uint32    = std::uint32_t;
+using Sint64    = std::int64_t;
+using Uint64    = std::uint64_t;
+using Sintmax   = std::intmax_t;
+using Uintmax   = std::uintmax_t;
+using Sintptr   = std::intptr_t;
+using Uintptr   = std::uintptr_t;
+
+using Size      = std::size_t;
+using Diff      = std::ptrdiff_t;
+
 template<typename ElementT>
   using Vector = rocket::cow_vector<ElementT>;
 template<typename FirstT, typename SecondT>
