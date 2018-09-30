8b22bbc78e01ad176b6bf9d91172aa10ae5f479d
Liu Hao <lh_mouse@126.com>
2018-09-30 21:29:23 +0800

    Fix typo.

--- asteria/src/instantiated_function.cpp
+++ asteria/src/instantiated_function.cpp
@@ -23,7 +23,7 @@ String Instantiated_function::describe() const
 Reference Instantiated_function::invoke(Global_context *global_opt, Reference self, Vector<Reference> args) const
   {
     Executive_context ctx_next;
-    ctx_next.initialize_for_function(this->m_params, this->m_file, this->m_line, std::move(self), std::move(std::move(args)));
+    ctx_next.initialize_for_function(this->m_params, this->m_file, this->m_line, std::move(self), std::move(args));
     return this->m_body.execute_as_function_in_place(ctx_next, global_opt);
   }
 
