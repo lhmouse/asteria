ac3adb072269adaea43116a620dc9ed8e1eb5e4c
Liu Hao <lh_mouse@126.com>
2018-10-07 20:48:32 +0800

    Update 'test/exception.cpp'.

--- test/exception.cpp
+++ test/exception.cpp
@@ -9,18 +9,8 @@ using namespace Asteria;
 int main()
   {
     try {
-      try {
-        throw Exception(Reference_root::S_constant { D_integer(42) });
-      } catch(Exception &e) {
-        const auto val = e.get_reference().read();
-        ASTERIA_TEST_CHECK(val.type() == Value::type_integer);
-        ASTERIA_TEST_CHECK(val.check<D_integer>() == 42);
-        e.set_reference(Reference(Reference_root::S_temporary { D_string(String::shallow("hello")) }));
-        throw;
-      }
+      throw Exception(D_integer(42));
     } catch(Exception &e) {
-      const auto val = e.get_reference().read();
-      ASTERIA_TEST_CHECK(val.type() == Value::type_string);
-      ASTERIA_TEST_CHECK(val.check<D_string>() == "hello");
+      ASTERIA_TEST_CHECK(e.get_value().check<D_integer>() == 42);
     }
   }
