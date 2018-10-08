ce3c1e26aa08e27c6007c04f5ba37af1d9bc5c5b
Liu Hao <lh_mouse@126.com>
2018-10-08 22:19:47 +0800

    Update 'check_includes.sh'.

--- check_includes.sh
+++ check_includes.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-function check_one(){
-  _cmd="gcc -std=c++11 -x c++ -c -o /dev/null"
-  echo "Checking \`#include\` directives:  ${_cmd}  \"$1\""
-  ${_cmd}  "$1"
-}
+function check_one()
+  {
+    _cmd="gcc -std=c++11 -x c++ -c -o /dev/null"
+    echo "Checking \`#include\` directives:  ${_cmd}  \"$1\""
+    ${_cmd}  "$1"
+  }
 
 export -f check_one
-find -L "asteria/src/" -name "*.hpp" -print0 | xargs -0 -I {} bash -c 'check_one "$@"' "$0" {}
+find -L "asteria/src/" -name "*.hpp" -print0 | xargs -0 -I {} bash -ec 'check_one "$@"' "$0" {}
