21d83c9f7b8782fb74eaf0396a50db2bfe87951f
Liu Hao <lh_mouse@126.com>
2018-10-11 11:12:03 +0800

    Minor.

--- asteria/src/variadic_arguer.cpp
+++ asteria/src/variadic_arguer.cpp
@@ -21,6 +21,11 @@ String Variadic_arguer::describe() const
 Reference Variadic_arguer::invoke(Global_context * /*global_opt*/, Reference /*self*/, Vector<Reference> args) const
   {
     switch(args.size()) {
+      case 0: {
+        // Return the number of variadic arguments.
+        Reference_root::S_constant ref_c = { D_integer(this->m_vargs.size()) };
+        return std::move(ref_c);
+      }
       case 1: {
         // Return the argument at the index specified.
         const auto ivalue = args.at(0).read();
@@ -36,11 +41,6 @@ Reference Variadic_arguer::invoke(Global_context * /*global_opt*/, Reference /*s
           return { };
         }
         return this->m_vargs.at(static_cast<Size>(rindex));
-      }
-      case 0: {
-        // Return the number of variadic arguments.
-        Reference_root::S_constant ref_c = { D_integer(this->m_vargs.size()) };
-        return std::move(ref_c);
       }
       default: {
         ASTERIA_THROW_RUNTIME_ERROR("A variadic argument accessor takes no more than one argument.");
