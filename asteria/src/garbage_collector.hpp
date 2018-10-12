3af3db28a56f04a4a9bb4b935219f7bc409f57f7
Liu Hao <lh_mouse@126.com>
2018-10-12 23:15:25 +0800

    Update `Garbage_collector`.

--- asteria/src/garbage_collector.hpp
+++ asteria/src/garbage_collector.hpp
@@ -9,15 +9,16 @@
 
 namespace Asteria {
 
-class Garbage_collector
+class Garbage_collector : public rocket::refcounted_base<Garbage_collector>
   {
   private:
+    Garbage_collector *m_tied_opt;
     Vector<rocket::refcounted_ptr<Variable>> m_vars;  // This is a flat map.
     Bivector<rocket::refcounted_ptr<Variable>, long> m_gcrefs;  // This is a flat map.
 
   public:
     Garbage_collector() noexcept
-      : m_vars(), m_gcrefs()
+      : m_tied_opt(nullptr), m_vars(), m_gcrefs()
       {
       }
     ~Garbage_collector();
@@ -28,9 +29,18 @@ class Garbage_collector
       = delete;
 
   public:
+    Garbage_collector * get_tied_collector_opt() const noexcept
+      {
+        return this->m_tied_opt;
+      }
+    void tie_collector(Garbage_collector *tied_opt) noexcept
+      {
+        this->m_tied_opt = tied_opt;
+      }
+
     bool track_variable(const rocket::refcounted_ptr<Variable> &var);
     bool untrack_variable(const rocket::refcounted_ptr<Variable> &var) noexcept;
-    void collect();
+    void collect(bool unreserve);
   };
 
 }
