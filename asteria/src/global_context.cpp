8f30ce67c1b9176563b3387dafb5ed212069ce98
Liu Hao <lh_mouse@126.com>
2018-10-13 14:55:55 +0800

    Add `Global_collector`.

--- asteria/src/global_context.cpp
+++ asteria/src/global_context.cpp
@@ -23,7 +23,21 @@ const Abstract_context * Global_context::get_parent_opt() const noexcept
 
 rocket::refcounted_ptr<Variable> Global_context::create_tracked_variable()
   {
-    return rocket::make_refcounted<Variable>();
+    auto coll = this->m_collector_opt;
+    if(!coll) {
+      coll = rocket::make_refcounted<Global_collector>();
+      this->m_collector_opt = coll;
+    }
+    return coll->create_tracked_variable();
+  }
+
+void Global_context::perform_garbage_collection(unsigned gen_max, bool unreserve)
+  {
+    auto coll = this->m_collector_opt;
+    if(!coll) {
+      return;
+    }
+    return coll->perform_garbage_collection(gen_max, unreserve);
   }
 
 }
