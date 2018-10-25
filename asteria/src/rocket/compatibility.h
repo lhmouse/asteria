8f15a6e88666551337a86d423e2b7dc55ab51dc8
Liu Hao <lh_mouse@126.com>
2018-10-25 22:19:03 +0800

    Check for compiler and library support in a more insane way.

--- asteria/src/rocket/compatibility.h
+++ asteria/src/rocket/compatibility.h
@@ -19,15 +19,21 @@
 //======================================================================
 #ifdef ROCKET_CLANG
 
-#if !defined(ROCKET_DEBUG) && defined(_LIBCPP_DEBUG)
-#  define ROCKET_DEBUG                   1
+// Check for compiler support.
+#define ROCKET_ATTRIBUTE_PRINTF(...)        __attribute__((__format__(__printf__, __VA_ARGS__)))
+#define ROCKET_FUNCSIG                      __PRETTY_FUNCTION__
+#define ROCKET_UNREACHABLE()                __builtin_unreachable()
+#define ROCKET_SELECTANY                    __attribute__((__weak__))
+
+// Check for either libc++ or libstdc++.
+#if defined(_LIBCPP_DEBUG) || defined(_GLIBCXX_DEBUG)
+#  define ROCKET_DEBUG                      1
 #endif
 
-#define ROCKET_ATTRIBUTE_PRINTF(...)     __attribute__((__format__(__printf__, __VA_ARGS__)))
-#define ROCKET_FUNCSIG                   __PRETTY_FUNCTION__
-#define ROCKET_UNREACHABLE()             __builtin_unreachable()
-#define ROCKET_SELECTANY                 __attribute__((__weak__))
-#define ROCKET_TRIVIAL_TYPE_TRAITS       1
+// Check for libc++ or libstdc++ from GCC 5.
+#if defined(_LIBCPP_VERSION) || defined(_GLIBCXX14_CONSTEXPR)
+#  define ROCKET_HAS_TRIVIALITY_TRAITS      1
+#endif
 
 #endif // ROCKET_CLANG
 
@@ -36,15 +42,21 @@
 //======================================================================
 #ifdef ROCKET_GCC
 
-#if !defined(ROCKET_DEBUG) && defined(_GLIBCXX_DEBUG)
-#  define ROCKET_DEBUG                   1
+// Check for compiler support.
+#define ROCKET_ATTRIBUTE_PRINTF(...)        __attribute__((__format__(__gnu_printf__, __VA_ARGS__)))
+#define ROCKET_FUNCSIG                      __PRETTY_FUNCTION__
+#define ROCKET_UNREACHABLE()                __builtin_unreachable()
+#define ROCKET_SELECTANY                    __attribute__((__weak__))
+
+// Check for libstdc++.
+#if defined(_GLIBCXX_DEBUG)
+#  define ROCKET_DEBUG                      1
 #endif
 
-#define ROCKET_ATTRIBUTE_PRINTF(...)     __attribute__((__format__(__gnu_printf__, __VA_ARGS__)))
-#define ROCKET_FUNCSIG                   __PRETTY_FUNCTION__
-#define ROCKET_UNREACHABLE()             __builtin_unreachable()
-#define ROCKET_SELECTANY                 __attribute__((__weak__))
-#define ROCKET_TRIVIAL_TYPE_TRAITS       (__GNUG__ >= 5)
+// Check for libstdc++ from GCC 5.
+#if defined(_GLIBCXX14_CONSTEXPR)
+#  define ROCKET_HAS_TRIVIALITY_TRAITS      1
+#endif
 
 #endif // ROCKET_GCC
 
@@ -53,15 +65,21 @@
 //======================================================================
 #ifdef ROCKET_MSVC
 
-#if !defined(ROCKET_DEBUG) && defined(_DEBUG)
-#  define ROCKET_DEBUG                   1
+// Check for compiler support.
+#define ROCKET_ATTRIBUTE_PRINTF(...)        // not implemented
+#define ROCKET_FUNCSIG                      __FUNCSIG__
+#define ROCKET_UNREACHABLE()                __assume(0)
+#define ROCKET_SELECTANY                    __declspec(selectany)
+
+// Check for project configuration.
+#if defined(_DEBUG)
+#  define ROCKET_DEBUG                      1
 #endif
 
-#define ROCKET_ATTRIBUTE_PRINTF(...)     // not implemented
-#define ROCKET_FUNCSIG                   __FUNCSIG__
-#define ROCKET_UNREACHABLE()             __assume(0)
-#define ROCKET_SELECTANY                 __declspec(selectany)
-#define ROCKET_TRIVIAL_TYPE_TRAITS       1
+// Check for the version of... the compiler?
+#if _MSC_VER >= 1700
+#  define ROCKET_HAS_TRIVIALITY_TRAITS      1
+#endif
 
 #endif // ROCKET_MSVC
 
