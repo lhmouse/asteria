10c98b9de2fe3360453ebcea5d2b2546f508fc55
Liu Hao <lh_mouse@126.com>
2018-11-14 17:20:50 +0800

    Make reference accessors inline.

--- asteria/src/reference.cpp
+++ asteria/src/reference.cpp
@@ -12,75 +12,9 @@ Reference::~Reference()
   {
   }
 
-Value Reference::read() const
+void Reference::do_throw_no_modifier() const
   {
-    // Dereference the root.
-    auto cur = std::ref(this->m_root.dereference_const());
-    // Apply modifiers.
-    const auto end = this->m_mods.end();
-    for(auto it = this->m_mods.begin(); it != end; ++it) {
-      const auto qnext = it->apply_const_opt(cur);
-      if(!qnext) {
-        return { };
-      }
-      cur = std::ref(*qnext);
-    }
-    return cur;
-  }
-
-Value & Reference::write(Value value) const
-  {
-    // Dereference the root.
-    auto cur = std::ref(this->m_root.dereference_mutable());
-    // Apply modifiers.
-    const auto end = this->m_mods.end();
-    for(auto it = this->m_mods.begin(); it != end; ++it) {
-      const auto qnext = it->apply_mutable_opt(cur, true);
-      if(!qnext) {
-        ROCKET_ASSERT(false);
-      }
-      cur = std::ref(*qnext);
-    }
-    return cur.get() = std::move(value);
-  }
-
-Value Reference::unset() const
-  {
-    if(this->m_mods.empty()) {
-      ASTERIA_THROW_RUNTIME_ERROR("Only array elements or object members may be `unset`.");
-    }
-    // Dereference the root.
-    auto cur = std::ref(this->m_root.dereference_mutable());
-    // Apply modifiers.
-    const auto end = this->m_mods.end() - 1;
-    for(auto it = this->m_mods.begin(); it != end; ++it) {
-      const auto qnext = it->apply_mutable_opt(cur, false);
-      if(!qnext) {
-        return { };
-      }
-      cur = std::ref(*qnext);
-    }
-    return end->apply_and_erase(cur);
-  }
-
-Reference & Reference::zoom_in(Reference_modifier mod)
-  {
-    // Append a modifier.
-    this->m_mods.emplace_back(std::move(mod));
-    return *this;
-  }
-
-Reference & Reference::zoom_out()
-  {
-    if(this->m_mods.empty()) {
-      // If there is no modifier, set `*this` to a null reference.
-      Reference_root::S_constant ref_c = { D_null() };
-      *this = std::move(ref_c);
-      return *this;
-    }
-    // Drop the last modifier.
-    this->m_mods.pop_back();
-    return *this;
+    ASTERIA_THROW_RUNTIME_ERROR("Only array elements or object members may be `unset`.");
   }
 
 Reference & Reference::convert_to_temporary()
@@ -107,16 +41,6 @@ Reference & Reference::convert_to_variable(Global_context &global, bool immutabl
     Reference_root::S_variable ref_c = { std::move(var) };
     *this = std::move(ref_c);
     return *this;
-  }
-
-void Reference::enumerate_variables(const Abstract_variable_callback &callback) const
-  {
-    this->m_root.enumerate_variables(callback);
-  }
-
-void Reference::dispose_variable(Global_context &global) const noexcept
-  {
-    this->m_root.dispose_variable(global);
   }
 
 }
--- asteria/src/reference.hpp
+++ asteria/src/reference.hpp
@@ -7,6 +7,7 @@
 #include "fwd.hpp"
 #include "reference_root.hpp"
 #include "reference_modifier.hpp"
+#include <functional>
 
 namespace Asteria {
 
@@ -39,24 +40,96 @@ class Reference
       }
     ROCKET_COPYABLE_DESTRUCTOR(Reference);
 
+  private:
+    [[noreturn]] void do_throw_no_modifier() const;
+
   public:
     bool is_constant() const noexcept
       {
         return this->m_root.index() == Reference_root::index_constant;
       }
 
-    Value read() const;
-    Value & write(Value value) const;
-    Value unset() const;
+    Value read() const
+      {
+        // Dereference the root.
+        auto cur = std::ref(this->m_root.dereference_const());
+        // Apply modifiers.
+        const auto end = this->m_mods.end();
+        for(auto it = this->m_mods.begin(); it != end; ++it) {
+          const auto qnext = it->apply_const_opt(cur);
+          if(!qnext) {
+            return { };
+          }
+          cur = std::ref(*qnext);
+        }
+        return cur;
+      }
+    template<typename ValueT>
+      Value & write(ValueT &&value) const
+      {
+        // Dereference the root.
+        auto cur = std::ref(this->m_root.dereference_mutable());
+        // Apply modifiers.
+        const auto end = this->m_mods.end();
+        for(auto it = this->m_mods.begin(); it != end; ++it) {
+          const auto qnext = it->apply_mutable_opt(cur, true);
+          if(!qnext) {
+            ROCKET_ASSERT(false);
+          }
+          cur = std::ref(*qnext);
+        }
+        return cur.get() = std::forward<ValueT>(value);
+      }
+    Value unset() const
+      {
+        if(this->m_mods.empty()) {
+          this->do_throw_no_modifier();
+        }
+        // Dereference the root.
+        auto cur = std::ref(this->m_root.dereference_mutable());
+        // Apply modifiers.
+        const auto end = this->m_mods.end() - 1;
+        for(auto it = this->m_mods.begin(); it != end; ++it) {
+          const auto qnext = it->apply_mutable_opt(cur, false);
+          if(!qnext) {
+            return { };
+          }
+          cur = std::ref(*qnext);
+        }
+        return end->apply_and_erase(cur);
+      }
 
-    Reference & zoom_in(Reference_modifier mod);
-    Reference & zoom_out();
+    template<typename XmodT>
+      Reference & zoom_in(XmodT &&mod)
+      {
+        // Append a modifier.
+        this->m_mods.emplace_back(std::forward<XmodT>(mod));
+        return *this;
+      }
+    Reference & zoom_out()
+      {
+        if(this->m_mods.empty()) {
+          // If there is no modifier, set `*this` to a null reference.
+          Reference_root::S_constant ref_c = { D_null() };
+          *this = std::move(ref_c);
+          return *this;
+        }
+        // Drop the last modifier.
+        this->m_mods.pop_back();
+        return *this;
+      }
+
+    void enumerate_variables(const Abstract_variable_callback &callback) const
+      {
+        this->m_root.enumerate_variables(callback);
+      }
+    void dispose_variable(Global_context &global) const noexcept
+      {
+        this->m_root.dispose_variable(global);
+      }
 
     Reference & convert_to_temporary();
     Reference & convert_to_variable(Global_context &global, bool immutable);
-
-    void enumerate_variables(const Abstract_variable_callback &callback) const;
-    void dispose_variable(Global_context &global) const noexcept;
   };
 
 }
