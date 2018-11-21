d671939b944920e394b1af86404558b81002f675
Liu Hao <lh_mouse@126.com>
2018-11-21 15:03:03 +0800

    GCC still emits calls to noop constructors???

--- asteria/src/runtime/value.cpp
+++ asteria/src/runtime/value.cpp
@@ -43,31 +43,11 @@ const char * Value::get_type_name(Value::Type type) noexcept
     }
   }
 
-    namespace {
-
-    union Constant_null
-      {
-        Value value;
-
-        Constant_null() noexcept
-          {
-          }
-        ~Constant_null()
-          {
-          }
-
-        operator const Value & () const noexcept
-          {
-            return this->value;
-          }
-      }
-    const s_null;  // Don't play with this at home.
-
-    }
-
 const Value & Value::get_null() noexcept
   {
-    return s_null;
+    alignas(Value) static constexpr char s_zeroes[sizeof(Value)] = { };
+    // Don't play with this at home.
+    return reinterpret_cast<const Value &>(s_zeroes);
   }
 
 Value::~Value()
