88e533752c3c534f78b62cb2f05791f6928df78f
Liu Hao <lh_mouse@126.com>
2018-12-12 20:33:27 +0800

    Minor.

--- asteria/src/rocket/cow_vector.hpp
+++ asteria/src/rocket/cow_vector.hpp
@@ -76,7 +76,7 @@ template<typename valueT, typename allocatorT = allocator<valueT>>
       };
 
     template<typename allocatorT>
-      struct copy_trivially : integral_constant<bool, is_trivial<typename allocatorT::value_type>::value && is_std_allocator<allocatorT>::value>
+      struct copy_trivially : conjunction<is_trivial<typename allocatorT::value_type>, is_std_allocator<allocatorT>>
       {
       };
 
