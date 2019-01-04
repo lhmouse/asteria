16d317533cdecbf794af2c157605e19a44ea2181
Liu Hao <lh_mouse@126.com>
2019-01-04 23:30:02 +0800

    Cleanup.

--- asteria/src/runtime/reference_stack.cpp
+++ asteria/src/runtime/reference_stack.cpp
@@ -12,25 +12,22 @@ Reference_stack::~Reference_stack()
   }
 
 void Reference_stack::do_switch_to_large()
-  try {
+  {
     ROCKET_ASSERT(this->m_small.size() == this->m_small.capacity());
     ROCKET_ASSERT(this->m_large.capacity() == 0);
     auto tptr = this->m_tptr;
     const auto nelem = this->m_small.size();
     // Note that this function has to provide strong exception safety guarantee.
     rocket::cow_vector<Reference> large;
-    large.reserve(nelem / 2 | nelem);
-    large.append(std::make_move_iterator(tptr - nelem), std::make_move_iterator(tptr));
+    large.reserve(nelem * 2);
+    for(auto i = nelem; i != 0; --i) {
+      large.emplace_back(std::move(tptr[-i]));
+    }
     tptr = large.mut_data() + nelem;
     // Set up the large buffer.
     this->m_tptr = tptr;
     this->m_large.swap(large);
     this->m_small.clear();
-  } catch(...) {
-    // The capacity must be preserved.
-    rocket::cow_vector<Reference> large;
-    this->m_large.swap(large);
-    throw;
   }
 
 }
