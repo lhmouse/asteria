825ca34721575eb1a68cc86a36254c17c3a453d6
Liu Hao <lh_mouse@126.com>
2019-01-15 13:54:20 +0800

    Fix CI error.

--- asteria/src/rocket/assert.hpp
+++ asteria/src/rocket/assert.hpp
@@ -8,35 +8,36 @@
 
 namespace rocket {
 
+// `report_assertion_failure()` is always provided even when assertions are disabled.
 [[noreturn]] extern void report_assertion_failure(const char *expr, const char *file, long line, const char *msg) noexcept;
 
-template<typename ...paramsT>
-  [[noreturn]] ROCKET_ARTIFICIAL_FUNCTION inline bool assertion_failure_handler(paramsT &&...params) noexcept
+// When an assertion fails, do this.
+[[noreturn]] ROCKET_ARTIFICIAL_FUNCTION inline bool assertion_failure_handler(const char *expr, const char *file, long line, const char *msg) noexcept
   {
 #ifdef ROCKET_DEBUG
-    // Disallow ADL.
-    (report_assertion_failure)(static_cast<paramsT &&>(params)...);
+    report_assertion_failure(expr, file, line, msg);
 #else
     ROCKET_UNREACHABLE();
-    // Silence 'unused parameter' warnings.
-    static_cast<void>(sizeof...(params));
 #endif
+    // Silence 'unused parameter' warnings.
+    (void(expr - file + line + msg));
   }
 
 // This function template exists to prevent the argument expression of `ROCKET_ASSERT()` from being evaluated more than once.
-template<typename valueT, typename ...paramsT>
-  ROCKET_ARTIFICIAL_FUNCTION constexpr valueT && asserted_value_wrapper(valueT &&value, paramsT &&...params) noexcept
+template<typename valueT>
+  ROCKET_ARTIFICIAL_FUNCTION constexpr valueT && asserted_value_wrapper(valueT &&value, const char *expr, const char *file, long line, const char *msg) noexcept
   {
-    return (value ? true : (assertion_failure_handler)(static_cast<paramsT &&>(params)...)),
+    return void(bool(value) || assertion_failure_handler(expr, file, line, msg)),
            static_cast<valueT &&>(value);
   }
 
 }
 
 #define ROCKET_ASSERT_IMPLEMENTATION_(value_, ...)  \
-    (ROCKET_CONSTANT_P(value_) ? (((value_) ? true : ::rocket::assertion_failure_handler(__VA_ARGS__)), \
-                                  (value_))  \
-                               : ::rocket::asserted_value_wrapper((value_), __VA_ARGS__))
+    (ROCKET_CONSTANT_P(value_)  \
+     ? void(bool(value_) || ::rocket::assertion_failure_handler(__VA_ARGS__)),  \
+       (value_)  \
+     : ::rocket::asserted_value_wrapper(value_, __VA_ARGS__))
 
 #define ROCKET_ASSERT(expr_)              ROCKET_ASSERT_IMPLEMENTATION_(expr_, #expr_, __FILE__, __LINE__, (""))
 #define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_ASSERT_IMPLEMENTATION_(expr_, #expr_, __FILE__, __LINE__, (m_))
