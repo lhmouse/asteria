9301ce7cee49db99a42e1fab8f2c0e7b83071627
Liu Hao <lh_mouse@126.com>
2019-01-15 13:17:05 +0800

    Fix CI error.

--- asteria/src/rocket/assert.hpp
+++ asteria/src/rocket/assert.hpp
@@ -10,8 +10,8 @@ namespace rocket {
 
 [[noreturn]] extern void report_assertion_failure(const char *expr, const char *file, long line, const char *msg) noexcept;
 
-template<typename valueT, typename ...paramsT>
-  [[noreturn]] inline valueT && assertion_failure_handler(paramsT &&...params) noexcept
+template<typename ...paramsT>
+  [[noreturn]] inline bool assertion_failure_handler(paramsT &&...params) noexcept
   {
 #ifdef ROCKET_DEBUG
     // Disallow ADL.
@@ -27,18 +27,18 @@ template<typename valueT, typename ...paramsT>
 template<typename valueT, typename ...paramsT>
   constexpr valueT && asserted_value_wrapper(valueT &&value, paramsT &&...params) noexcept
   {
-    return value ? static_cast<valueT &&>(value)
-                 : assertion_failure_handler<valueT>(static_cast<paramsT &&>(params)...);
+    return (value ? true : (assertion_failure_handler)(static_cast<paramsT &&>(params)...)),
+           static_cast<valueT &&>(value);
   }
 
 }
 
 #define ROCKET_ASSERT_IMPLEMENTATION_(value_, ...)  \
-    (ROCKET_CONSTANT_P(value_) ? ((value_) ? (value_)  \
-                                           : ::rocket::assertion_failure_handler<decltype((value_))>(__VA_ARGS__))  \
+    (ROCKET_CONSTANT_P(value_) ? (((value_) ? true : ::rocket::assertion_failure_handler(__VA_ARGS__)), \
+                                  (value_))  \
                                : ::rocket::asserted_value_wrapper((value_), __VA_ARGS__))
 
-#define ROCKET_ASSERT(expr_)              ROCKET_ASSERT_IMPLEMENTATION_((expr_), #expr_, __FILE__, __LINE__, (""))
-#define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_ASSERT_IMPLEMENTATION_((expr_), #expr_, __FILE__, __LINE__, (m_))
+#define ROCKET_ASSERT(expr_)              ROCKET_ASSERT_IMPLEMENTATION_(expr_, #expr_, __FILE__, __LINE__, (""))
+#define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_ASSERT_IMPLEMENTATION_(expr_, #expr_, __FILE__, __LINE__, (m_))
 
 #endif
