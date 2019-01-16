33f26b7c162c2a056ff6c354390d899ec6ff2bc1
Liu Hao <lh_mouse@126.com>
2019-01-16 20:22:28 +0800

    Update 'assert.hpp'.

--- asteria/src/rocket/assert.hpp
+++ asteria/src/rocket/assert.hpp
@@ -11,15 +11,25 @@ namespace rocket {
 // `report_assertion_failure()` is always provided even when assertions are disabled.
 [[noreturn]] extern bool report_assertion_failure(const char *expr, const char *file, long line, const char *msg) noexcept;
 
+    namespace details_assert {
+
+    [[noreturn]] ROCKET_ARTIFICIAL_FUNCTION inline bool unreachable() noexcept
+      {
+        ROCKET_UNREACHABLE();
+      };
+
+    }
+
 }
 
 #ifdef ROCKET_DEBUG
-#  define ROCKET_DETAILS_FAIL_ASSERT_(...)    (::rocket::report_assertion_failure(__VA_ARGS__))
+#  define ROCKET_DETAILS_FAIL_ASSERTION_(...)    ::rocket::report_assertion_failure(__VA_ARGS__)
 #else
-#  define ROCKET_DETAILS_FAIL_ASSERT_(...)    (ROCKET_UNREACHABLE(), true)
+#  define ROCKET_DETAILS_FAIL_ASSERTION_(...)    ::rocket::details_assert::unreachable()
 #endif
 
-#define ROCKET_DETAILS_ASSERT_(pred_, xstr_, msg_)    (void((pred_) ? true : ROCKET_DETAILS_FAIL_ASSERT_(xstr_, __FILE__, __LINE__, msg_)))
+#define ROCKET_DETAILS_ASSERT_(pred_, xstr_, msg_)    (void(static_cast<bool>(pred_) ||  \
+                                                            ROCKET_DETAILS_FAIL_ASSERTION_(xstr_, __FILE__, __LINE__, msg_)))
 
 #define ROCKET_ASSERT(expr_)              ROCKET_DETAILS_ASSERT_(expr_, #expr_, "")
 #define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_DETAILS_ASSERT_(expr_, #expr_, m_)
