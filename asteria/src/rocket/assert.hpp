9ac3f067ae90cefe4e8bab916a89d6a53f55ae84
Liu Hao <lh_mouse@126.com>
2019-01-16 20:13:16 +0800

    Fix 'rocket/assert.hpp'.

--- asteria/src/rocket/assert.hpp
+++ asteria/src/rocket/assert.hpp
@@ -14,14 +14,14 @@ namespace rocket {
 }
 
 #ifdef ROCKET_DEBUG
-#  define ROCKET_DETAILS_REPORT_ASSERTION_FAILURE_(...)    ::rocket::report_assertion_failure(__VA_ARGS__)
+#  define ROCKET_DETAILS_FAIL_ASSERT_(...)    (::rocket::report_assertion_failure(__VA_ARGS__))
 #else
-#  define ROCKET_DETAILS_REPORT_ASSERTION_FAILURE_(...)    ROCKET_UNREACHABLE(::rocket::report_assertion_failure(__VA_ARGS__))
+#  define ROCKET_DETAILS_FAIL_ASSERT_(...)    (ROCKET_UNREACHABLE(), true)
 #endif
 
-#define ROCKET_DETAILS_IMPLEMENT_ASSERT_(pred_, xstr_, msg_)     static_cast<void>(bool(pred_) || ::rocket::report_assertion_failure(xstr_, __FILE__, __LINE__, msg_))
+#define ROCKET_DETAILS_ASSERT_(pred_, xstr_, msg_)    (void((pred_) ? true : ROCKET_DETAILS_FAIL_ASSERT_(xstr_, __FILE__, __LINE__, msg_)))
 
-#define ROCKET_ASSERT(expr_)              ROCKET_DETAILS_IMPLEMENT_ASSERT_(expr_, #expr_, "")
-#define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_DETAILS_IMPLEMENT_ASSERT_(expr_, #expr_, m_)
+#define ROCKET_ASSERT(expr_)              ROCKET_DETAILS_ASSERT_(expr_, #expr_, "")
+#define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_DETAILS_ASSERT_(expr_, #expr_, m_)
 
 #endif
