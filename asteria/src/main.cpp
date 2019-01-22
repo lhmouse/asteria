53adbaed81057dd8dcda4571fdd4cbc807faf6e9
Liu Hao <lh_mouse@126.com>
2019-01-22 23:57:33 +0800

    Update 'main.cpp'.

--- asteria/src/main.cpp
+++ asteria/src/main.cpp
@@ -12,26 +12,38 @@ using namespace Asteria;
 
 int main()
   try {
+    // prepare test code.
     static constexpr char src[] = R"_____(
       func fib(n) {
-        var r = n;
-        if!(n <= 1) {
-          r = fib(n-1) + fib(n-2);
+        if(n <= 1) {
+          return& n;
         }
-        return& r;
+        return& fib(n-1) + fib(n-2);
       }
       return fib(30);
     )_____";
     rocket::insertable_istream iss(rocket::sref(src));
-    std::cerr << "Source code:\n---\n" << src << "\n---\nRunning..." << std::endl;;
+    std::cerr << "Source code:\n---\n" << src << "\n---" << std::endl;;
+    // parse it.
     Simple_Source_File code(iss, rocket::sref("my_file"));
     Global_Context global;
+    // run it and measure the time.
     const auto t1 = std::chrono::high_resolution_clock::now();
     auto res = code.execute(global, { });
     const auto t2 = std::chrono::high_resolution_clock::now();
-    std::cout << "Finished in " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms:\n---\n" << res.read() << std::endl;
+    // print the time elasped and the result.
+    std::cerr << "Finished in " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms:\n---" << std::endl;
+    std::cout << res.read() << std::endl;
+    // finish.
+    return 0;
   } catch(Traceable_Exception &e) {
-    std::cerr << "Caught `Traceable_Exception`:\n---\n" << e.get_value() << std::endl;
+    // print the exception.
+    std::cerr << "Caught `Asteria::Traceable_Exception`:\n---" << std::endl;
+    std::cerr << e.get_value() << std::endl;
+    return 1;
   } catch(std::exception &e) {
-    std::cerr << "Caught `std::exception`:\n---\n" << e.what() << std::endl;
+    // print the exception.
+    std::cerr << "Caught `std::exception`:\n---" << std::endl;
+    std::cerr << e.what() << std::endl;
+    return 1;
   }
