9a6856b42d216ac5c947a0f665582ddb1df40a76
Liu Hao <lh_mouse@126.com>
2019-01-22 23:57:33 +0800

    Update 'main.cpp'.

--- asteria/src/main.cpp
+++ asteria/src/main.cpp
@@ -4,15 +4,15 @@
 #include "compiler/simple_source_file.hpp"
 #include "runtime/global_context.hpp"
 #include "runtime/traceable_exception.hpp"
+#include "rocket/insertable_istream.hpp"
 #include <iostream>
-#include <sstream>
 #include <chrono>
 
 using namespace Asteria;
 
 int main()
   try {
-    std::istringstream iss(R"__(
+    static constexpr char src[] = R"_____(
       func fib(n) {
         var r = n;
         if!(n <= 1) {
@@ -21,15 +21,17 @@ int main()
         return& r;
       }
       return fib(30);
-    )__");
+    )_____";
+    rocket::insertable_istream iss(rocket::sref(src));
+    std::cerr << "Source code:\n---\n" << src << "\n---\nRunning..." << std::endl;;
     Simple_Source_File code(iss, rocket::sref("my_file"));
     Global_Context global;
     const auto t1 = std::chrono::high_resolution_clock::now();
     auto res = code.execute(global, { });
     const auto t2 = std::chrono::high_resolution_clock::now();
-    std::cout << "Finished in " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms:\n--\n" << res.read() << std::endl;
+    std::cout << "Finished in " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms:\n---\n" << res.read() << std::endl;
   } catch(Traceable_Exception &e) {
-    std::cerr << "Caught `Traceable_Exception`:\n--\n" << e.get_value() << std::endl;
+    std::cerr << "Caught `Traceable_Exception`:\n---\n" << e.get_value() << std::endl;
   } catch(std::exception &e) {
-    std::cerr << "Caught `std::exception`:\n--\n" << e.what() << std::endl;
+    std::cerr << "Caught `std::exception`:\n---\n" << e.what() << std::endl;
   }
