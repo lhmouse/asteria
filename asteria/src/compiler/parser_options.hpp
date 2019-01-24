c41cd84aae8d92d17fec969676959afe5e7a2c41
Liu Hao <lh_mouse@126.com>
2019-01-24 23:16:49 +0800

    Add 'asteria/src/compiler/parser_options.hpp'.

---
+++ asteria/src/compiler/parser_options.hpp
@@ -0,0 +1,45 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2019, LH_Mouse. All wrongs reserved.
+
+#ifndef ASTERIA_COMPILER_PARSER_OPTIONS_HPP_
+#define ASTERIA_COMPILER_PARSER_OPTIONS_HPP_
+
+#include "../fwd.hpp"
+
+namespace Asteria {
+
+enum class Parser_Options : std::uint32_t
+  {
+    none                  =          0,
+    integer_as_real       =  1ul <<  0,  // Tokenize `integer` literals as `real` literals.
+  };
+
+constexpr Parser_Options operator&(Parser_Options lhs, Parser_Options rhs) noexcept
+  {
+    return Parser_Options(std::uint32_t(lhs) & std::uint32_t(rhs));
+  }
+constexpr Parser_Options operator|(Parser_Options lhs, Parser_Options rhs) noexcept
+  {
+    return Parser_Options(std::uint32_t(lhs) | std::uint32_t(rhs));
+  }
+constexpr Parser_Options operator^(Parser_Options lhs, Parser_Options rhs) noexcept
+  {
+    return Parser_Options(std::uint32_t(lhs) ^ std::uint32_t(rhs));
+  }
+
+inline Parser_Options & operator&=(Parser_Options &lhs, Parser_Options rhs) noexcept
+  {
+    return lhs = lhs & rhs;
+  }
+inline Parser_Options & operator|=(Parser_Options &lhs, Parser_Options rhs) noexcept
+  {
+    return lhs = lhs | rhs;
+  }
+inline Parser_Options & operator^=(Parser_Options &lhs, Parser_Options rhs) noexcept
+  {
+    return lhs = lhs ^ rhs;
+  }
+
+}
+
+#endif
