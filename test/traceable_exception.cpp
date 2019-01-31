5d5805a5a606bd5c64264bf8ded2be3e083a1e5b
Liu Hao <lh_mouse@126.com>
2019-01-31 17:14:17 +0800

    Fix CI error.

--- test/traceable_exception.cpp
+++ test/traceable_exception.cpp
@@ -9,14 +9,21 @@ using namespace Asteria;
 int main()
   {
     try {
-      Traceable_Exception except(D_integer(42));
-      except.append_frame(Source_Location(rocket::sref("myfile"), 123), rocket::sref("myfunc"));
+      Traceable_Exception except(D_integer(42), Source_Location(rocket::sref("file1"), 1), rocket::sref("func1"));
+      except.append_frame(Source_Location(rocket::sref("file2"), 2), rocket::sref("func2"));
+      except.append_frame(Source_Location(rocket::sref("file3"), 3), rocket::sref("func3"));
       throw except;
     } catch(Traceable_Exception &e) {
       ASTERIA_TEST_CHECK(e.get_value().check<D_integer>() == 42);
-      ASTERIA_TEST_CHECK(e.get_frame_count() == 1);
-      ASTERIA_TEST_CHECK(e.get_frame(0).source_file() == "myfile");
-      ASTERIA_TEST_CHECK(e.get_frame(0).source_line() == 123);
-      ASTERIA_TEST_CHECK(e.get_frame(0).function_signature() == "myfunc");
+      ASTERIA_TEST_CHECK(e.get_frame_count() == 3);
+      ASTERIA_TEST_CHECK(e.get_frame(0).source_file() ==        "file1");
+      ASTERIA_TEST_CHECK(e.get_frame(0).source_line() ==             1 );
+      ASTERIA_TEST_CHECK(e.get_frame(0).function_signature() == "func1");
+      ASTERIA_TEST_CHECK(e.get_frame(1).source_file() ==        "file2");
+      ASTERIA_TEST_CHECK(e.get_frame(1).source_line() ==             2 );
+      ASTERIA_TEST_CHECK(e.get_frame(1).function_signature() == "func2");
+      ASTERIA_TEST_CHECK(e.get_frame(2).source_file() ==        "file3");
+      ASTERIA_TEST_CHECK(e.get_frame(2).source_line() ==             3 );
+      ASTERIA_TEST_CHECK(e.get_frame(2).function_signature() == "func3");
     }
   }
