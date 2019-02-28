3d4b108eb2d51e9ad7e54e84f4c80c03067939d0
Liu Hao <lh_mouse@126.com>
2019-02-28 16:56:22 +0800

    Add `Air_Node`.

---
+++ asteria/src/runtime/air_node.cpp
@@ -0,0 +1,13 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2019, LH_Mouse. All wrongs reserved.
+
+#include "../precompiled.hpp"
+#include "air_node.hpp"
+
+namespace Asteria {
+
+Air_Node::~Air_Node()
+  {
+  }
+
+}
