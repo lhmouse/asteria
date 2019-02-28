555de524ff5ebbdb4e4287b4e0953e27c076d0e2
Liu Hao <lh_mouse@126.com>
2019-02-28 23:36:16 +0800

    [WIP] Update `Simple_Source_File`.

--- asteria/src/compiler/simple_source_file.cpp
+++ asteria/src/compiler/simple_source_file.cpp
@@ -5,71 +5,82 @@
 #include "simple_source_file.hpp"
 #include "token_stream.hpp"
 #include "parser.hpp"
-#include "../syntax/source_location.hpp"
-#include "../runtime/reference.hpp"
-#include "../runtime/variadic_arguer.hpp"
+#include "../syntax/statement.hpp"
+#include "../runtime/air_node.hpp"
+#include "../runtime/function_analytic_context.hpp"
+#include "../runtime/instantiated_function.hpp"
 #include "../utilities.hpp"
 #include <fstream>
 
 namespace Asteria {
 
-void Simple_Source_File::do_throw_error(const Parser_Error &err)
+Parser_Error Simple_Source_File::do_reload_file(bool throw_on_failure, const Cow_String &filename)
   {
-    rocket::insertable_ostream mos;
-    mos << "An error was encountered while parsing source data: " << Parser_Error::get_code_description(err.code());
-    // Append error location information.
-    mos << "\n[parser error " << err.code() << " encountered at ";
-    if(err.line() == 0) {
-      mos << "the end of stream";
-    } else {
-      mos << "line " << err.line() << ", offset " << err.offset() << ", length " << err.length();
+    std::ifstream ifstrm(filename.c_str());
+    return this->do_reload_stream(throw_on_failure, ifstrm, filename);
+  }
+
+    namespace {
+
+    Parser_Error do_handle_parser_error(bool throw_on_failure, const Parser_Error &error)
+      {
+        if(ROCKET_EXPECT(!throw_on_failure)) {
+          return error;
+        }
+        rocket::insertable_ostream mos;
+        mos << "An error was encountered while parsing source data: " << Parser_Error::get_code_description(error.code());
+        // Append error location information.
+        mos << "\n[parser error " << error.code() << " encountered at ";
+        if(error.line() == 0) {
+          mos << "the end of stream";
+        } else {
+          mos << "line " << error.line() << ", offset " << error.offset() << ", length " << error.length();
+        }
+        mos << "]";
+        // Throw it now.
+        throw_runtime_error(ROCKET_FUNCSIG, mos.extract_string());
+      }
+
     }
-    mos << "]";
-    // Throw it now.
-    throw_runtime_error(ROCKET_FUNCSIG, mos.extract_string());
-  }
 
-Parser_Error Simple_Source_File::load_file(const Cow_String &filename)
-  {
-    std::ifstream ifs(filename.c_str());
-    return this->load_stream(ifs, filename);
-  }
-
-Parser_Error Simple_Source_File::load_stream(std::istream &cstrm_io, const Cow_String &filename)
+Parser_Error Simple_Source_File::do_reload_stream(bool throw_on_failure, std::istream &cstrm_io, const Cow_String &filename)
   {
     // Use default options.
-    static constexpr Parser_Options options;
-    // Tokenize.
+    static constexpr Parser_Options options = { };
+    // Tokenize the character stream.
     Token_Stream tstrm;
     if(!tstrm.load(cstrm_io, filename, options)) {
-      return tstrm.get_parser_error();
+      return do_handle_parser_error(throw_on_failure, tstrm.get_parser_error());
     }
-    // Parse.
+    // Parse tokens.
     Parser parser;
     if(!parser.load(tstrm, options)) {
-      return parser.get_parser_error();
+      return do_handle_parser_error(throw_on_failure, parser.get_parser_error());
     }
-    // Accept the document.
-    this->m_code = parser.get_statements();
-    this->m_file = filename;
+    // Initialize parameters of the top scope.
+    const Source_Location sloc(filename, 0);
+    const Cow_Vector<PreHashed_String> params;
+    // Generate code.
+    Cow_Vector<RefCnt_Object<Air_Node>> code;
+    Function_Analytic_Context ctx(nullptr, params);
+    for(const auto &stmt : parser.get_statements()) {
+      stmt.generate_code(code, ctx);
+    }
+    // Accept the code.
+    this->m_codev.clear();
+    this->m_codev.emplace_back(sloc, rocket::sref("<file scope>"), params, std::move(code));
     return Parser_Error(0, 0, 0, Parser_Error::code_success);
-  }
-
-void Simple_Source_File::clear() noexcept
-  {
-    this->m_code = Cow_Vector<Statement>();
-    this->m_file = rocket::sref("");
   }
 
 Reference Simple_Source_File::execute(const Global_Context &global, Cow_Vector<Reference> &&args) const
   {
-    // Create absent arguments.
-    Rcobj<Variadic_Arguer> zvarg(Variadic_Arguer(Source_Location(this->m_file, 0), rocket::sref("<file scope>")));
-    Cow_Vector<PreHashed_String> params;
-    // Call the function.
-    Reference self;
-    this->m_code.execute_as_function(self, rocket::move(zvarg), rocket::move(params), global, rocket::move(args));
-    return self;
+    // This is initialized to `null`.
+    Reference result;
+    if(ROCKET_EXPECT(!this->m_codev.empty())) {
+      // Execute the instantiated function.
+      this->m_codev.front().invoke(result, global, std::move(args));
+    }
+    return result;
   }
 
 }
