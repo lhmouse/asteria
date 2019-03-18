cd23fa716e6dbf10f7ae6f0d08c1bc76d9173423
Liu Hao <lh_mouse@126.com>
2019-03-18 21:13:50 +0800

    [WIP] Add `std.string.trim()`, `std.string.trim_left()` and `std.string.trim_right()` functions.

--- asteria/src/library/bindings_string.hpp
+++ asteria/src/library/bindings_string.hpp
@@ -9,7 +9,12 @@
 namespace Asteria {
 
 extern int std_string_compare(const Cow_String &text_one, const Cow_String &text_two, std::size_t length);
+
 extern Cow_String std_string_reverse(const Cow_String &text);
+
+extern Cow_String std_string_trim(const Cow_String &text, const Cow_String &reject);
+extern Cow_String std_string_trim_left(const Cow_String &text, const Cow_String &reject);
+extern Cow_String std_string_trim_right(const Cow_String &text, const Cow_String &reject);
 
 // Create an object that is to be referenced as `std.string`.
 extern D_object create_bindings_string();
