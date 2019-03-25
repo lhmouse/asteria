b69a296bc8eceff1cdd10616d1e636d8fa8e763b
Liu Hao <lh_mouse@126.com>
2019-03-26 02:02:17 +0800

    Update test.

--- asteria/test/simple_source_file.cpp
+++ asteria/test/simple_source_file.cpp
@@ -14,10 +14,10 @@ int main()
       var one = 1;
       const two = 2;
       func fib(n) {
-        return n <= one ? one : fib(n - one) + fib(n - two);
+        return n <= one ? n : fib(n - one) + fib(n - two);
       }
       var con = { };
-      con["value"] = fib(10);
+      con["value"] = fib(11);
       con["const"] = one;
       return con.value + con.const;
     )__");
