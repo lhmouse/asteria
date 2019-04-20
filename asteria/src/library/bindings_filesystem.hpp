9948fad40d20fd5138b6a5a127b49a053de51527
Liu Hao <lh_mouse@126.com>
2019-04-20 17:25:26 +0800

    [WIP] Update `std.filesystem.append()`.

--- asteria/src/library/bindings_filesystem.hpp
+++ asteria/src/library/bindings_filesystem.hpp
@@ -15,9 +15,9 @@ extern Opt<G_integer> std_filesystem_directory_create(const G_string& path);
 extern Opt<G_array> std_filesystem_directory_list(const G_string& path);
 extern Opt<G_integer> std_filesystem_directory_remove(const G_string& path);
 
-extern Opt<G_string> std_filesystem_file_read(const G_string& path, const Opt<G_integer>& offset, const Opt<G_integer>& limit);
-extern bool std_filesystem_file_write(const G_string& path, const G_string& data, const Opt<G_integer>& offset);
-extern bool std_filesystem_file_append(const G_string& path, const G_string& data);
+extern Opt<G_string> std_filesystem_file_read(const G_string& path, const Opt<G_integer>& offset = rocket::nullopt, const Opt<G_integer>& limit = rocket::nullopt);
+extern bool std_filesystem_file_write(const G_string& path, const G_string& data, const Opt<G_integer>& offset = rocket::nullopt);
+extern bool std_filesystem_file_append(const G_string& path, const G_string& data, const Opt<G_boolean>& exclusive = rocket::nullopt);
 extern bool std_filesystem_file_remove(const G_string& path);
 
 // Create an object that is to be referenced as `std.filesystem`.
