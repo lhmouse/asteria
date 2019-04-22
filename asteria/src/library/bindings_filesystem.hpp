f063b9ba537cb353d3693728b460154160086286
Liu Hao <lh_mouse@126.com>
2019-04-22 15:29:15 +0800

    [WIP] Update `std.filesystem.directory_list()\`.

--- asteria/src/library/bindings_filesystem.hpp
+++ asteria/src/library/bindings_filesystem.hpp
@@ -15,7 +15,7 @@ extern bool std_filesystem_move_from(const G_string& path_new, const G_string& p
 extern Opt<G_integer> std_filesystem_remove_recursive(const G_string& path);
 
 extern Opt<G_integer> std_filesystem_directory_create(const G_string& path);
-extern Opt<G_array> std_filesystem_directory_list(const G_string& path);
+extern Opt<G_object> std_filesystem_directory_list(const G_string& path);
 extern Opt<G_integer> std_filesystem_directory_remove(const G_string& path);
 
 extern Opt<G_string> std_filesystem_file_read(const G_string& path, const Opt<G_integer>& offset = rocket::nullopt, const Opt<G_integer>& limit = rocket::nullopt);
