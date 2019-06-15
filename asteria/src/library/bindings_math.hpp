dc9abd70aec7bdf53ece3e526c49e677d6bf22a9
Liu Hao <lh_mouse@126.com>
2019-06-16 00:45:16 +0800

    Add trigonometric functions.

--- asteria/src/library/bindings_math.hpp
+++ asteria/src/library/bindings_math.hpp
@@ -14,6 +14,16 @@ extern G_real std_math_pow(const G_real& x, const G_real& y);
 extern G_real std_math_log(const G_real& x, const Opt<G_real>& base);
 extern G_real std_math_log1p(const G_real& x);
 
+extern G_real std_math_sin(const G_real& x);
+extern G_real std_math_cos(const G_real& x);
+extern G_real std_math_tan(const G_real& x);
+extern G_real std_math_asin(const G_real& x);
+extern G_real std_math_acos(const G_real& x);
+extern G_real std_math_atan(const G_real& x);
+
+extern G_real std_math_atan2(const G_real& y, const G_real& x);
+extern G_real std_math_hypot(const Cow_Vector<Value>& values);
+
 // Create an object that is to be referenced as `std.math`.
 extern void create_bindings_math(G_object& result, API_Version version);
 
