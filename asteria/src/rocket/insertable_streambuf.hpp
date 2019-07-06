740963827e5d55b091d9e78a384421945e9fdc4e
Liu Hao <lh_mouse@126.com>
2019-07-06 21:18:25 +0800

    Cleanup.

--- asteria/src/rocket/insertable_streambuf.hpp
+++ asteria/src/rocket/insertable_streambuf.hpp
@@ -63,115 +63,109 @@ template<typename charT, typename traitsT, typename allocatorT> class basic_inse
 
     streamsize showmanyc() override
       {
-        if(this->m_which & ios_base::in) {
-          // Return the number of characters inside the internal buffer, minus those that have been read from it.
-          auto ntotal = this->m_str.size();
-          // N.B. This will yield the correct result (zero) even when both pointers are null.
-          auto ngot = static_cast<size_type>(this->gptr() - this->eback());
-          return static_cast<streamsize>(ntotal - ngot);
-        } else {
+        if(!(this->m_which & ios_base::in)) {
           // Non-input stream buffers can't be read from.
           return 0;
         }
+        // Return the number of characters inside the internal buffer, minus those that have been read from it.
+        auto ntotal = this->m_str.size();
+        // N.B. This will yield the correct result (zero) even when both pointers are null.
+        auto ngot = static_cast<size_type>(this->gptr() - this->eback());
+        return static_cast<streamsize>(ntotal - ngot);
       }
     streamsize xsgetn(char_type* s, streamsize n) override
       {
-        if(this->m_which & ios_base::in) {
-          // Tidy the get area.
-          this->basic_insertable_streambuf::sync();
-          // Read and discard characters from the internal buffer directly.
-          auto ngot = (static_cast<unsigned long long>(n) <= this->m_str.size()) ? static_cast<size_type>(n) : this->m_str.size();
-          traits_type::copy(s, this->m_str.data(), ngot);
-          return static_cast<streamsize>(ngot);
-        } else {
+        if(!(this->m_which & ios_base::in)) {
           // Non-input stream buffers can't be read from.
           return 0;
         }
+        // Tidy the get area.
+        this->sync();
+        // Read and discard characters from the internal buffer directly.
+        auto ngot = (static_cast<unsigned long long>(n) <= this->m_str.size()) ? static_cast<size_type>(n) : this->m_str.size();
+        traits_type::copy(s, this->m_str.data(), ngot);
+        return static_cast<streamsize>(ngot);
       }
     int_type underflow() override
       {
-        if(this->m_which & ios_base::in) {
-          // Tidy the get area.
-          this->basic_insertable_streambuf::sync();
-          // Set the get area to the entire string.
-          if(this->m_str.empty()) {
-              return traits_type::eof();
-          }
-          auto gp = this->m_str.mut_data();
-          this->setg(gp, gp, gp + this->m_str.size());
-          return traits_type::to_int_type(*gp);
-        } else {
+        if(!(this->m_which & ios_base::in)) {
           // Non-input stream buffers can't be read from.
           return traits_type::eof();
         }
+        // Tidy the get area.
+        this->sync();
+        // Set the get area to the entire string.
+        if(this->m_str.empty()) {
+          return traits_type::eof();
+        }
+        auto gp = this->m_str.mut_data();
+        this->setg(gp, gp, gp + this->m_str.size());
+        return traits_type::to_int_type(*gp);
       }
 
     int_type pbackfail(int_type c) override
       {
-        if(this->m_which & ios_base::out) {
-          if(traits_type::eq_int_type(c, traits_type::eof())) {
-            return traits_type::eof();
-          }
-          // Tidy the get area, as the internal buffer is subject to reallocation.
-          this->basic_insertable_streambuf::sync();
-          // Put the character provided back into the front of the internal buffer. This is not meant to be efficient anyway.
-          this->m_str.insert(0, 1, traits_type::to_char_type(c));
-          return c;
-        } else {
+        if(!(this->m_which & ios_base::out)) {
           // Non-output stream buffers can't be written to.
           return traits_type::eof();
         }
+        if(traits_type::eq_int_type(c, traits_type::eof())) {
+          return traits_type::eof();
+        }
+        // Tidy the get area, as the internal buffer is subject to reallocation.
+        this->sync();
+        // Put the character provided back into the front of the internal buffer. This is not meant to be efficient anyway.
+        this->m_str.insert(0, 1, traits_type::to_char_type(c));
+        return c;
       }
 
     streamsize xsputn(const char_type* s, streamsize n) override
       {
-        if(this->m_which & ios_base::out) {
-          // Tidy the get area, as the internal buffer is subject to reallocation.
-          this->basic_insertable_streambuf::sync();
-          // Write the string provided as a single operation.
-          auto nput = (static_cast<unsigned long long>(n) <= this->m_str.max_size()) ? static_cast<size_type>(n) : this->m_str.max_size();
-          if(this->m_caret == npos) {
-            // Append the string provided to the internal buffer.
-            this->m_str.insert(this->m_str.size(), s, nput);
-          } else {
-            // Insert the string provided at the specified position of the internal buffer.
-            this->m_str.insert(this->m_caret, s, nput);
-            this->m_caret += nput;
-          }
-          return static_cast<streamsize>(nput);
-        } else {
+        if(!(this->m_which & ios_base::out)) {
           // Non-output stream buffers can't be written to.
           return 0;
         }
+        // Tidy the get area, as the internal buffer is subject to reallocation.
+        this->sync();
+        // Write the string provided as a single operation.
+        auto nput = (static_cast<unsigned long long>(n) <= this->m_str.max_size()) ? static_cast<size_type>(n) : this->m_str.max_size();
+        if(this->m_caret == npos) {
+          // Append the string provided to the internal buffer.
+          this->m_str.insert(this->m_str.size(), s, nput);
+        } else {
+          // Insert the string provided at the specified position of the internal buffer.
+          this->m_str.insert(this->m_caret, s, nput);
+          this->m_caret += nput;
+        }
+        return static_cast<streamsize>(nput);
       }
     int_type overflow(int_type c) override
       {
-        if(this->m_which & ios_base::out) {
-          if(traits_type::eq_int_type(c, traits_type::eof())) {
-            return traits_type::not_eof(c);
-          }
-          // Tidy the get area, as the internal buffer is subject to reallocation.
-          this->basic_insertable_streambuf::sync();
-          // Write the character provided as a single operation.
-          if(this->m_caret == npos) {
-            // Append the character provided to the internal buffer.
-            this->m_str.push_back(traits_type::to_char_type(c));
-          } else {
-            // Insert the character provided at the specified position of the internal buffer.
-            this->m_str.insert(this->m_caret, static_cast<size_type>(1), traits_type::to_char_type(c));
-            this->m_caret++;
-          }
-          return c;
-        } else {
+        if(!(this->m_which & ios_base::out)) {
           // Non-output stream buffers can't be written to.
           return traits_type::eof();
         }
+        if(traits_type::eq_int_type(c, traits_type::eof())) {
+          return traits_type::not_eof(c);
+        }
+        // Tidy the get area, as the internal buffer is subject to reallocation.
+        this->sync();
+        // Write the character provided as a single operation.
+        if(this->m_caret == npos) {
+          // Append the character provided to the internal buffer.
+          this->m_str.push_back(traits_type::to_char_type(c));
+        } else {
+          // Insert the character provided at the specified position of the internal buffer.
+          this->m_str.insert(this->m_caret, static_cast<size_type>(1), traits_type::to_char_type(c));
+          this->m_caret++;
+        }
+        return c;
       }
 
   public:
     string_type& get_string()
       {
-        this->basic_insertable_streambuf::sync();
+        this->sync();
         return this->m_str;
       }
     size_type get_caret() const noexcept
@@ -180,7 +174,7 @@ template<typename charT, typename traitsT, typename allocatorT> class basic_inse
       }
     void set_string(string_type str, size_type caret = npos)
       {
-        this->basic_insertable_streambuf::sync();
+        this->sync();
         this->m_str = noadl::move(str);
         this->m_caret = caret;
       }
@@ -191,7 +185,7 @@ template<typename charT, typename traitsT, typename allocatorT> class basic_inse
     string_type extract_string()
       {
         string_type str;
-        this->basic_insertable_streambuf::sync();
+        this->sync();
         this->m_str.swap(str);
         this->m_caret = npos;
         return str;
