396acd9b0df8e0108f2ad3fae2d01dea030459f7
Liu Hao <lh_mouse@126.com>
2019-07-17 12:07:17 +0800

    Minor.

--- asteria/src/runtime/instantiated_function.cpp
+++ asteria/src/runtime/instantiated_function.cpp
@@ -32,13 +32,13 @@ Reference& Instantiated_Function::invoke(Reference& self, const Global_Context& 
                  [&](const Air_Node& node) { return (status = node.execute(stack, ctx_func,
                                                                            this->m_zvarg->get_function_signature(), global))
                                                     != Air_Node::status_next;  });
-    if(status == Air_Node::status_next) {
-      // Return `null` if the control flow reached the end of the function.
-      self = Reference_Root::S_null();
-    }
-    else if(status == Air_Node::status_return){
+    if(status == Air_Node::status_return){
       // Return the reference at the top of `stack`.
       self = rocket::move(stack.open_top_reference());
+    }
+    else if(status == Air_Node::status_next) {
+      // Return `null` if the control flow reached the end of the function.
+      self = Reference_Root::S_null();
     }
     else {
       ASTERIA_THROW_RUNTIME_ERROR("An invalid status code `", status, "` was returned from a function. This is likely a bug. Please report.");
