8002c366f01eddbed956296e0f857f36f1418a94
Liu Hao <lh_mouse@126.com>
2019-07-28 21:46:56 +0800

    Minor.

--- asteria/src/llds/air_queue.cpp
+++ asteria/src/llds/air_queue.cpp
@@ -10,8 +10,12 @@ namespace Asteria {
 void Air_Queue::do_clear_nodes() const noexcept
   {
     auto next = this->m_stor.head;
-    while(ROCKET_EXPECT(next)) {
-      auto qnode = std::exchange(next, next->m_xt);
+    for(;;) {
+      auto qnode = next;
+      if(ROCKET_UNEXPECT(!qnode)) {
+        break;
+      }
+      next = qnode->m_xt;
       // Destroy and deallocate the node.
       delete qnode;
     }
@@ -20,18 +24,29 @@ void Air_Queue::do_clear_nodes() const noexcept
 void Air_Queue::execute(Air_Node::Status& status, Executive_Context& ctx) const
   {
     auto next = this->m_stor.head;
-    while(ROCKET_EXPECT(next && !status)) {
-      auto qnode = std::exchange(next, next->m_xt);
+    for(;;) {
+      auto qnode = next;
+      if(ROCKET_UNEXPECT(!qnode)) {
+        break;
+      }
+      next = qnode->m_xt;
       // Execute this node and return any status code unexpected to the caller verbatim.
       status = qnode->execute(ctx);
+      if(ROCKET_UNEXPECT(status != Air_Node::status_next)) {
+        return;
+      }
     }
   }
 
 void Air_Queue::enumerate_variables(const Abstract_Variable_Callback& callback) const
   {
     auto next = this->m_stor.head;
-    while(ROCKET_EXPECT(next)) {
-      auto qnode = std::exchange(next, next->m_xt);
+    for(;;) {
+      auto qnode = next;
+      if(ROCKET_UNEXPECT(!qnode)) {
+        break;
+      }
+      next = qnode->m_xt;
       // Enumerate varables in this node recursively.
       qnode->enumerate_variables(callback);
     }
