e4760decf71021f53ee2c90c6f8cb78c6c526d7b
Liu Hao <lh_mouse@126.com>
2019-08-25 22:32:24 +0800

    Cleanup.

--- asteria/rocket/assert.cpp
+++ asteria/rocket/assert.cpp
@@ -7,7 +7,7 @@
 
 namespace rocket {
 
-bool report_assertion_failure(const char* expr, const char* file, long line, const char* msg) noexcept
+void report_assertion_failure(const char* expr, const char* file, long line, const char* msg) noexcept
   {
     // Write a message to the standard error stream.
     ::std::fprintf(stderr,
--- asteria/rocket/assert.hpp
+++ asteria/rocket/assert.hpp
@@ -8,29 +8,19 @@
 
 namespace rocket {
 
-    namespace details_assert {
-
-    [[noreturn]] ROCKET_ARTIFICIAL_FUNCTION inline bool unreachable() noexcept
-      {
-        ROCKET_UNREACHABLE();
-      }
-
-    }  // namespace details_assert
-
 // `report_assertion_failure()` is always provided even when assertions are disabled.
-[[noreturn]] extern bool report_assertion_failure(const char* expr, const char* file, long line, const char* msg) noexcept;
+[[noreturn]] extern void report_assertion_failure(const char* expr, const char* file, long line, const char* msg) noexcept;
 
 }  // namespace rocket
 
 #ifdef ROCKET_DEBUG
-#  define ROCKET_DETAILS_FAIL_ASSERTION_(...)    ::rocket::report_assertion_failure(__VA_ARGS__)
+#  define ROCKET_XASSERT_FAIL_(...)        ::rocket::report_assertion_failure(__VA_ARGS__)
 #else
-#  define ROCKET_DETAILS_FAIL_ASSERTION_(...)    ::rocket::details_assert::unreachable()
+#  define ROCKET_XASSERT_FAIL_(...)        ROCKET_UNREACHABLE()
 #endif
+#define ROCKET_XASSERT_(cond_, str_, m_)   ((cond_) ? void(0) : ROCKET_XASSERT_FAIL_(str_, __FILE__, __LINE__, m_))
 
-#define ROCKET_DETAILS_ASSERT_(pred_, xstr_, msg_)    (void(bool(pred_) || ROCKET_DETAILS_FAIL_ASSERTION_(xstr_, __FILE__, __LINE__, msg_)))
-
-#define ROCKET_ASSERT(expr_)              ROCKET_DETAILS_ASSERT_(expr_, #expr_, "")
-#define ROCKET_ASSERT_MSG(expr_, m_)      ROCKET_DETAILS_ASSERT_(expr_, #expr_, m_)
+#define ROCKET_ASSERT(expr_)               ROCKET_XASSERT_(expr_, #expr_, "")
+#define ROCKET_ASSERT_MSG(expr_, m_)       ROCKET_XASSERT_(expr_, #expr_, m_)
 
 #endif
