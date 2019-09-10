cbd3778e7df813764324e41897448c824f452e3f
Liu Hao <lh_mouse@126.com>
2019-09-10 22:47:01 +0800

    Add static assertions.

--- asteria/src/runtime/exception.cpp
+++ asteria/src/runtime/exception.cpp
@@ -10,4 +10,9 @@ Exception::~Exception()
   {
   }
 
+static_assert(std::is_nothrow_copy_constructible<Exception>::value, "Copy constructors of exceptions are not allow to throw exceptions.");
+static_assert(std::is_nothrow_move_constructible<Exception>::value, "Move constructors of exceptions are not allow to throw exceptions.");
+static_assert(std::is_nothrow_copy_assignable<Exception>::value, "Copy assignment operators of exceptions are not allow to throw exceptions.");
+static_assert(std::is_nothrow_move_assignable<Exception>::value, "Move assignment operators of exceptions are not allow to throw exceptions.");
+
 }  // namespace Asteria
