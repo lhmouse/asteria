fbbbb16d01cafb18f72a054446aa30214b3a8798
Liu Hao <lh_mouse@126.com>
2019-11-21 20:34:09 +0800

    Use `-fsyntax-only`.
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- check_includes.sh
+++ check_includes.sh
@@ -5,7 +5,7 @@ export CXX
 
 function check_one()
   {
-    _cmd="${CXX} -std=gnu++14 -x c++ -S -o /dev/null"
+    _cmd="${CXX} -std=gnu++14 -x c++ -fsyntax-only"
     echo "Checking \`#include\` directives:  ${_cmd}  \"$1\""
     ${_cmd}  "$1"
   }
