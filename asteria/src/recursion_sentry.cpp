9dc9ef7c4d8682d6b29bd8c6d62cf3a7645cb141
Liu Hao <lh_mouse@126.com>
2019-12-17 20:01:59 +0800

    *: Add `Recursion_Sentry`

---
+++ asteria/src/recursion_sentry.cpp
@@ -0,0 +1,15 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2019, LH_Mouse. All wrongs reserved.
+
+#include "precompiled.hpp"
+#include "recursion_sentry.hpp"
+#include "utilities.hpp"
+
+namespace Asteria {
+
+void Recursion_Sentry::do_throw_stack_overflow(size_t usage, size_t limit)
+  {
+    ASTERIA_THROW("stack overflow averted (stack usage `$1` exceeded `$2`)", usage, limit);
+  }
+
+}  // namespace Asteria
