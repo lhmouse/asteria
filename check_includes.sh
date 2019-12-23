dfacf6ad5a8ad938949d3bca1d77b142ec69ce17
Liu Hao <lh_mouse@126.com>
2019-12-23 23:01:21 +0800

    ci: Fix AppVeyor error

--- check_includes.sh
+++ check_includes.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 
 test -n "${CXX}" || CXX=g++
+CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -D_WIN32_WINNT=0x0600"
+CXXFLAGS="-std=gnu++14"
+
 export CXX
+export CPPFLAGS
+export CXXFLAGS
 
 function check_one()
   {
-    _cmd="${CXX} -std=gnu++14 -x c++ -fsyntax-only -I."
+    _cmd="${CXX} ${CPPFLAGS} ${CXXFLAGS} -x c++ -fsyntax-only -I."
     echo "Checking \`#include\` directives:  ${_cmd}  \"$1\""
     ${_cmd}  "$1"
   }
