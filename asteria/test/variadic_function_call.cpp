2e2a7b3384931d2b72f94132706b24d7c22aff2d
Liu Hao <lh_mouse@126.com>
2020-01-09 17:44:15 +0800

    test/variadic_function_call: New test

---
+++ asteria/test/variadic_function_call.cpp
@@ -0,0 +1,58 @@
+// This file is part of Asteria.
+// Copyleft 2018, LH_Mouse. All wrongs reserved.
+
+#include "test_utilities.hpp"
+#include "../src/runtime/simple_script.hpp"
+#include "../src/runtime/global_context.hpp"
+
+using namespace Asteria;
+
+int main()
+  {
+    ::rocket::tinybuf_str cbuf;
+    cbuf.set_string(::rocket::sref(
+      R"__(
+///////////////////////////////////////////////////////////////////////////////
+
+        var num = 4;
+
+        var obj = {
+          value = "hello";
+
+          do_imuladd = func(x, y, z) {
+            assert this.value == "hello";  // member function
+
+            x = x * y + z;
+            return "ifma success";
+          };
+
+          generator = func(x) {
+            assert this.value == "hello";  // member function
+
+            if(x == null)
+              return 3;     // num of args := 3
+            if(x == 0)
+              return& num;  // arg 0 := the variable
+            if(x == 1)
+              return 2;     // arg 1 := 2
+            if(x == 2)
+              return 5;     // arg 2 := 5
+
+            assert false;  // unreachable
+          };
+        };
+
+        assert __vcall(obj.do_imuladd, obj.generator) == "ifma success";
+        assert num == 13;  // 4 * 2 + 5
+        assert __vcall(obj.do_imuladd, obj.generator) == "ifma success";
+        assert num == 31;  // 13 * 2 + 5
+        assert __vcall(obj.do_imuladd, obj.generator) == "ifma success";
+        assert num == 67;  // 31 * 2 + 5
+
+///////////////////////////////////////////////////////////////////////////////
+      )__"), tinybuf::open_read);
+
+    Simple_Script code(cbuf, ::rocket::sref(__FILE__));
+    Global_Context global;
+    code.execute(global);
+  }
