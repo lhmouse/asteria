2b6d50cecb1cd9b37af37e65b352b2c599b51b5d
Liu Hao <lh_mouse@126.com>
2020-01-18 15:35:24 +0800

    library/array: Fix `this` in callbacks

--- asteria/src/library/bindings_array.cpp
+++ asteria/src/library/bindings_array.cpp
@@ -84,7 +84,7 @@ template<typename IteratorT> opt<IteratorT> do_find_if_opt(Global& global, Itera
       cow_vector<Reference> args;
       do_push_argument(args, *it);
       // Call the predictor function and check the return value.
-      Reference self = Reference_Root::S_void();
+      Reference self = Reference_Root::S_constant();
       pred->invoke(self, global, ::rocket::move(args));
       self.finish_call(global);
       if(self.read().test() == match)
@@ -101,7 +101,7 @@ Compare do_compare(Global& global, const Fval& comp, const Value& lhs, const Val
     do_push_argument(args, lhs);
     do_push_argument(args, rhs);
     // Call the predictor function and compare the result with `0`.
-    Reference self = Reference_Root::S_void();
+    Reference self = Reference_Root::S_constant();
     comp->invoke(self, global, ::rocket::move(args));
     self.finish_call(global);
     return self.read().compare(Ival(0));
@@ -736,7 +736,7 @@ Aval std_array_generate(Global& global, Fval generator, Ival length)
       do_push_argument(args, Ival(i));
       do_push_argument(args, res.empty() ? null_value : res.back());
       // Call the generator function and push the return value.
-      Reference self = Reference_Root::S_void();
+      Reference self = Reference_Root::S_constant();
       generator->invoke(self, global, ::rocket::move(args));
       self.finish_call(global);
       res.emplace_back(self.read());
