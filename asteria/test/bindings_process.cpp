c4359380214c09403039914439ebfad10775f3c9
Liu Hao <lh_mouse@126.com>
2020-01-30 23:17:45 +0800

    test/process: New test

---
+++ asteria/test/bindings_process.cpp
@@ -0,0 +1,40 @@
+// This file is part of Asteria.
+// Copyleft 2018, LH_Mouse. All wrongs reserved.
+
+#include "test_utilities.hpp"
+#include "../src/runtime/simple_script.hpp"
+#include "../src/runtime/global_context.hpp"
+
+using namespace Asteria;
+
+int main()
+  {
+    ::rocket::tinybuf_str cbuf;
+    cbuf.set_string(::rocket::sref(
+      R"__(
+        assert std.process.execute('/bin/true') == 0;
+        assert std.process.execute('/bin/false') != 0;
+
+        try {
+          std.process.execute('nonexistent-command');
+          assert false;
+        }
+        catch(e)
+          ;
+
+        assert std.process.execute('/bin/bash',
+          [ '/bin/bash', '-c', 'kill -4 $$' ]) == 132;
+        assert std.process.execute('/bin/bash',
+          [ '/bin/bash', '-c', 'kill -9 $$' ]) == 137;
+
+        assert std.process.execute('/bin/bash',
+          [ '/bin/bash', '-c', 'test $VAR == yes' ], [ 'VAR=yes' ]) == 0;
+        assert std.process.execute('/bin/bash',
+          [ '/bin/bash', '-c', 'test $VAR == yes' ], [ 'VAR=no' ]) != 0;
+
+      )__"), tinybuf::open_read);
+
+    Simple_Script code(cbuf, ::rocket::sref(__FILE__));
+    Global_Context global;
+    code.execute(global);
+  }
