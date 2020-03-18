b4cb44527ec5735f2b36cbc736ed2d162d2c5828
Liu Hao <lh_mouse@126.com>
2020-03-18 13:45:01 +0800

    ci: Update 'check_includes.sh'

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -1,18 +1,14 @@
 #!/bin/bash -e
 
-test -n "${CXX}" || CXX=g++
-CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -D_WIN32_WINNT=0x0600"
-CXXFLAGS="-std=gnu++14"
-
-export CXX
-export CPPFLAGS
-export CXXFLAGS
+export CXX=${CXX:-"g++"}
+export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -D_WIN32_WINNT=0x0600"
+export CXXFLAGS="-std=gnu++14"
 
 _sem="parallel --will-cite --semaphore"
 
 for _file in $(find -L "asteria" -name "*.[hc]pp")
 do
-  _cmd="${CXX} ${CPPFLAGS} ${CXXFLAGS} -x c++ -fsyntax-only -I."
+  _cmd="${CXX} ${CPPFLAGS} ${CXXFLAGS} -x c++ -fsyntax-only -DHAVE_CONFIG_H -I."
   echo "Checking \`#include\` directives:  ${_cmd}  \"${_file}\""
   ${_sem} -j+0 -- ${_cmd}  "${_file}"
 done
