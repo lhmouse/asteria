ce58fb3984f2b393253d9a6a5ad4cd53d0924441
Liu Hao <lh_mouse@126.com>
2020-04-08 00:54:22 +0800

    rocket/unique_handle: Reformat

--- asteria/rocket/unique_handle.hpp
+++ asteria/rocket/unique_handle.hpp
@@ -145,14 +145,12 @@ template<typename handleT, typename closerT> constexpr bool operator>=(
                const unique_handle<handleT, closerT>& lhs, const unique_handle<handleT, closerT>& rhs)
   { return lhs.get() >= rhs.get();  }
 
-template<typename handleT, typename closerT>
-    inline void swap(unique_handle<handleT, closerT>& lhs,
-                     unique_handle<handleT, closerT>& rhs) noexcept(noexcept(lhs.swap(rhs)))
+template<typename handleT, typename closerT> inline void swap(
+               unique_handle<handleT, closerT>& lhs, unique_handle<handleT, closerT>& rhs) noexcept(noexcept(lhs.swap(rhs)))
   { lhs.swap(rhs);  }
 
-template<typename charT, typename traitsT, typename handleT, typename closerT>
-    inline basic_tinyfmt<charT, traitsT>& operator<<(basic_tinyfmt<charT, traitsT>& fmt,
-                                                     const unique_handle<handleT, closerT>& rhs)
+template<typename charT, typename traitsT, typename handleT, typename closerT> inline basic_tinyfmt<charT, traitsT>& operator<<(
+               basic_tinyfmt<charT, traitsT>& fmt, const unique_handle<handleT, closerT>& rhs)
   { return fmt << rhs.get();  }
 
 }  // namespace rocket
