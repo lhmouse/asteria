593575a44bea6c4d8b257eefd76a35f4d0bbfb1a
Liu Hao <lh_mouse@126.com>
2020-04-26 16:20:42 +0800

    build: Update DEB packaging scripts

--- makedeb.sh
+++ makedeb.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -e
 
 _pkgname="asteria"
-_maintainer="$(git config --get user.email)"
-_pkgversion="$(git describe 2>/dev/null ||
-               printf "0.%u.%s" "$(git rev-list --count HEAD)"  \
-                                "$(git rev-parse --short HEAD)")"
+_pkggroup="asteria"
+_pkgsource="https://github.com/lhmouse/asteria"
+_pkglicense="BSD 3-Clause License"
 
-sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no  \
-  --addso=yes --pkgname="${_pkgname}" --maintainer="${_maintainer}"  \
-  --pkgversion="${_pkgversion}"
+_pkgversion=$(git describe 2>/dev/null ||
+              printf "0.%u.%s" "$(git rev-list --count HEAD)"  \
+                               "$(git rev-parse --short HEAD)")
+
+_maintainer=$(printf "%s <%s>" "$(git config --get user.name)"  \
+                               "$(git config --get user.email)")
+
+sudo ./ci/checkinstall  \
+  --backup=no --nodoc --default --strip=no --stripso=no --addso=yes  \
+  --pkgname="${_pkgname}"  \
+  --pkggroup="${_pkggroup}"  \
+  --pkgsource="${_pkgsource}"  \
+  --pkglicense="${_pkglicense}"  \
+  --pkgversion="${_pkgversion}"  \
+  --maintainer="${_maintainer}"  \
+  --exclude="${_tmpdir}"
