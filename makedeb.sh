33bf87442e528833461e36471506510085d00f78
Liu Hao <lh_mouse@126.com>
2020-04-26 11:28:49 +0800

    build: Reformat 'makedeb'

--- makedeb.sh
+++ makedeb.sh
@@ -2,7 +2,10 @@
 
 _pkgname="asteria"
 _maintainer="$(git config --get user.email)"
-_pkgversion="$(git describe 2>/dev/null || printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")"
+_pkgversion="$(git describe 2>/dev/null ||
+               printf "0.%u.%s" "$(git rev-list --count HEAD)"  \
+                                "$(git rev-parse --short HEAD)")"
 
-sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --addso=yes  \
-  --pkgname="${_pkgname}" --maintainer="${_maintainer}" --pkgversion="${_pkgversion}"
+sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no  \
+  --addso=yes --pkgname="${_pkgname}" --maintainer="${_maintainer}"  \
+  --pkgversion="${_pkgversion}"
