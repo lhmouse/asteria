11ed074f0812922433b596d3f526b02d0de49921
Liu Hao <lh_mouse@126.com>
2020-05-06 23:44:41 +0800

    library/json: Reformat

--- asteria/src/library/json.cpp
+++ asteria/src/library/json.cpp
@@ -160,10 +160,10 @@ do_quote_string(tinyfmt& fmt, const cow_string& str)
     while(offset < str.size()) {
       // Convert UTF-8 to UTF-16.
       char32_t cp;
-      if(!utf8_decode(cp, str, offset)) {
+      if(!utf8_decode(cp, str, offset))
         // Invalid UTF-8 code units are replaced with the replacement character.
         cp = 0xFFFD;
-      }
+
       // Escape double quotes, backslashes, and control characters.
       switch(cp) {
         case '\"':
@@ -200,10 +200,12 @@ do_quote_string(tinyfmt& fmt, const cow_string& str)
             fmt << static_cast<char>(cp);
             break;
           }
+
           // Encode the character in UTF-16.
           char16_t ustr[2];
           char16_t* epos = ustr;
           utf16_encode(epos, cp);
+
           // Write code units.
           ::rocket::ascii_numput nump;
           for(auto p = ustr;  p != epos;  ++p) {
@@ -254,34 +256,26 @@ do_format_scalar(tinyfmt& fmt, const Value& value, bool json5)
         // Write the integer in decimal.
         return fmt << V_real(value.as_integer());
 
-      case vtype_real: {
-        // Is the value finite?
-        switch(::std::fpclassify(value.as_real())) {
-          case FP_INFINITE:
-            if(!json5)
-              break;
-            // JSON5 allows `Infinity` in ECMAScript form.
-            return fmt << "Infinity";
+      case vtype_real:
+        if(::std::isinf(value.as_real()))
+          // JSON5 allows `Infinity` in ECMAScript form.
+          return fmt << (json5 ? "Infinity" : "null");
 
-          case FP_NAN:
-            if(!json5)
-              break;
-            // JSON5 allows `NaN` in ECMAScript form.
-            return fmt << "NaN";
+        if(::std::isnan(value.as_real()))
+          // JSON5 allows `NaN` in ECMAScript form.
+          return fmt << (json5 ? "NaN" : "null");
 
-          default:
-            // Write the real in decimal.
-            return fmt << value.as_real();
-        }
-        break;
-      }
+        // Write the real in decimal.
+        return fmt << value.as_real();
 
       case vtype_string:
         // Write the quoted string.
         return do_quote_string(fmt, value.as_string());
+
+      default:
+        // Anything else is censored to `null`.
+        return fmt << "null";
     }
-    // Anything else is censored to `null`.
-    return fmt << "null";
   }
 
 struct S_xformat_array
@@ -302,13 +296,16 @@ V_string
 do_format_nonrecursive(const Value& value, bool json5, Indenter& indent)
   {
     ::rocket::tinyfmt_str fmt;
+
     // Transform recursion to iteration using a handwritten stack.
     auto qvalue = ::std::addressof(value);
     cow_vector<Xformat> stack;
+
     for(;;) {
       // Find a leaf value. `qvalue` must always point to a valid value here.
       if(qvalue->is_array()){
         const auto& array = qvalue->as_array();
+
         // Open an array.
         fmt << '[';
         auto curp = array.begin();
@@ -316,17 +313,20 @@ do_format_nonrecursive(const Value& value, bool json5, Indenter& indent)
           // Indent the body.
           indent.increment_level();
           indent.break_line(fmt);
+
           // Decend into the array.
           S_xformat_array ctxa = { ::rocket::ref(array), curp };
           stack.emplace_back(::std::move(ctxa));
           qvalue = ::std::addressof(*curp);
           continue;
         }
+
         // Write an empty array.
         fmt << ']';
       }
       else if(qvalue->is_object()) {
         const auto& object = qvalue->as_object();
+
         // Open an object.
         fmt << '{';
         auto curp = do_find_uncensored(object, object.begin());
@@ -334,76 +334,86 @@ do_format_nonrecursive(const Value& value, bool json5, Indenter& indent)
           // Indent the body.
           indent.increment_level();
           indent.break_line(fmt);
+
           // Write the key followed by a colon.
           do_quote_object_key(fmt, json5, curp->first);
           fmt << ':';
           if(indent.has_indention())
             fmt << ' ';
+
           // Decend into the object.
           S_xformat_object ctxo = { ::rocket::ref(object), curp };
           stack.emplace_back(::std::move(ctxo));
           qvalue = ::std::addressof(curp->second);
           continue;
         }
+
         // Write an empty object.
         fmt << '}';
       }
-      else {
+      else
         // Just write a scalar value which is never recursive.
         do_format_scalar(fmt, *qvalue, json5);
-      }
+
+      // A complete value has been written. Advance to the next element if any.
       for(;;) {
-        // Advance to the next element if any.
-        if(stack.empty()) {
+        if(stack.empty())
           // Finish the root value.
           return fmt.extract_string();
-        }
+
         if(stack.back().index() == 0) {
           auto& ctxa = stack.mut_back().as<0>();
+
           // Advance to the next element.
           auto curp = ++(ctxa.curp);
           if(curp != ctxa.refa->end()) {
             // Add a comma between elements.
             fmt << ',';
             indent.break_line(fmt);
+
             // Format the next element.
             ctxa.curp = curp;
             qvalue = ::std::addressof(*curp);
             break;
           }
-          // Add a trailing comma.
           if(json5 && indent.has_indention())
             fmt << ',';
+
           // Unindent the body.
           indent.decrement_level();
           indent.break_line(fmt);
+
           // Finish this array.
           fmt << ']';
         }
         else {
           auto& ctxo = stack.mut_back().as<1>();
+
           // Advance to the next key-value pair.
           auto curp = do_find_uncensored(ctxo.refo, ++(ctxo.curp));
           if(curp != ctxo.refo->end()) {
             // Add a comma between elements.
             fmt << ',';
             indent.break_line(fmt);
+
             // Write the key followed by a colon.
             do_quote_object_key(fmt, json5, curp->first);
             fmt << ':';
             if(indent.has_indention())
               fmt << ' ';
+
             // Format the next value.
             ctxo.curp = curp;
             qvalue = ::std::addressof(curp->second);
             break;
           }
-          // Add a trailing comma.
           if(json5 && indent.has_indention())
             fmt << ',';
+
           // Unindent the body.
           indent.decrement_level();
           indent.break_line(fmt);
+
           // Finish this array.
           fmt << '}';
         }
@@ -422,83 +432,86 @@ optV_string
 do_accept_identifier_opt(Token_Stream& tstrm, initializer_list<const char*> accept)
   {
     auto qtok = tstrm.peek_opt();
-    if(!qtok) {
+    if(!qtok)
       return nullopt;
-    }
-    if(qtok->is_identifier()) {
-      auto name = qtok->as_identifier();
-      if(::rocket::is_any_of(name, accept)) {
-        tstrm.shift();
-        // A match has been found.
-        return ::std::move(name);
-      }
-    }
-    return nullopt;
+
+    if(!qtok->is_identifier())
+      return nullopt;
+
+    auto name = qtok->as_identifier();
+    if(::rocket::is_none_of(name, accept))
+      return nullopt;
+
+    // A match has been found.
+    tstrm.shift();
+    return ::std::move(name);
   }
 
 opt<Punctuator>
 do_accept_punctuator_opt(Token_Stream& tstrm, initializer_list<Punctuator> accept)
   {
     auto qtok = tstrm.peek_opt();
-    if(!qtok) {
+    if(!qtok)
       return nullopt;
-    }
-    if(qtok->is_punctuator()) {
-      auto punct = qtok->as_punctuator();
-      if(::rocket::is_any_of(punct, accept)) {
-        tstrm.shift();
-        // A match has been found.
-        return punct;
-      }
-    }
-    return nullopt;
+
+    if(!qtok->is_punctuator())
+      return nullopt;
+
+    auto punct = qtok->as_punctuator();
+    if(::rocket::is_none_of(punct, accept))
+      return nullopt;
+
+    // A match has been found.
+    tstrm.shift();
+    return punct;
   }
 
 optV_real
 do_accept_number_opt(Token_Stream& tstrm)
   {
     auto qtok = tstrm.peek_opt();
-    if(!qtok) {
+    if(!qtok)
       return nullopt;
-    }
+
     if(qtok->is_integer_literal()) {
+      // Convert integers to reals, as JSON does not have an integral type.
       auto val = qtok->as_integer_literal();
       tstrm.shift();
-      // Convert integers to reals, as JSON does not have an integral type.
       return V_real(val);
     }
+
     if(qtok->is_real_literal()) {
+      // This real can be copied as is.
       auto val = qtok->as_real_literal();
       tstrm.shift();
-      // This real can be copied as is.
       return V_real(val);
     }
+
     if(qtok->is_punctuator()) {
       auto punct = qtok->as_punctuator();
-      if(::rocket::is_any_of(punct, { punctuator_add, punctuator_sub })) {
-        V_real sign = (punct == punctuator_sub) ? -1.0 : +1.0;
-        // Only `Infinity` and `NaN` are allowed.
-        // Please note that the tokenizer will have merged sign symbols into adjacent number literals.
-        qtok = tstrm.peek_opt(1);
-        if(!qtok) {
-          return nullopt;
-        }
-        if(!qtok->is_identifier()) {
-          return nullopt;
-        }
-        const auto& name = qtok->as_identifier();
-        if(name == "Infinity") {
-          tstrm.shift(2);
-          // Accept a signed `Infinity`.
-          return ::std::copysign(::std::numeric_limits<V_real>::infinity(), sign);
-        }
-        if(name == "NaN") {
-          tstrm.shift(2);
-          // Accept a signed `NaN`.
-          return ::std::copysign(::std::numeric_limits<V_real>::quiet_NaN(), sign);
-        }
-      }
+      if(::rocket::is_none_of(punct, { punctuator_add, punctuator_sub }))
+        return nullopt;
+
+      // Only `Infinity` and `NaN` may follow.
+      // Please note that the tokenizer will have merged sign symbols into adjacent number literals.
+      qtok = tstrm.peek_opt(1);
+      if(!qtok)
+        return nullopt;
+
+      if(!qtok->is_identifier())
+        return nullopt;
+
+      const auto& name = qtok->as_identifier();
+      if(::rocket::is_none_of(name, { "Infinity", "NaN" }))
+        return nullopt;
+
+      // Assemble the value.
+      return ::std::copysign(
+          (name[0] == 'I') ? ::std::numeric_limits<V_real>::infinity()
+                           : ::std::numeric_limits<V_real>::quiet_NaN(),
+          (punct == punctuator_add) - 1);
     }
+
     return nullopt;
   }
 
@@ -506,16 +519,16 @@ optV_string
 do_accept_string_opt(Token_Stream& tstrm)
   {
     auto qtok = tstrm.peek_opt();
-    if(!qtok) {
+    if(!qtok)
       return nullopt;
-    }
-    if(qtok->is_string_literal()) {
-      auto val = qtok->as_string_literal();
-      tstrm.shift();
-      // This string literal can be copied as is in UTF-8.
-      return V_string(::std::move(val));
-    }
-    return nullopt;
+
+    if(!qtok->is_string_literal())
+      return nullopt;
+
+    // This string literal can be copied verbatim in UTF-8.
+    auto val = qtok->as_string_literal();
+    tstrm.shift();
+    return ::std::move(val);
   }
 
 opt<Value>
@@ -563,21 +576,23 @@ optV_string
 do_accept_key_opt(Token_Stream& tstrm)
   {
     auto qtok = tstrm.peek_opt();
-    if(!qtok) {
+    if(!qtok)
       return nullopt;
-    }
+
     if(qtok->is_identifier()) {
+      // Identifiers are allowed unquoted in JSON5.
       auto name = qtok->as_identifier();
       tstrm.shift();
-      // Identifiers are allowed unquoted in JSON5.
-      return V_string(::std::move(name));
+      return ::std::move(name);
     }
+
     if(qtok->is_string_literal()) {
+      // This string literal can be copied as is in UTF-8.
       auto val = qtok->as_string_literal();
       tstrm.shift();
-      // This string literal can be copied as is in UTF-8.
-      return V_string(::std::move(val));
+      return ::std::move(val);
     }
+
     return nullopt;
   }
 
@@ -598,8 +613,10 @@ Value
 do_json_parse_nonrecursive(Token_Stream& tstrm)
   {
     Value value;
-    // Implement a recursive descent parser without recursion.
+
+    // Implement a non-recursive descent parser.
     cow_vector<Xparse> stack;
+
     for(;;) {
       // Accept a leaf value. No other things such as closed brackets are allowed.
       auto kpunct = do_accept_punctuator_opt(tstrm, { punctuator_bracket_op, punctuator_brace_op });
@@ -612,6 +629,7 @@ do_json_parse_nonrecursive(Token_Stream& tstrm)
           stack.emplace_back(::std::move(ctxa));
           continue;
         }
+
         // Accept an empty array.
         value = V_array();
       }
@@ -621,86 +639,93 @@ do_json_parse_nonrecursive(Token_Stream& tstrm)
         if(!kpunct) {
           // A key followed by a colon is expected.
           auto qkey = do_accept_key_opt(tstrm);
-          if(!qkey) {
+          if(!qkey)
             throw Parser_Error(parser_status_closed_brace_or_json5_key_expected, tstrm.next_sloc(),
                                tstrm.next_length());
-          }
+
           kpunct = do_accept_punctuator_opt(tstrm, { punctuator_colon });
-          if(!kpunct) {
+          if(!kpunct)
             throw Parser_Error(parser_status_colon_expected, tstrm.next_sloc(), tstrm.next_length());
-          }
+
           // Descend into a new object.
           S_xparse_object ctxo = { nullopt, ::std::move(*qkey) };
           stack.emplace_back(::std::move(ctxo));
           continue;
         }
+
         // Accept an empty object.
         value = V_object();
       }
       else {
         // Just accept a scalar value which is never recursive.
         auto qvalue = do_accept_scalar_opt(tstrm);
-        if(!qvalue) {
+        if(!qvalue)
           throw Parser_Error(parser_status_expression_expected, tstrm.next_sloc(), tstrm.next_length());
-        }
+
         value = ::std::move(*qvalue);
       }
-      // Insert the value into its parent array or object.
+
+      // A complete value has been accepted. Insert it into its parent array or object.
       for(;;) {
-        if(stack.empty()) {
+        if(stack.empty())
           // Accept the root value.
           return value;
-        }
+
         if(stack.back().index() == 0) {
           auto& ctxa = stack.mut_back().as<0>();
+
           // Append the value to its parent array.
           ctxa.array.emplace_back(::std::move(value));
+
           // Look for the next element.
           kpunct = do_accept_punctuator_opt(tstrm, { punctuator_bracket_cl, punctuator_comma });
-          if(!kpunct) {
+          if(!kpunct)
             throw Parser_Error(parser_status_comma_expected, tstrm.next_sloc(), tstrm.next_length());
-          }
+
           if(*kpunct == punctuator_comma) {
+            // Check for termination of this array.
             kpunct = do_accept_punctuator_opt(tstrm, { punctuator_bracket_cl });
-            if(!kpunct) {
-              // The next element is expected to follow the comma.
+            if(!kpunct)
+              // Look for the next element.
               break;
-            }
-            // An extra comma is allowed in JSON5.
           }
-          // Pop the array.
+
+          // The array has been terminated. Pop it.
           value = ::std::move(ctxa.array);
         }
         else {
           auto& ctxo = stack.mut_back().as<1>();
+
           // Insert the value into its parent object.
           ctxo.object.insert_or_assign(::std::move(ctxo.key), ::std::move(value));
+
           // Look for the next element.
           kpunct = do_accept_punctuator_opt(tstrm, { punctuator_brace_cl, punctuator_comma });
-          if(!kpunct) {
+          if(!kpunct)
             throw Parser_Error(parser_status_closed_brace_or_comma_expected, tstrm.next_sloc(),
                                tstrm.next_length());
-          }
+
           if(*kpunct == punctuator_comma) {
+            // Check for termination of this object.
             kpunct = do_accept_punctuator_opt(tstrm, { punctuator_brace_cl });
             if(!kpunct) {
-              // The next key is expected to follow the comma.
+              // Look for the next key.
               auto qkey = do_accept_key_opt(tstrm);
-              if(!qkey) {
+              if(!qkey)
                 throw Parser_Error(parser_status_closed_brace_or_json5_key_expected, tstrm.next_sloc(),
                                    tstrm.next_length());
-              }
+
               kpunct = do_accept_punctuator_opt(tstrm, { punctuator_colon });
-              if(!kpunct) {
+              if(!kpunct)
                 throw Parser_Error(parser_status_colon_expected, tstrm.next_sloc(), tstrm.next_length());
-              }
+
               ctxo.key = ::std::move(*qkey);
-              // The next value is expected to follow the colon.
+              // Look for the next value.
               break;
             }
-            // An extra comma is allowed in JSON5.
           }
-          // Pop the object.
+
+          // The object has been terminated. Pop it.
           value = ::std::move(ctxo.object);
         }
         stack.pop_back();
