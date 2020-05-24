3b13c63b9d54dc9fa8934bb452d04f77a4d1233c
Liu Hao <lh_mouse@126.com>
2020-05-25 00:33:42 +0800

    rocket/hash_table_utilities: Reformat

--- asteria/rocket/hash_table_utilities.hpp
+++ asteria/rocket/hash_table_utilities.hpp
@@ -15,6 +15,7 @@ get_probing_origin(bucketT* begin, bucketT* end, size_t hval)
 noexcept
   {
     ROCKET_ASSERT(begin < end);
+
     // Multiplication is faster than division.
     auto seed = hval * 0x9E3779B9 / 2;
     auto ratio = static_cast<double>(static_cast<long>(seed) & 0x7FFFFFFF) / 0x80000000;
@@ -31,14 +32,17 @@ linear_probe(bucketT* begin, bucketT* to, bucketT* from, bucketT* end, const pre
     ROCKET_ASSERT(to <= from);
     ROCKET_ASSERT(from <= end);
     ROCKET_ASSERT(begin < end);
+
     // Phase 1: Probe from `from` to `end`.
     for(auto bkt = from; bkt != end; ++bkt)
       if(!*bkt || pred(*bkt))
         return bkt;
+
     // Phase 2: Probe from `begin` to `to`.
     for(auto bkt = begin; bkt != to; ++bkt)
       if(!*bkt || pred(*bkt))
         return bkt;
+
     // The table is full and no desired bucket has been found so far.
     return nullptr;
   }
