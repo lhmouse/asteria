41b1b96aeb1f36b14e6382ad8390f620c361bf36
Liu Hao <lh_mouse@126.com>
2020-06-15 19:54:53 +0800

    rocket/condition_variable: Use integral arithmetic to calculate time points

--- asteria/rocket/condition_variable.hpp
+++ asteria/rocket/condition_variable.hpp
@@ -39,6 +39,7 @@ class condition_variable
   public:
     void
     wait(mutex::unique_lock& lock)
+    noexcept
       {
         // The mutex will be unlocked, so release the lock.
         auto owns = lock.m_sth.release();
@@ -55,6 +56,7 @@ class condition_variable
     template<typename predT>
     void
     wait(mutex::unique_lock& lock, predT&& pred)
+    noexcept(noexcept(pred()))
       {
         while(!pred())
           this->wait(lock);
@@ -62,27 +64,42 @@ class condition_variable
 
     void
     wait_for(mutex::unique_lock& lock, long msecs)
+    noexcept
       {
-        // Get the current time.
-        ::timespec ts;
-        int r = ::clock_gettime(CLOCK_REALTIME, &ts);
-        ROCKET_ASSERT(r == 0);
-
-        // Get the end time.
-        double end = noadl::clamp(
-            double(ts.tv_sec) + double(ts.tv_nsec) * 1.0e-9 + double(msecs) * 1.0e-3,
-            double(0), double(::std::numeric_limits<::time_t>::max()));
-
-        // Break the time down into second and subsecond parts.
-        ts.tv_sec = static_cast<::time_t>(end);
-        ts.tv_nsec = static_cast<long>((end - static_cast<double>(ts.tv_sec)) * 1.0e9);
+        // Assume non-positive timeouts expire immediately.
+        if(msecs <= 0)
+          return;
 
         // The mutex will be unlocked, so release the lock.
         auto owns = lock.m_sth.release();
         ROCKET_ASSERT(owns);
 
-        // Now wait on the condition variable.
-        details_condition_variable::do_cond_timedwait(this->m_cond, *owns, ts);
+        // Get the current time.
+        ::timespec ts;
+        int r = ::clock_gettime(CLOCK_REALTIME, &ts);
+        ROCKET_ASSERT(r == 0);
+
+        // Ensure we don't cause overflows.
+        int64_t mlim = ::std::numeric_limits<::time_t>::max();
+        mlim -= ts.tv_sec + 1;
+        mlim *= 1000;
+
+        if(msecs < mlim) {
+          // If overflow is not possible, add `msecs` to the current time.
+          long secs = static_cast<long>(static_cast<unsigned long>(msecs) / 1'000);
+          ts.tv_sec += static_cast<::time_t>(secs);
+          ts.tv_nsec += (msecs - secs * 1'000) * 1'000'000;
+
+          long mask = (999'999'999 - ts.tv_nsec) >> 31;
+          ts.tv_sec -= mask;
+          ts.tv_nsec -= mask & 1'000'000'000;
+
+          // Now wait on the condition variable.
+          details_condition_variable::do_cond_timedwait(this->m_cond, *owns, ts);
+        }
+        else
+          // If the value overflows, wait forever.
+          details_condition_variable::do_cond_wait(this->m_cond, *owns);
 
         // The mutex has been locked again, so reset it.
         ROCKET_ASSERT(lock.m_sth.get() == nullptr);
@@ -92,6 +109,7 @@ class condition_variable
     template<typename predT>
     void
     wait_for(mutex::unique_lock& lock, long msecs, predT&& pred)
+    noexcept(noexcept(pred()))
       {
         while(!pred())
           this->wait_for(lock, msecs);
