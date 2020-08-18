a54686d21c8cc348e538393a084b6f5aa3994f24
Liu Hao <lh_mouse@126.com>
2020-08-18 13:10:55 +0800

    repl: Minor

--- asteria/src/repl.cpp
+++ asteria/src/repl.cpp
@@ -605,6 +605,7 @@ main(int argc, char** argv)
     else
       do_single_noreturn();
   }
-  catch(exception& stdex)
+  catch(exception& stdex) {
     // Print a message followed by the backtrace if it is available. There isn't much we can do.
-    { do_exit(exit_system_error, "! unhandled exception: %s\n", stdex.what());  }
+    do_exit(exit_system_error, "! unhandled exception: %s\n", stdex.what());
+  }
