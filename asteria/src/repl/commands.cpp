b4a8574343496f465e2d6391fd5f5de8a604d067
Liu Hao <lh_mouse@126.com>
2020-11-02 00:55:02 +0800

    repl/commands: Add `help` and `exit`

--- asteria/src/repl/commands.cpp
+++ asteria/src/repl/commands.cpp
@@ -28,7 +28,7 @@ struct Command
 
     virtual
     const char*
-    manual()  // the long description for `help [cmd]`
+    description()  // the long description for `help [cmd]`
       const noexcept
       = 0;
 
@@ -44,6 +44,59 @@ Command::
   {
   }
 
+struct Command_exit
+  final
+  : public Command
+  {
+    const char*
+    cmd()
+      const noexcept override
+      { return "exit";  }
+
+    const char*
+    oneline()
+      const noexcept override
+      { return "exits the interpreter";  }
+
+    const char*
+    description()
+      const noexcept override
+      { return
+//       1         2         3         4         5         6         7      |
+// 4567890123456789012345678901234567890123456789012345678901234567890123456|
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
+* exit [CODE]
+
+  Exits the interpreter. If CODE is specified, it shall be a decimal integer
+  which is used as the process exit status. If CODE is not a valid integer,
+  the process exits with an unspecified non-zero status. If CODE is absent,
+  the process exits with zero.
+)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
+// 4567890123456789012345678901234567890123456789012345678901234567890123456|
+//       1         2         3         4         5         6         7      |
+      }
+
+    [[noreturn]]
+    void
+    handle(cow_string&& args)
+      const override
+      {
+        const char* bptr = args.data();
+        const char* eptr = bptr + args.size();
+        if(bptr == eptr)
+          printf_and_exit(exit_success, "* have a nice day :)\n");
+
+        uint8_t stat;
+        ::rocket::ascii_numget numg;
+        if(!numg.get(stat, bptr, eptr) || (bptr != eptr)) {
+          ::fprintf(stderr, "! warning: invalid exit status ignored: %s\n",
+                            args.c_str());
+          stat = exit_non_integer;
+        }
+        printf_and_exit(static_cast<Exit_Status>(stat), "* exiting: %d\n", stat);
+      }
+  };
+
 struct Command_help
   final
   : public Command
@@ -56,21 +109,20 @@ struct Command_help
     const char*
     oneline()
       const noexcept override
-      { return "display information about a command";  }
+      { return "obtains information about a command";  }
 
     const char*
-    manual()
+    description()
       const noexcept override
       { return
 //       1         2         3         4         5         6         7      |
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
-  help [COMMAND]
+* help [COMMAND]
 
-  When no COMMAND is specified, a list of all available commands with short
-  descriptions is displayed.
-
-  When COMMAND is specified, the full description for COMMAND is displayed.
+  When COMMAND is specified, prints the full description of COMMAND. When no
+  COMMAND is specified, prints a list of all available commands with short
+  descriptions.
 )'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 //       1         2         3         4         5         6         7      |
@@ -78,16 +130,48 @@ struct Command_help
 
     void
     handle(cow_string&& args)
-      const override
-      {
-        ::fprintf(stderr, "TODO args = %s\n", args.c_str());
-      }
+      const override;
   };
 
-const uptr<Command> s_commands[] =
+const uptr<const Command> s_commands[] =
   {
+    // Please keep this list in lexicographical order.
+    ::rocket::make_unique<Command_exit>(),
     ::rocket::make_unique<Command_help>(),
   };
+
+void
+Command_help::
+handle(cow_string&& args)
+  const
+  {
+    if(!args.empty()) {
+      // Convert all letters in `args` into lowercase.
+      ::std::for_each(args.mut_begin(), args.mut_end(),
+          [&](char& ch) { ch = ::rocket::ascii_to_lower(ch);  });
+
+      // Search for the argument.
+      for(const auto& ptr : s_commands)
+        if(ptr->cmd() == args)
+          return (void)::fputs(ptr->description(), stderr);
+
+      // Report the failure, followed by all commands for reference.
+      ::fprintf(stderr, "! unknown command `%s`\n", args.c_str());
+    }
+
+    // List all commands.
+    ::fprintf(stderr, "* list of commands:\n");
+
+    // Get the maximum length of commands.
+    size_t max_len = 8;
+    for(const auto& ptr : s_commands)
+      max_len = ::rocket::max(max_len, ::std::strlen(ptr->cmd()));
+
+    // Print all of them.
+    for(const auto& ptr : s_commands)
+      ::fprintf(stderr, "  %-*s  %s\n",
+                static_cast<int>(max_len), ptr->cmd(), ptr->oneline());
+  }
 
 }  // namesapce
 
@@ -103,8 +187,10 @@ handle_repl_command(cow_string&& cmd, cow_string&& args)
       if(ptr->cmd() == cmd)
         return ptr->handle(::std::move(args));
 
-    // Ignore this command
-    ::fprintf(stderr, "! unknown command: %s\n", cmd.c_str());
+    // Ignore this command.
+    ::fprintf(stderr,
+        "! unknown command `%s` (type `:help` for available commands)\n",
+        cmd.c_str());
   }
 
 }  // namespace asteria
