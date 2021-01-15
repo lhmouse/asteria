bab5d04c9349a02bd2d8f45d66a616cf2bd9f7ad
Liu Hao <lh_mouse@126.com>
2021-01-15 15:47:43 +0800

    ci: Disable 'make check' on MSYS2
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,25 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 # setup
 export CXX=${CXX:-"g++"}
 export CXXFLAGS='-O2 -g0'
+
+_nocheck=
+
+while test $# -gt 0
+do
+  case $1 in
+    --disable-make-check )
+      _nocheck=1
+      shift 1
+      ;;
+
+    * )
+      echo "WARNING: unknown option -- '$1'" >&2
+      shift 1
+      ;;
+  esac
+done
 
 # build
 ${CXX} --version
@@ -12,4 +29,7 @@ autoreconf -ifv
 make -j$(nproc)
 
 # test
-make -j$(nproc) check || (cat ./test-suite.log; false)
+if ! test -n "$_nocheck"
+then
+  make -j$(nproc) check || (cat ./test-suite.log; false)
+fi
