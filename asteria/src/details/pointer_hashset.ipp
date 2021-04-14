e39cb4272a930ef1c3f54efc9382b067be2772d4
Liu Hao <lh_mouse@126.com>
2021-04-14 17:23:34 +0800

    llds/pointer_hashset: New

---
+++ asteria/src/details/pointer_hashset.ipp
@@ -0,0 +1,23 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2021, LH_Mouse. All wrongs reserved.
+
+#ifndef ASTERIA_LLDS_POINTER_HASHSET_HPP_
+#  error Please include <asteria/llds/pointer_hashset.hpp> instead.
+#endif
+
+namespace asteria {
+namespace details_pointer_hashset {
+
+struct Bucket
+  {
+    Bucket* next;  // the next bucket in the [non-circular] list
+    Bucket* prev;  // the previous bucket in the [circular] list
+    const void* key;  // initialized iff `prev` is non-null
+
+    explicit operator
+    bool() const noexcept
+      { return this->prev != nullptr;  }
+  };
+
+}  // namespace details_pointer_hashset
+}  // namespace asteria
---
+++ asteria/src/llds/pointer_hashset.cpp
@@ -0,0 +1,122 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2021, LH_Mouse. All wrongs reserved.
+
+#include "../precompiled.hpp"
+#include "pointer_hashset.hpp"
+#include "../utils.hpp"
+
+namespace asteria {
+
+details_pointer_hashset::Bucket*
+Pointer_HashSet::
+do_xprobe(const void* ptr) const noexcept
+  {
+    auto bptr = this->m_bptr;
+    auto eptr = this->m_eptr;
+
+    // Find a bucket using linear probing.
+    // We keep the load factor below 1.0 so there will always be some empty buckets
+    // in the table.
+    auto mptr = ::rocket::get_probing_origin(bptr, eptr,
+                    reinterpret_cast<uintptr_t>(ptr));
+    auto qbkt = ::rocket::linear_probe(bptr, mptr, mptr, eptr,
+                    [&](const Bucket& r) { return r.key == ptr;  });
+
+    // The load factor is kept <= 0.5 so there must always be a bucket available.
+    ROCKET_ASSERT(qbkt);
+    return qbkt;
+  }
+
+void
+Pointer_HashSet::
+do_xrelocate_but(Bucket* qxcld) noexcept
+  {
+    ::rocket::linear_probe(
+      this->m_bptr,
+      qxcld,
+      qxcld + 1,
+      this->m_eptr,
+      [&](Bucket& rb) {
+        auto sbkt = &rb;
+
+        // Mark this bucket empty, without destroying its contents.
+        ROCKET_ASSERT(*sbkt);
+        this->do_list_detach(sbkt);
+
+        // Find a new bucket for the pointer using linear probing.
+        // Uniqueness has already been implied for all elements, so there is no need
+        // to check for collisions.
+        auto mptr = ::rocket::get_probing_origin(this->m_bptr, this->m_eptr,
+                        reinterpret_cast<uintptr_t>(sbkt->key));
+        auto qbkt = ::rocket::linear_probe(this->m_bptr, mptr, mptr,this->m_eptr,
+                        [&](const Bucket&) { return false;  });
+        ROCKET_ASSERT(qbkt);
+
+        // Mark the new bucket non-empty.
+        ROCKET_ASSERT(!*qbkt);
+        this->do_list_attach(qbkt);
+
+        // If the two pointers reference the same one, no relocation is needed.
+        if(ROCKET_EXPECT(qbkt == sbkt))
+          return false;
+
+        // Relocate the bucket.
+        qbkt->key = sbkt->key;
+        sbkt->key = reinterpret_cast<void*>(0xBEEFDEAD);
+
+        // Keep probing until an empty bucket is found.
+        return false;
+      });
+  }
+
+void
+Pointer_HashSet::
+do_rehash_more()
+  {
+    // Allocate a new table.
+    size_t nbkt = (this->m_size * 3 + 5) | 67;
+    if(nbkt > PTRDIFF_MAX / sizeof(Bucket))
+      throw ::std::bad_array_new_length();
+
+    if(nbkt / 2 <= this->m_size)
+      throw ::std::bad_alloc();
+
+    auto bptr = static_cast<Bucket*>(::operator new(nbkt * sizeof(Bucket)));
+    auto eptr = bptr + nbkt;
+
+    // Initialize an empty table.
+    ::std::for_each(bptr, eptr, [&](Bucket& r) { r.prev = nullptr;  });
+    auto bold = ::std::exchange(this->m_bptr, bptr);
+    this->m_eptr = eptr;
+
+    // Move buckets into the new table.
+    // Warning: No exception shall be thrown from the code below.
+    auto sbkt = ::std::exchange(this->m_head, nullptr);
+    while(ROCKET_EXPECT(sbkt)) {
+      ROCKET_ASSERT(*sbkt);
+
+      // Find a new bucket for the pointer using linear probing.
+      // Uniqueness has already been implied for all elements, so there is no need
+      // to check for collisions.
+      auto mptr = ::rocket::get_probing_origin(bptr, eptr,
+                      reinterpret_cast<uintptr_t>(sbkt->key));
+      auto qbkt = ::rocket::linear_probe(bptr, mptr, mptr, eptr,
+                      [&](const Bucket&) { return false;  });
+      ROCKET_ASSERT(qbkt);
+
+      // Mark the new bucket non-empty.
+      ROCKET_ASSERT(!*qbkt);
+      this->do_list_attach(qbkt);
+
+      // Relocate the bucket.
+      qbkt->key = sbkt->key;
+      sbkt->key = reinterpret_cast<void*>(0xBEEFDEAD);
+
+      // Process the next bucket.
+      sbkt = sbkt->next;
+    }
+    if(bold)
+      ::operator delete(bold);
+  }
+
+}  // namespace asteria
