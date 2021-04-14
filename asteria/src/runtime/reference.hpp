e69175cc457cacd4a25d917df5cf6d6df5d480ed
Liu Hao <lh_mouse@126.com>
2021-04-14 23:31:02 +0800

    runtime/reference: Add pure attribute
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- asteria/src/runtime/reference.hpp
+++ asteria/src/runtime/reference.hpp
@@ -51,7 +51,7 @@ class Reference
       { return this->assign(::std::move(other));  }
 
   private:
-    const Value&
+    ROCKET_PURE_FUNCTION const Value&
     do_dereference_readonly_slow() const;
 
     Value&
