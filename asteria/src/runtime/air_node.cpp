8a3b9d9317b30f54a8bb5cdf5de35e7f4831abdd
Liu Hao <lh_mouse@126.com>
2021-04-20 23:39:18 +0800

    runtime/air_node: Minor

--- asteria/src/runtime/air_node.cpp
+++ asteria/src/runtime/air_node.cpp
@@ -976,10 +976,9 @@ struct Traits_push_local_reference
       {
         // Get the context.
         Executive_Context* qctx = &ctx;
-        for(uint32_t k = 0;  k != up.u32;  ++k) {
-          qctx = qctx->get_parent_opt();
-          ROCKET_ASSERT(qctx);
-        }
+        for(uint32_t k = 0;  k != up.u32;  ++k)
+          qctx = qctx->get_parent_opt(),
+            ROCKET_ASSERT(qctx);
 
         // Look for the name in the context.
         auto qref = qctx->get_named_reference_opt(name);
@@ -4982,10 +4981,10 @@ rebind_opt(Abstract_Context& ctx) const
         // Get the context.
         // Don't bind references in analytic contexts.
         Abstract_Context* qctx = &ctx;
-        for(uint32_t k = 0;  k != altr.depth;  ++k) {
-          qctx = qctx->get_parent_opt();
-          ROCKET_ASSERT(qctx);
-        }
+        for(uint32_t k = 0;  k != altr.depth;  ++k)
+          qctx = qctx->get_parent_opt(),
+            ROCKET_ASSERT(qctx);
+
         if(qctx->is_analytic())
           return nullopt;
 
