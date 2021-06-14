45c1e485f4af4b15346845701f12bcd63dce975f
Liu Hao <lh_mouse@126.com>
2021-06-14 22:59:41 +0800

    repl/commands: Pass in arguments as vectors

--- asteria/src/repl/commands.cpp
+++ asteria/src/repl/commands.cpp
@@ -8,236 +8,253 @@
 namespace asteria {
 namespace {
 
-struct Command
+struct Handler
   {
     virtual
-    ~Command();
+    ~Handler();
 
     virtual const char*
-    cmd()  // the name of this command
-      const noexcept
+    cmd() const  // the name of this command
       = 0;
 
     virtual const char*
-    oneline()  // the one-line description for `help`
-      const noexcept
+    oneline() const  // the one-line description for `help`
       = 0;
 
     virtual const char*
-    description()  // the long description for `help [cmd]`
-      const noexcept
+    help() const  // the long description for `help [cmd]`
       = 0;
 
     virtual void
-    handle(cow_string&& args)  // do something
-      const
+    handle(cow_vector<cow_string>&& args)  // do something
       = 0;
   };
 
-Command::
-~Command()
+Handler::
+~Handler()
   {
   }
 
-cow_vector<uptr<const Command>> s_commands;
+cow_vector<uptr<Handler>> s_handlers;
 
-void
-do_lowercase_string(cow_string& str)
+Handler*
+do_find_handler_opt(cow_string&& cmd)
   {
     char ch;
-    for(size_t k = 0;  k != str.size();  ++k)
-      if((ch = ::rocket::ascii_to_lower(str[k])) != str[k])
-        str.mut(k) = ch;
+    for(size_t k = 0;  k != cmd.size();  ++k)
+      if((ch = ::rocket::ascii_to_lower(cmd[k])) != cmd[k])
+        cmd.mut(k) = ch;
+
+    for(const auto& up : s_handlers)
+      if(up->cmd() == cmd)
+        return up.get();
+
+    return nullptr;
   }
 
-template<typename CommandT>
+template<typename HandlerT>
 void
-do_add_command()
+do_add_handler()
   {
-    s_commands.emplace_back(::rocket::make_unique<CommandT>());
+    s_handlers.emplace_back(::rocket::make_unique<HandlerT>());
   }
 
-struct Command_exit final
-  : public Command
+struct Handler_exit final
+  : public Handler
   {
     const char*
-    cmd() const noexcept override
+    cmd() const override
       { return "exit";  }
 
     const char*
-    oneline() const noexcept override
+    oneline() const override
       { return "exit the interpreter";  }
 
     const char*
-    description() const noexcept override
+    help() const override
       { return
 //       1         2         3         4         5         6         7      |
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
-* exit [CODE]
+  exit [CODE]
 
   Exit the interpreter. If CODE is absent, the process exits with zero. If
   CODE is specified, it shall be a decimal integer denoting the process exit
   status. If CODE is not a valid integer, the process exits anyway, with an
   unspecified non-zero status.
-)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
+)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+3;
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 //       1         2         3         4         5         6         7      |
       }
 
     [[noreturn]] void
-    handle(cow_string&& args) const override
+    handle(cow_vector<cow_string>&& args)
       {
-        const char* bptr = args.data();
-        const char* eptr = bptr + args.size();
-        if(bptr == eptr)
+        if(args.empty())
           exit_printf(exit_success, "* have a nice day :)\n");
 
-        uint8_t stat;
+        // Parse the status code.
+        Exit_Status stat = exit_non_integer;
+
         ::rocket::ascii_numget numg;
-        if(!numg.get(stat, bptr, eptr) || (bptr != eptr)) {
-          repl_printf("! warning: invalid exit status: %s\n", args.c_str());
-          stat = exit_non_integer;
-        }
-        exit_printf(static_cast<Exit_Status>(stat), "* exiting: %d\n", stat);
+        uint8_t num;
+        const char* bptr = args[0].data();
+        const char* eptr = bptr + args[0].size();
+
+        if(numg.get(num, bptr, eptr) && (bptr == eptr))
+          stat = static_cast<Exit_Status>(num);
+        else
+          repl_printf("! warning: invalid exit status: %s\n", args[0].c_str());
+
+        if(args.size() > 1)
+          repl_printf("! warning: excess arguments ignored\n");
+
+        // Exit the interpreter now.
+        exit_printf(stat, "* exiting: %d\n", stat);
       }
   };
 
-struct Command_help final
-  : public Command
+struct Handler_help final
+  : public Handler
   {
     const char*
-    cmd() const noexcept override
+    cmd() const override
       { return "help";  }
 
     const char*
-    oneline() const noexcept override
+    oneline() const override
       { return "obtain information about a command";  }
 
     const char*
-    description() const noexcept override
+    help() const override
       { return
 //       1         2         3         4         5         6         7      |
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
-* help [COMMAND]
+  help [COMMAND]
 
   When COMMAND is specified, prints the full description of COMMAND. When no
   COMMAND is specified, prints a list of all available commands with short
   descriptions.
-)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
+)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+3;
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 //       1         2         3         4         5         6         7      |
       }
 
     void
-    handle(cow_string&& args) const override
+    list_all_commands() const
       {
-        if(!args.empty()) {
-          // Search for the argument.
-          do_lowercase_string(args);
+        int max_len = 8;
+        for(const auto& ptr : s_handlers)
+          max_len = ::rocket::max(max_len, (int)::strlen(ptr->cmd()));
 
-          for(const auto& ptr : s_commands)
-            if(ptr->cmd() == args)
-              return repl_printf("%s", ptr->description());
-
-          // Report the failure, followed by all commands for reference.
-          repl_printf("! unknown command `%s`\n", args.c_str());
-        }
-
-        // Get the maximum length of commands.
-        size_t max_len = 8;
-        for(const auto& ptr : s_commands)
-          max_len = ::rocket::max(max_len, ::std::strlen(ptr->cmd()));
-
-        // List all commands.
         repl_printf("* list of commands:\n");
-        for(const auto& ptr : s_commands)
-          repl_printf("  %-*s  %s\n", static_cast<int>(max_len), ptr->cmd(),
-                                      ptr->oneline());
+        for(const auto& ptr : s_handlers)
+          repl_printf("  %-*s  %s\n", max_len, ptr->cmd(), ptr->oneline());
+      }
+
+    void
+    help_command(cow_string&& cmd) const
+      {
+        auto qhand = do_find_handler_opt(::std::move(cmd));
+        if(!qhand)
+          repl_printf("! unknown command `%s`\n", cmd.c_str());
+        else
+          repl_printf("* %s", qhand->help());
+      }
+
+    void
+    handle(cow_vector<cow_string>&& args) override
+      {
+        if(args.empty())
+          return this->list_all_commands();
+
+        for(size_t k = 0;  k != args.size();  ++k)
+          this->help_command(::std::move(args.mut(k)));
       }
   };
 
-struct Command_heredoc final
-  : public Command
+struct Handler_heredoc final
+  : public Handler
   {
     const char*
-    cmd() const noexcept override
+    cmd() const override
       { return "heredoc";  }
 
     const char*
-    oneline() const noexcept override
+    oneline() const override
       { return "enter heredoc mode";  }
 
     const char*
-    description() const noexcept override
+    help() const override
       { return
 //       1         2         3         4         5         6         7      |
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
-* heredoc DELIM
+  heredoc DELIM
 
   Enter heredoc mode. This prevents the interpreter from submitting scripts
   on line breaks. Instead, a script is terminated by a line that matches
   DELIM, without any leading or trailing spaces.
-)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
+)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+3;
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 //       1         2         3         4         5         6         7      |
       }
 
     void
-    handle(cow_string&& args) const override
+    handle(cow_vector<cow_string>&& args) override
       {
-        if(args.empty())
-          return repl_printf("! `heredoc` requires a terminator string\n");
+        if(args.size() != 1)
+          return repl_printf("! exactly one terminator string expected\n");
 
-        repl_heredoc = ::std::move(args);
+        repl_heredoc = ::std::move(args.mut(0));
         repl_printf("* the next snippet will be terminated by `%s`\n",
                     repl_heredoc.c_str());
       }
   };
 
-struct Command_source final
-  : public Command
+struct Handler_source final
+  : public Handler
   {
     const char*
-    cmd() const noexcept override
+    cmd() const override
       { return "source";  }
 
     const char*
-    oneline() const noexcept override
+    oneline() const override
       { return "load and execute a script file";  }
 
     const char*
-    description() const noexcept override
+    help() const override
       { return
 //       1         2         3         4         5         6         7      |
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
-* source PATH
+  source PATH [ARGUMENTS...]
 
-  Load and execute the file designated by PATH.
-)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
+  Load and execute the file designated by PATH. ARGUMENTS are passed to the
+  script as strings, which can be accessed via `__varg()`.
+)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+3;
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 //       1         2         3         4         5         6         7      |
       }
 
     void
-    handle(cow_string&& args) const override
+    handle(cow_vector<cow_string>&& args) override
       {
         if(args.empty())
-          return repl_printf("! `source` requires a file path\n");
+          return repl_printf("! file path expected\n");
 
         cow_string source;
         long line = 0;
         cow_string textln;
 
         ::rocket::tinybuf_file file;
-        file.open(args.safe_c_str(), ::rocket::tinybuf::open_read);
+        file.open(args[0].safe_c_str(), ::rocket::tinybuf::open_read);
 
         int indent;
         ::snprintf(nullptr, 0, "#%lu:%lu%n> ", repl_index, line, &indent);
-        repl_printf("* loading file '%s'...\n", args.c_str());
+        repl_printf("* loading file '%s'...\n", args[0].c_str());
 
         int ch;
         bool noeol = false;
@@ -270,49 +287,52 @@ struct Command_source final
         if(noeol)
           repl_printf("! warning: missing new line at end of file\n");
 
-        repl_printf("* finished loading file '%s'\n", args.c_str());
+        repl_printf("* finished loading file '%s'\n", args[0].c_str());
 
+        // Set the script to execute.
         repl_source = ::std::move(source);
-        repl_file = ::std::move(args);
+        repl_file = ::std::move(args.mut(0));
+        repl_args.assign(args.move_begin() + 1, args.move_end());
       }
   };
 
-struct Command_again final
-  : public Command
+struct Handler_again final
+  : public Handler
   {
     const char*
-    cmd() const noexcept override
+    cmd() const override
       { return "again";  }
 
     const char*
-    oneline() const noexcept override
+    oneline() const override
       { return "reload last snippet compiled successfully";  }
 
     const char*
-    description() const noexcept override
+    help() const override
       { return
 //       1         2         3         4         5         6         7      |
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""" R"'''''''''''''''(
-* again
+  again [ARGUMENTS...]
 
   Reload and execute the last snippet that has been compiled successfully.
-)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+1;
+  ARGUMENTS are passed to the script as strings, which can be accessed via
+  `__varg()`.
+)'''''''''''''''" """"""""""""""""""""""""""""""""""""""""""""""""""""""""+3;
 // 4567890123456789012345678901234567890123456789012345678901234567890123456|
 //       1         2         3         4         5         6         7      |
       }
 
     void
-    handle(cow_string&& args) const override
+    handle(cow_vector<cow_string>&& args) override
       {
-        if(args.size())
-          return repl_printf("! `again` takes no argument\n");
-
         if(repl_last_source.empty())
           return repl_printf("! no snippet has been compiled so far\n");
 
+        // Set the script to execute.
         repl_source = repl_last_source;
         repl_file = repl_last_file;
+        repl_args.assign(args.move_begin(), args.move_end());
       }
   };
 
@@ -321,40 +341,97 @@ struct Command_again final
 void
 prepare_repl_commands()
   {
-    if(s_commands.size())
+    if(s_handlers.size())
       return;
 
     // Create command interfaces. Note the list of commands is printed
     // according to this vector, so please ensure elements are sorted
     // lexicographically.
-    do_add_command<Command_again>();
-    do_add_command<Command_exit>();
-    do_add_command<Command_help>();
-    do_add_command<Command_heredoc>();
-    do_add_command<Command_source>();
+    do_add_handler<Handler_again>();
+    do_add_handler<Handler_exit>();
+    do_add_handler<Handler_help>();
+    do_add_handler<Handler_heredoc>();
+    do_add_handler<Handler_source>();
   }
 
 void
 handle_repl_command(cow_string&& cmdline)
   {
-    // TODO
-    size_t pos = cmdline.find_first_of(" \t");
-    auto cmd = cmdline.substr(0, pos);
+    // Tokenize the command line.
+    cow_string cmd;
+    cow_vector<cow_string> args;
 
-    pos = cmdline.find_first_not_of(pos, " \t");
-    cmdline.erase(0, pos);
+    char quote = 0;
+    bool has_token = false;
+    size_t pos = 0;
 
-    // Find a command and execute it.
-    do_lowercase_string(cmd);
+    for(;;) {
+      char ch;
 
-    for(const auto& ptr : s_commands)
-      if(ptr->cmd() == cmd)
-        return ptr->handle(::std::move(cmdline));
+      if(quote) {
+        if(pos >= cmdline.size())
+          ::rocket::sprintf_and_throw<::std::invalid_argument>(
+                "unmatched %c", quote);
 
-    // Ignore this command.
-    repl_printf(
-        "! unknown command `%s` (type `:help` for available commands)\n",
-        cmd.c_str());
+        ch = cmdline[pos++];
+
+        // Check for the end of this quoted argument.
+        if(ch == quote) {
+          quote = 0;
+          continue;
+        }
+      }
+      else {
+        // Blank characters outside quotes terminate arguments.
+        size_t next = cmdline.find_first_not_of(pos, sref(" \f\n\r\t\v"));
+        if((next != pos) && has_token) {
+          args.emplace_back(::std::move(cmd));
+          cmd.clear();
+          has_token = false;
+        }
+
+        pos = next;
+        if(pos == cow_string::npos)
+          break;
+
+        ch = cmdline[pos++];
+        has_token = true;
+
+        // Check for quoted arguments.
+        if(::rocket::is_any_of(ch, { '\'', '\"' })) {
+          quote = ch;
+          continue;
+        }
+      }
+
+      // Escape sequences are allowed except in single quotes.
+      if((quote != '\'') && (ch == '\\')) {
+        if(pos >= cmdline.size())
+          ::rocket::sprintf_and_throw<::std::invalid_argument>(
+                "dangling \\ at end of command");
+
+        ch = cmdline[pos++];
+      }
+
+      cmd += ch;
+    }
+
+    ROCKET_ASSERT(!has_token);
+
+    if(args.empty())
+      return;
+
+    // Pop the command and execute it.
+    cmd = ::std::move(args.mut(0));
+    args.erase(0, 1);
+
+    auto qhand = do_find_handler_opt(::std::move(cmd));
+    if(!qhand)
+      ::rocket::sprintf_and_throw<::std::invalid_argument>(
+          "unknown command `%s` (type `:help` for available commands)",
+          cmd.c_str());
+
+    qhand->handle(::std::move(args));
   }
 
 }  // namespace asteria
