5694c676518a78a9147cc9472f5c3bd2b3ac62ba
LIU Hao <lh_mouse@126.com>
2021-07-07 21:52:51 +0800

    source_location: Remove `explicit` from constructors

--- asteria/src/source_location.hpp
+++ asteria/src/source_location.hpp
@@ -16,12 +16,10 @@ class Source_Location
     int m_column;
 
   public:
-    explicit
     Source_Location() noexcept
       : m_file(sref("[unknown]")), m_line(-1), m_column(-1)
       { }
 
-    explicit
     Source_Location(const cow_string& xfile, int xline, int xcolumn) noexcept
       : m_file(xfile), m_line(xline), m_column(xcolumn)
       { }
