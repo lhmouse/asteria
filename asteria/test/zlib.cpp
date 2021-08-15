ec7b3f319fc89745eb74c3e1fb0d2ed754d1a2de
LIU Hao <lh_mouse@126.com>
2021-08-15 22:58:01 +0800

    test/zlib: Add comments

--- asteria/test/zlib.cpp
+++ asteria/test/zlib.cpp
@@ -16,6 +16,7 @@ int main()
         const trailer = "\x00\x00\xff\xff";
 
         // https://datatracker.ietf.org/doc/html/rfc7692#section-7.2.3.2
+        // 7.2.3.2.  Sharing LZ77 Sliding Window
         var defl = std.zlib.Deflator("raw");
         var infl = std.zlib.Inflator("raw");
 
@@ -35,8 +36,10 @@ int main()
         assert infl.output == "HelloHello";
 
         // https://datatracker.ietf.org/doc/html/rfc7692#section-7.2.3.3
+        // 7.2.3.3.  Using a DEFLATE Block with No Compression
         var defl = std.zlib.Deflator("raw", 0);
         var infl = std.zlib.Inflator("raw");
+
         defl.update("Hello");
         defl.flush();
         assert defl.output == "\x00\x05\x00\xfa\xff\x48\x65\x6c\x6c\x6f\x00" + trailer;
@@ -45,8 +48,10 @@ int main()
         assert infl.output == "Hello";
 
         // https://datatracker.ietf.org/doc/html/rfc7692#section-7.2.3.5
+        // 7.2.3.5.  Two DEFLATE Blocks in One Message
         var defl = std.zlib.Deflator("raw");
         var infl = std.zlib.Inflator("raw");
+
         defl.update("He");
         defl.flush();
         defl.update("llo");
