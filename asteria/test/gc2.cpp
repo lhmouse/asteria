57d720350df85b1bd61a1835154ad44387ff773e
LIU Hao <lh_mouse@126.com>
2021-12-06 00:59:25 +0800

    test/gc2: New

---
+++ asteria/test/gc2.cpp
@@ -0,0 +1,68 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2021, LH_Mouse. All wrongs reserved.
+
+#include "utils.hpp"
+#include "../src/simple_script.hpp"
+#include "../src/runtime/garbage_collector.hpp"
+#include "../src/runtime/variable.hpp"
+#include <algorithm>
+
+using namespace asteria;
+
+sso_vector<void*, 1000> alloc_list;
+sso_vector<void*, 1000> free_list;
+
+void* operator new(size_t cb)
+  {
+    auto ptr = ::std::malloc(cb);
+    if(!ptr)
+      throw ::std::bad_alloc();
+
+    alloc_list.push_back(ptr);
+    return ptr;
+  }
+
+void operator delete(void* ptr) noexcept
+  {
+    if(!ptr)
+      return;
+
+    free_list.push_back(ptr);
+    ::std::free(ptr);
+  }
+
+void operator delete(void* ptr, size_t) noexcept
+  {
+    ::operator delete(ptr);
+  }
+
+int main()
+  {
+    // Ignore leaks of emutls, emergency pool, etc.
+    delete new int;
+
+    {
+      Simple_Script code;
+      code.reload_string(
+        sref(__FILE__), __LINE__, sref(R"__(
+///////////////////////////////////////////////////////////////////////////////
+
+          (func(){
+            (func(){
+              func foo() { }
+              var x = foo;
+            }());
+          }());
+
+          assert std.system.gc_collect() == 2;  // foo, x
+
+///////////////////////////////////////////////////////////////////////////////
+        )__"));
+      code.execute();
+    }
+
+    std::sort(alloc_list.mut_begin(), alloc_list.mut_end());
+    std::sort(free_list.mut_begin(), free_list.mut_end());
+    ASTERIA_TEST_CHECK(::std::equal(
+           alloc_list.begin(), alloc_list.end(), free_list.begin(), free_list.end()));
+  }
