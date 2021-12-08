fab8870db913840e309f3d2767a7cb17c076eed9
LIU Hao <lh_mouse@126.com>
2021-12-08 22:48:18 +0800

    test/c_stack_overflow: Use a very small stack

--- asteria/test/c_stack_overflow.cpp
+++ asteria/test/c_stack_overflow.cpp
@@ -3,10 +3,11 @@
 
 #include "utils.hpp"
 #include "../src/simple_script.hpp"
+#include <pthread.h>
 
 using namespace asteria;
 
-int main()
+static void* thread_proc(void*)
   {
     Simple_Script code;
     code.reload_string(
@@ -32,4 +33,17 @@ int main()
 ///////////////////////////////////////////////////////////////////////////////
       )__"));
     code.execute();
+    return nullptr;
+  }
+
+int main()
+  {
+    ::pthread_attr_t attr;
+    ASTERIA_TEST_CHECK(::pthread_attr_init(&attr) == 0);
+    ASTERIA_TEST_CHECK(::pthread_attr_setguardsize(&attr, 0x2000) == 0);
+    ASTERIA_TEST_CHECK(::pthread_attr_setstacksize(&attr, 0xE000) == 0);
+
+    ::pthread_t thrd;
+    ASTERIA_TEST_CHECK(::pthread_create(&thrd, &attr, thread_proc, nullptr) == 0);
+    ASTERIA_TEST_CHECK(::pthread_join(thrd, nullptr) == 0);
   }
