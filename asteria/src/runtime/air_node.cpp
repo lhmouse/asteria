1f715f910decb55817ce5f6a310675f9724f1b7d
LIU Hao <lh_mouse@126.com>
2022-02-06 21:48:36 +0800

    runtime/air_node: Provide software FMA locally

--- asteria/src/runtime/air_node.cpp
+++ asteria/src/runtime/air_node.cpp
@@ -3729,6 +3729,84 @@ struct Traits_apply_xop_assign
       }
   };
 
+// Provide a software version for some buggy CRT libraries.
+template<typename floatT>
+struct float_traits;
+
+template<>
+struct float_traits<float>
+  {
+    static_assert(sizeof(float) == 4);
+
+    static constexpr int exp_bits = 8;
+    static constexpr int mant_bits = 24;
+    using bit_cast_type = uint32_t;
+  };
+
+template<>
+struct float_traits<double>
+  {
+    static_assert(sizeof(double) == 8);
+
+    static constexpr int exp_bits = 11;
+    static constexpr int mant_bits = 53;
+    using bit_cast_type = uint64_t;
+  };
+
+template<typename floatT>
+inline void
+do_split(floatT& hi, floatT& lo, floatT value) noexcept
+  {
+    using traits = float_traits<floatT>;
+    using bit_cast_type = typename traits::bit_cast_type;
+    constexpr int half_bits = ((traits::mant_bits + 1) / 2);
+
+    bit_cast_type bits;
+    ::std::memcpy(&bits, &value, sizeof(bits));
+    bits >>= half_bits;
+    bits <<= half_bits;
+    ::std::memcpy(&hi, &bits, sizeof(bits));
+    lo = value - hi;
+  }
+
+template<typename floatT>
+inline floatT
+do_fma_impl(floatT x, floatT y, floatT z)
+  {
+    // Forward NaN arguments verbatim.
+    floatT temp = x * y + z;
+    if(!::std::isfinite(temp))
+      return temp;
+
+    // -------------------------
+    //              aaaaa bbbbb
+    //              fffff ggggg
+    // -------------------------
+    //              bg_hi bg_lo
+    //        ag_hi ag_lo
+    //        fb_hi fb_lo
+    //  af_hi af_lo
+    // -------------------------
+    floatT aa, bb, ff, gg;
+    floatT bg_hi, bg_lo, ag_hi, ag_lo;
+    floatT fb_hi, fb_lo, af_hi, af_lo;
+
+    do_split(aa, bb, x);
+    do_split(ff, gg, y);
+    do_split(bg_hi, bg_lo, bb * gg);
+    do_split(ag_hi, ag_lo, aa * gg);
+    do_split(fb_hi, fb_lo, ff * bb);
+    do_split(af_hi, af_lo, aa * ff);
+
+    // Accumulate all terms from MSB to LSB.
+    temp = z;
+    temp += af_hi;
+    temp += af_lo + fb_hi + ag_hi;
+    temp += fb_lo + ag_lo + bg_hi;
+    temp += bg_lo;
+    return temp;
+  }
+
 struct Traits_apply_xop_fma
   {
     // `up` is `assign`.
@@ -3766,7 +3844,13 @@ struct Traits_apply_xop_fma
             ROCKET_ASSERT(lhs.is_real());
             ROCKET_ASSERT(mid.is_real());
             ROCKET_ASSERT(rhs.is_real());
-            lhs = ::std::fma(lhs.as_real(), mid.as_real(), rhs.as_real());
+            lhs =
+#ifdef FP_FAST_FMA
+              ::std::fma
+#else
+              do_fma_impl<V_real>
+#endif
+                    (lhs.as_real(), mid.as_real(), rhs.as_real());
             return air_status_next;
           }
 
