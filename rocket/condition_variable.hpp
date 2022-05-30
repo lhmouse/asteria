53cc033dc0b91b728296c0a14de3b40e7b7c068a
LIU Hao <lh_mouse@126.com>
2022-05-30 17:23:00 +0800

    rocket/condition_variable: Cleanup

--- rocket/condition_variable.hpp
+++ rocket/condition_variable.hpp
@@ -37,132 +37,83 @@ class condition_variable
       }
 
   private:
-    static
-    bool
-    do_make_abstime(::timespec& ts, long msecs) noexcept
+    ::timespec
+    do_abs_timeout(int64_t ms) noexcept
       {
-        // Get the current time.
+        ::timespec ts;
+        double value = (double) ms * 0.001 + 0.0009999999;
+
         int r = ::clock_gettime(CLOCK_REALTIME, &ts);
         ROCKET_ASSERT(r == 0);
+        value += (double) ts.tv_sec + (double) ts.tv_nsec * 0.000000001;
 
-        // Ensure we don't cause overflows.
-        constexpr int64_t secs_max = noadl::min(INT64_MAX / 1000,
-                                         ::std::numeric_limits<::time_t>::max());
-        if(msecs > (secs_max - 1 - ts.tv_sec) * 1000)
-          return false;
+        ts.tv_sec = ::std::numeric_limits<::time_t>::max();
+        ts.tv_nsec = 0;
 
-        // If overflow is not possible, add `msecs` to the current time.
-        long secs = static_cast<long>(static_cast<unsigned long>(msecs) / 1'000);
-        ts.tv_sec += static_cast<::time_t>(secs);
+        if(value >= (double) ts.tv_sec)
+          return ts;  // overflowed
 
-        long mrem = msecs - secs * 1'000;
-        if(mrem != 0) {
-          ts.tv_nsec += mrem * 1'000'000;
-
-          long mask = static_cast<long>((999'999'999 - ts.tv_nsec) >> 31);
-          ts.tv_sec -= mask;
-          ts.tv_nsec -= mask & 1'000'000'000;
-        }
-        return true;
-      }
-
-    template<typename mktmT, typename predT>
-    bool
-    do_wait_check_loop(mutex::unique_lock& lock, mktmT&& make_time, predT&& pred)
-      {
-        // Release the lock, as the mutex will be unlocked by `do_cond_wait()`
-        auto owns = lock.m_sth.release();
-        ROCKET_ASSERT(owns);
-
-        // Calculate the time point to give up the wait.
-        ::timespec ts;
-        if(make_time(ts))
-          for(;;) {
-            // Wait until `ts`.
-            int r = ::pthread_cond_timedwait(this->m_cond, owns, &ts);
-            ROCKET_ASSERT(r != EINVAL);
-
-            // Reset the lock, as the mutex shall have been locked again.
-            ROCKET_ASSERT(lock.m_sth.get() == nullptr);
-            lock.m_sth.reset(owns);
-
-            // Return `true` if the condition has been met.
-            if(pred())
-              return true;
-
-            // Return `false` if the wait timed out.
-            if(r == ETIMEDOUT)
-              return false;
-
-            // Restart the wait.
-            owns = lock.m_sth.release();
-            ROCKET_ASSERT(owns);
-          }
-        else
-          for(;;) {
-            // Wait forever.
-            int r = ::pthread_cond_wait(this->m_cond, owns);
-            ROCKET_ASSERT(r != EINVAL);
-
-            // Reset the lock, as the mutex shall have been locked again.
-            ROCKET_ASSERT(lock.m_sth.get() == nullptr);
-            lock.m_sth.reset(owns);
-
-            // Return `true` if the condition has been met.
-            if(pred())
-              return true;
-
-            // Restart the wait.
-            owns = lock.m_sth.release();
-            ROCKET_ASSERT(owns);
-          }
+        ts.tv_sec = (time_t) value;
+        ts.tv_nsec = (long) (value - (double) ts.tv_sec);
+        return ts;
       }
 
   public:
-    void
-    wait_for(mutex::unique_lock& lock, long msecs) noexcept
-      {
-        if(msecs <= 0)
-          return;
-
-        this->do_wait_check_loop(lock,
-            [&](::timespec& ts) { return this->do_make_abstime(ts, msecs);  },
-            [&]() { return true;  });
-      }
-
-    template<typename predT>
     bool
-    wait_for(mutex::unique_lock& lock, long msecs, predT&& pred) noexcept(noexcept(pred()))
-      {
-        if(pred())
-          return true;
-
-        if(msecs <= 0)
-          return false;
-
-        return this->do_wait_check_loop(lock,
-            [&](::timespec& ts) { return this->do_make_abstime(ts, msecs);  },
-            ::std::forward<predT>(pred));
-      }
-
-    void
     wait(mutex::unique_lock& lock) noexcept
       {
-        this->do_wait_check_loop(lock,
-            [&](::timespec& /*ts*/) { return false;  },
-            [&]() { return true;  });
+        auto owns = lock.m_sth.release();
+        ROCKET_ASSERT(owns);
+
+        int r = ::pthread_cond_wait(this->m_cond, owns);
+        ROCKET_ASSERT((r == 0) || (r == EINTR));
+
+        ROCKET_ASSERT(lock.m_sth.get() == nullptr);
+        lock.m_sth.reset(owns);
+        return r == 0;
       }
 
     template<typename predT>
-    bool
+    void
     wait(mutex::unique_lock& lock, predT&& pred) noexcept(noexcept(pred()))
       {
-        if(pred())
-          return true;
+        while(!pred())
+          this->wait(lock);
+      }
 
-        return this->do_wait_check_loop(lock,
-            [&](::timespec& /*ts*/) { return false;  },
-            ::std::forward<predT>(pred));
+    bool
+    wait_until(mutex::unique_lock& lock, const ::timespec& ts) noexcept
+      {
+        auto owns = lock.m_sth.release();
+        ROCKET_ASSERT(owns);
+
+        int r = ::pthread_cond_timedwait(this->m_cond, owns, &ts);
+        ROCKET_ASSERT((r == 0) || (r == EINTR) || (r == ETIMEDOUT));
+
+        ROCKET_ASSERT(lock.m_sth.get() == nullptr);
+        lock.m_sth.reset(owns);
+        return r == 0;
+      }
+
+    template<typename predT>
+    void
+    wait_until(mutex::unique_lock& lock, const ::timespec& ts, predT&& pred) noexcept(noexcept(pred()))
+      {
+        while(!pred())
+          this->wait_until(lock, ts);
+      }
+
+    bool
+    wait_for(mutex::unique_lock& lock, int64_t ms) noexcept
+      {
+        return this->wait_until(lock, this->do_abs_timeout(ms));
+      }
+
+    template<typename predT>
+    void
+    wait_for(mutex::unique_lock& lock, int64_t ms, predT&& pred) noexcept(noexcept(pred()))
+      {
+        return this->wait_until(lock, this->do_abs_timeout(ms), ::std::forward<predT>(pred));
       }
 
     void
