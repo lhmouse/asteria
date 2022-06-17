cb00113b0c0fb5a7212c23a3faf401c23cfb5dc1
LIU Hao <lh_mouse@126.com>
2022-06-17 17:01:36 +0800

    ci: Update 'check_includes.sh'

--- ci/check_includes.sh
+++ ci/check_includes.sh
@@ -2,13 +2,13 @@
 
 # setup
 export CXX=${CXX:-"g++"}
-export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -D_WIN32_WINNT=0x0600"
-export CXXFLAGS='-std=gnu++14 -fno-gnu-keywords -Wno-zero-as-null-pointer-constant'
+export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE"
+export CXXFLAGS='-std=c++17 -fno-gnu-keywords -Wno-zero-as-null-pointer-constant'
 
 # note: `sem` is not always available
 _sem="parallel --will-cite --semaphore --halt soon,fail=1"
 
-for _file in $(find -L "asteria" -name "*.[hc]pp")
+for _file in $(find -L "src" -name "*.[hc]pp")
 do
   _cmd="${CXX} ${CPPFLAGS} ${CXXFLAGS} -x c++ -fsyntax-only -DHAVE_CONFIG_H -I."
   echo "Checking \`#include\` directives:  ${_cmd}  \"${_file}\""
