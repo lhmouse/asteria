96293f7ffd905044bc9f11921214e4a414401eb4
LIU Hao <lh_mouse@126.com>
2022-10-30 22:42:37 +0800

    ci: Update scripts

--- ci/build.sh
+++ ci/build.sh
@@ -4,23 +4,6 @@
 export CXX=${CXX:-"g++"}
 export CXXFLAGS='-O2 -g0'
 
-_fail=1
-
-while test $# -gt 0
-do
-  case $1 in
-    --disable-make-check )
-      _fail=0
-      shift 1
-      ;;
-
-    * )
-      echo "WARNING: unknown option -- '$1'" >&2
-      shift 1
-      ;;
-  esac
-done
-
 # build
 ${CXX} --version
 mkdir -p m4
@@ -28,11 +11,11 @@ autoreconf -ifv
 
 cd $(mktemp -d)
 trap 'rm -rf ~+ || true' EXIT
-~-/configure --disable-silent-rules --enable-debug-checks --disable-static
+~-/configure --disable-silent-rules --enable-debug-checks --{build,host}=${CONFIGURE_HOST} ${CONFIGURE_OPTS}
 
 # test
 if ! make -j$(nproc) distcheck
 then
   cat ./test-suite.log
-  exit ${_fail}
+  exit 1
 fi
