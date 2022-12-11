a9116ee6b4aec294ca3152830576676127a8b4e9
LIU Hao <lh_mouse@126.com>
2022-12-11 22:19:42 +0800

    build: Remove libtool files before building packages

--- makedeb.sh
+++ makedeb.sh
@@ -12,6 +12,7 @@ mkdir -p ${_tempdir}
 cp -pr DEBIAN -t ${_tempdir}
 
 make install DESTDIR=${_tempdir}
+find ${_tempdir} -name "*.la" -delete
 sed -i "s/{_pkgname}/${_pkgname}/" ${_debiandir}/control
 sed -i "s/{_pkgversion}/${_pkgversion}/" ${_debiandir}/control
 sed -i "s/{_pkgarch}/${_pkgarch}/" ${_debiandir}/control
