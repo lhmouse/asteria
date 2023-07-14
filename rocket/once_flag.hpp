cd3bba00c79f025438ca97e9f19ed42ca61cde66
LIU Hao <lh_mouse@126.com>
2023-07-14 17:50:24 +0800

    once_flag: Add `test()`

--- rocket/once_flag.hpp
+++ rocket/once_flag.hpp
@@ -21,31 +21,30 @@ class once_flag
     once_flag& operator=(const once_flag&) = delete;
 
   public:
+    bool
+    test() const noexcept
+      {
+        return __atomic_load_n((const bool*) this->m_guard, __ATOMIC_RELAXED);
+      }
+
     template<typename funcT, typename... paramsT>
     void
     call(funcT&& func, paramsT&&... params)
       {
-        // Load the first byte with acquire semantics.
-        // The value is 0 prior to any initialization and 1 afterwards.
-        if(__atomic_load_n((char*) this->m_guard, __ATOMIC_ACQUIRE))
-          return;
-
-        // Try acquiring the guard.
-        // If 0 is returned, another thread will have finished initialization.
-        int status = ::__cxxabiv1::__cxa_guard_acquire(this->m_guard);
-        if(ROCKET_EXPECT(status == 0))
+        // Try acquiring the guard. If `__atomic_load_n()` returns zero, another
+        // thread will have finished initialization.
+        if(__atomic_load_n((const bool*) this->m_guard, __ATOMIC_ACQUIRE)
+            || (::__cxxabiv1::__cxa_guard_acquire(this->m_guard) == 0))
           return;
 
         try {
-          // Perform the initialization now.
-          ::std::forward<funcT>(func)(::std::forward<paramsT>(params)...);
+          ::std::forward<funcT>(func) (::std::forward<paramsT>(params)...);
+          ::__cxxabiv1::__cxa_guard_release(this->m_guard);
         }
         catch(...) {
-          // ... undo it.
           ::__cxxabiv1::__cxa_guard_abort(this->m_guard);
           throw;
         }
-        ::__cxxabiv1::__cxa_guard_release(this->m_guard);
       }
   };
 
