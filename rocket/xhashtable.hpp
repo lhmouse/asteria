5b0fa158d619aaf8dece1bfa7e4ea5c993fdf199
LIU Hao <lh_mouse@126.com>
2023-08-22 11:34:52 +0800

    rocket/xhashtable: Cleanup

--- rocket/xhashtable.hpp
+++ rocket/xhashtable.hpp
@@ -15,15 +15,15 @@ get_probing_origin(bucketT* begin, bucketT* end, size_t hval) noexcept
     ROCKET_ASSERT(begin < end);
 
     // Make a fixed-point value in the interval [0,1).
-    uint64_t dist = static_cast<uint32_t>(hval * 0x9E3779B9);
+    uint64_t dist = (uint32_t) (hval * 0x9E3779B9U);
 
     // Multiply it by the number of buckets.
-    dist *= static_cast<size_t>((char*)end - (char*)begin);
+    dist *= (size_t) ((char*) end - (char*) begin);
     dist >>= 32;
     dist /= sizeof(*begin);
 
     // Return a pointer to the origin.
-    return begin + static_cast<ptrdiff_t>(dist);
+    return begin + (ptrdiff_t) dist;
   }
 
 template<typename bucketT, typename predT>
@@ -37,12 +37,12 @@ linear_probe(bucketT* begin, bucketT* to, bucketT* from, bucketT* end, predT&& p
 
     // Phase 1: Probe from `from` to `end`.
     for(auto bkt = from;  bkt != end;  ++bkt)
-      if(!*bkt || bool(pred(*bkt)))
+      if(!*bkt || (bool) pred(*bkt))
         return bkt;
 
     // Phase 2: Probe from `begin` to `to`.
     for(auto bkt = begin;  bkt != to;  ++bkt)
-      if(!*bkt || bool(pred(*bkt)))
+      if(!*bkt || (bool) pred(*bkt))
         return bkt;
 
     // The table is full and no desired bucket has been found so far.
