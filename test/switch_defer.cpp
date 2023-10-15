b306876275ec98cdfc6ad287de2fc2fef41cc489
LIU Hao <lh_mouse@126.com>
2023-10-16 02:22:19 +0800

    test/switch_defer: New test

---
+++ test/switch_defer.cpp
@@ -0,0 +1,112 @@
+// This file is part of Asteria.
+// Copyleft 2018 - 2023, LH_Mouse. All wrongs reserved.
+
+#include "utils.hpp"
+#include "../asteria/simple_script.hpp"
+using namespace ::asteria;
+
+int main()
+  {
+    Simple_Script code;
+    code.reload_string(
+      sref(__FILE__), __LINE__, sref(R"__(
+///////////////////////////////////////////////////////////////////////////////
+
+        var str = '+';
+        switch(1) {
+          case 2:
+            defer str += '2a';
+            defer str += '2b';
+          case 1:
+            defer str += '1a';
+            defer str += '1b';
+          case 0:
+            defer str += '0a';
+            defer str += '0b';
+        }
+        assert str == '+0b0a1b1a';
+
+        str = '+';
+        switch(1) {
+          case 2:
+            defer str += '2a';
+            defer str += '2b';
+          case 1:
+            defer str += '1a';
+            break;
+            defer str += '1b';
+          case 0:
+            defer str += '0a';
+            defer str += '0b';
+        }
+        assert str =='+1a';
+
+        str = '+';
+        switch(1) {
+          case 2:
+            defer str += '2a';
+            defer str += '2b';
+          case 1:
+            defer str += '1a';
+            defer str += '1b';
+          case 0:
+            defer str += '0a';
+            break;
+            defer str += '0b';
+        }
+        assert str == '+0a1b1a';
+
+        str = '+';
+        try
+          switch(1) {
+            case 2:
+              defer str += '2a';
+              defer str += '2b';
+            case 1:
+              defer str += '1a';
+              throw 42;
+              defer str += '1b';
+            case 0:
+              defer str += '0a';
+              defer str += '0b';
+          }
+        catch(e)
+          ;
+        assert str =='+1a';
+
+        str = '+';
+        try
+          switch(1) {
+            case 2:
+              defer str += '2a';
+              defer str += '2b';
+            case 1:
+              defer str += '1a';
+              defer str += '1b';
+            case 0:
+              defer str += '0a';
+              throw 42;
+              defer str += '0b';
+          }
+        catch(e)
+          ;
+        assert str == '+0a1b1a';
+
+        str = '+';
+        switch(9999) {
+          case 2:
+            defer str += '2a';
+            defer str += '2b';
+          case 1:
+            defer str += '1a';
+            defer str += '1b';
+          case 0:
+            defer str += '0a';
+            defer str += '0b';
+        }
+        assert str == '+';
+
+///////////////////////////////////////////////////////////////////////////////
+      )__"));
+    code.execute();
+  }
