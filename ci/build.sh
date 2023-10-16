648867489b727cf9cc321de28fa5575cf16c4f46
LIU Hao <lh_mouse@126.com>
2023-10-17 00:19:54 +0800

    ci: Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -2,21 +2,32 @@
 
 # setup
 export CXX=${CXX:-"g++"}
-export CXXFLAGS='-O0 -g0'
+export CFLAGS='-O0 -g0'
+export DISTCHECK_CONFIGURE_FLAGS="${CONFIGURE_OPTS}"
 
 # build
+if test "${CONFIGURE_BUILD}" != ""
+then
+  DISTCHECK_CONFIGURE_FLAGS="--build=${CONFIGURE_BUILD} ${DISTCHECK_CONFIGURE_FLAGS}"
+fi
+
+if test "${CONFIGURE_HOST}" != ""
+then
+  DISTCHECK_CONFIGURE_FLAGS="--host=${CONFIGURE_HOST} ${DISTCHECK_CONFIGURE_FLAGS}"
+  CXX="${CONFIGURE_HOST}-g++"
+fi
+
 ${CXX} --version
 mkdir -p m4
 autoreconf -ifv
 
 cd $(mktemp -d)
 trap 'rm -rf ~+ || true' EXIT
-~-/configure --build=${CONFIGURE_BUILD} --host=${CONFIGURE_HOST}  \
-    ${CONFIGURE_OPTS} --disable-dependency-tracking --disable-silent-rules  \
-    --enable-debug-checks
+~-/configure ${DISTCHECK_CONFIGURE_FLAGS}  \
+    --disable-dependency-tracking --disable-silent-rules --enable-debug-checks
 
 # test
-if ! make -j$(nproc) distcheck DISTCHECK_CONFIGURE_FLAGS=${CONFIGURE_OPTS}
+if ! make -j$(nproc) distcheck
 then
   find . -name 'test-suite.log' -exec cat '{}' ';'
   exit 1
