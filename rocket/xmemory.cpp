5a7c15cc3ad5610e6cdc3d1e5f8ba0e558565326
LIU Hao <lh_mouse@126.com>
2023-11-16 14:33:19 +0800

    Revert "rocket/xmemory: Use lockless lists"
    This reverts commit 49d081defcd184590b823c676051ac4ac450eb49.

--- rocket/xmemory.cpp
+++ rocket/xmemory.cpp
@@ -2,44 +2,20 @@
 // Copyleft 2018 - 2023, LH_Mouse. All wrongs reserved.
 
 #include "xmemory.hpp"
+#include "mutex.hpp"
 namespace rocket {
 namespace {
 
 struct free_block
   {
+    size_t size;
     free_block* next;
-    void* padding;
   };
 
 struct alignas(64) pool
   {
-    free_block* volatile ptr_stor[2];  // head, tail
-
-    bool
-    compare_exchange(free_block*& head_cmp, free_block*& tail_cmp, free_block* head_set,
-                     free_block* tail_set) noexcept
-      {
-        bool equal;
-#if defined(__x86_64__) && (__SIZEOF_POINTER__ != 4)
-        // This is for x86-64 and not x32
-        // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80878
-        __asm__ (
-          "lock cmpxchg16b %1"
-          : "=@cce"(equal), "+m"(this->ptr_stor), "+a"(head_cmp), "+d"(tail_cmp)
-          : "b"(head_set), "c"(tail_set)
-          : "memory"
-        );
-#else
-        // This is for x86, x32, ARM, etc.
-        free_block* stor_cmp[2] = { head_cmp, tail_cmp };
-        free_block* stor_set[2] = { head_set, tail_set };
-        equal = __atomic_compare_exchange(&(this->ptr_stor), &stor_cmp, &stor_set,
-                                  1 /* weak */, __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE);
-        head_cmp = stor_cmp[0];
-        tail_cmp = stor_cmp[1];
-#endif
-        return equal;
-      }
+    mutex m;
+    free_block* head;
   };
 
 pool s_pools[64];
@@ -67,17 +43,15 @@ xmemalloc(xmeminfo& info, xmemopt opt)
 
     free_block* b = nullptr;
     auto& p = do_get_pool_for_size(rsize);
+    mutex::unique_lock lock;
 
     if(opt == xmemopt_use_cache) {
-      // Detach all blocks from the cache, then leave the first and put all
-      // the others back.
-      free_block* tail = nullptr;
-      while(!p.compare_exchange(b, tail, nullptr, nullptr));
-
-      free_block* t = exchange(tail, nullptr);
-      if(ROCKET_EXPECT(b != t))
-        while(!p.compare_exchange(t->next, tail, b->next, tail ? tail : t));
+      // Get a block from the cache.
+      lock.lock(p.m);
+      if(p.head != nullptr)
+        b = exchange(p.head, p.head->next);
     }
+    lock.unlock();
 
     // If the cache was empty, allocate a block from the system.
     if(b == nullptr)
@@ -99,23 +73,26 @@ xmemfree(xmeminfo& info, xmemopt opt) noexcept
 
     size_t rsize = info.element_size * info.count;
     auto& p = do_get_pool_for_size(rsize);
+    mutex::unique_lock lock;
 
 #ifdef ROCKET_DEBUG
     ::memset(b, 0xCB, rsize);
 #endif
+    b->size = rsize;
     b->next = nullptr;
 
     if(opt == xmemopt_use_cache) {
       // Put the block into the cache.
-      free_block* tail = nullptr;
-      while(!p.compare_exchange(b->next, tail, b, tail ? tail : b));
+      lock.lock(p.m);
+      b->next = exchange(p.head, b);
       b = nullptr;
     }
     else if(opt == xmemopt_clear_cache) {
       // Append all blocks from the cache to `b`.
-      free_block* tail = nullptr;
-      while(!p.compare_exchange(b->next, tail, nullptr, nullptr));
+      lock.lock(p.m);
+      b->next = exchange(p.head, nullptr);
     }
+    lock.unlock();
 
     // Return all blocks to the system.
     while(b != nullptr)
@@ -126,11 +103,10 @@ void
 xmemflush() noexcept
   {
     for(auto& p : s_pools) {
-      free_block* b = nullptr;
-
-      // Append all blocks from the cache to `b`.
-      free_block* tail = nullptr;
-      while(!p.compare_exchange(b, tail, nullptr, nullptr));
+      // Extract all blocks.
+      mutex::unique_lock lock(p.m);
+      free_block* b = exchange(p.head, nullptr);
+      lock.unlock();
 
       // Return all blocks to the system.
       while(b != nullptr)
