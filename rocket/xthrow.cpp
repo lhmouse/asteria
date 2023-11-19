99fc095eb5459d10824ca9b5c1afb6b7d40cfd6f
LIU Hao <lh_mouse@126.com>
2023-11-19 17:41:24 +0800

    rocket/xthrow: Cleanup

--- rocket/xthrow.cpp
+++ rocket/xthrow.cpp
@@ -14,10 +14,11 @@ sprintf_and_throw(const char* fmt, ...)
     ::va_list ap;
     va_start(ap, fmt);
     char strbuf[4096];
-    long t = ::vsnprintf(strbuf, sizeof(strbuf), fmt, ap) - 1;
+    int ret = ::vsnprintf(strbuf, sizeof(strbuf), fmt, ap);
     va_end(ap);
 
     // Remove trailing line breaks.
+    long t = noadl::clamp_cast<long>(ret, 0, (long) sizeof(strbuf)) - 1;
     while((t >= 0) && (strbuf[t] == '\n'))
       strbuf[--t] = 0;
 
