3e428c77eb697231ea3f1fdc1c43fec4f8a65a62
LIU Hao <lh_mouse@126.com>
2023-11-25 19:45:17 +0800

    library/system: Fix name of a local variable

--- asteria/library/system.cpp
+++ asteria/library/system.cpp
@@ -299,12 +299,13 @@ std_system_uuid(Global_Context& global)
     //  * N: any of `0`-`7` (UUID variant)
     //  * z: context ID
     //  * w: random bytes
-    static atomic<uint64_t> m_serial;
+    static atomic<uint64_t> serial;
     const auto prng = global.random_engine();
+
     ::timespec ts;
     ::clock_gettime(CLOCK_REALTIME, &ts);
 
-    uint64_t x = (uint64_t) ts.tv_sec * 30518U + (uint32_t) ts.tv_nsec / 32768U + m_serial.xadd(1U);
+    uint64_t x = (uint64_t) ts.tv_sec * 30518U + (uint32_t) ts.tv_nsec / 32768U + serial.xadd(1U);
     uint64_t y = (uint32_t) ::getpid();
     uint64_t z = (uint64_t)(void*) &global >> 12;
     uint64_t w = (uint64_t) prng->bump() << 32 | prng->bump();
