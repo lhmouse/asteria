3082f6542b395ead1636063b74b2f6605f8ac069
LIU Hao <lh_mouse@126.com>
2023-11-27 18:57:16 +0800

    TEST biased reference count

--- rocket/reference_counter.hpp
+++ rocket/reference_counter.hpp
@@ -20,22 +20,33 @@ class reference_counter
     using value_type  = valueT;
 
   private:
+ROCKET_PURE static
+void*
+xtid() noexcept
+  {
+    void* tid;
+    __asm__ ("mov %0, qword ptr fs:[0]" : "=r"(tid));
+    return tid;
+  }
+
     ::std::atomic<value_type> m_nref;
+    value_type m_local_nref;
+    void* m_tid;
 
   public:
-    constexpr
+    //constexpr
     reference_counter() noexcept
       :
-        m_nref(1)
+        m_nref(1), m_local_nref(1), m_tid(xtid())
       { }
 
-    explicit constexpr
+    explicit // constexpr
     reference_counter(value_type nref) noexcept
       :
-        m_nref(nref)
+        m_nref(1), m_local_nref(nref), m_tid(xtid())
       { }
 
-    constexpr
+    //constexpr
     reference_counter(const reference_counter&) noexcept
       :
         reference_counter()
@@ -55,16 +66,30 @@ class reference_counter
   public:
     bool
     unique() const noexcept
-      { return this->m_nref.load(memory_order_relaxed) == 1;  }
+      {
+        return this->m_nref.load(memory_order_relaxed) == 1
+&& this->m_local_nref == 1;
+      }
 
     value_type
     get() const noexcept
-      { return this->m_nref.load(memory_order_relaxed);  }
+      {
+if(this->m_local_nref != 0)
+  return this->m_local_nref + this->m_nref.load(memory_order_relaxed) - 1;
+
+        return this->m_nref.load(memory_order_relaxed);
+      }
 
     // Increment the counter only if it is non-zero, and return its new value.
     value_type
     try_increment() noexcept
       {
+if(this->m_local_nref != 0) {
+  this->m_local_nref ++;
+  ROCKET_ASSERT(this->m_local_nref >= 2);
+  return this->m_local_nref;
+}
+
         auto old = this->m_nref.load(memory_order_relaxed);
         for(;;)
           if(old == 0)
@@ -78,6 +103,12 @@ class reference_counter
     value_type
     increment() noexcept
       {
+if(this->m_local_nref != 0) {
+  this->m_local_nref ++;
+  ROCKET_ASSERT(this->m_local_nref >= 2);
+  return this->m_local_nref;
+}
+
         auto old = this->m_nref.fetch_add(1, memory_order_relaxed);
         ROCKET_ASSERT(old >= 1);
         return old + 1;
@@ -90,6 +121,12 @@ class reference_counter
     value_type
     decrement() noexcept
       {
+if(this->m_local_nref != 0) {
+  this->m_local_nref --;
+  if(this->m_local_nref != 0)
+    return this->m_local_nref;
+}
+
         auto old = this->m_nref.fetch_sub(1, memory_order_acq_rel);
         ROCKET_ASSERT(old >= 1);
         return old - 1;
