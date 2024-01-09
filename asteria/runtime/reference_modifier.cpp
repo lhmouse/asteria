c40bd3929f275cbe553752b418e20f2bc93f3349
LIU Hao <lh_mouse@126.com>
2024-01-09 21:14:50 +0800

    reference_modifier: Cleanup

--- asteria/runtime/reference_modifier.cpp
+++ asteria/runtime/reference_modifier.cpp
@@ -16,10 +16,8 @@ apply_read_opt(const Value& parent) const
       case index_array_index: {
         // Get the element at the given index.
         const auto& altr = this->m_stor.as<S_array_index>();
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Integer subscript not applicable (parent type `$1`)",
@@ -27,40 +25,39 @@ apply_read_opt(const Value& parent) const
 
         const auto& arr = parent.as_array();
         auto w = wrap_array_index(arr.ssize(), altr.index);
+
         if(w.nprepend | w.nappend)
           return nullptr;
 
-        return &(arr[w.rindex]);
+        return arr.data() + w.rindex;
       }
 
       case index_object_key: {
         // Get the value with the given key.
         const auto& altr = this->m_stor.as<S_object_key>();
-        if(parent.is_null()) {
-          // Members of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_object())
           throw Runtime_Error(xtc_format,
                    "String subscript not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         const auto& obj = parent.as_object();
+
         return obj.ptr(altr.key);
       }
 
       case index_array_head: {
         // Get the first element.
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Head operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         const auto& arr = parent.as_array();
+
         if(arr.empty())
           return nullptr;
 
@@ -69,16 +66,15 @@ apply_read_opt(const Value& parent) const
 
       case index_array_tail: {
         // Get the last element.
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Tail operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         const auto& arr = parent.as_array();
+
         if(arr.empty())
           return nullptr;
 
@@ -88,21 +84,20 @@ apply_read_opt(const Value& parent) const
       case index_array_random: {
         // Get a random element.
         const auto& altr = this->m_stor.as<S_array_random>();
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Random operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         const auto& arr = parent.as_array();
+
         if(arr.empty())
           return nullptr;
 
-        size_t r = ::rocket::probe_origin(arr.size(), altr.seed);
-        return arr.data() + r;
+        size_t off = ::rocket::probe_origin(arr.size(), altr.seed);
+        return arr.data() + off;
       }
 
       default:
@@ -118,10 +113,8 @@ apply_write_opt(Value& parent) const
       case index_array_index: {
         // Get the element at the given index.
         const auto& altr = this->m_stor.as<S_array_index>();
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Integer subscript not applicable (parent type `$1`)",
@@ -129,40 +122,39 @@ apply_write_opt(Value& parent) const
 
         auto& arr = parent.mut_array();
         auto w = wrap_array_index(arr.ssize(), altr.index);
+
         if(w.nprepend | w.nappend)
           return nullptr;
 
-        return &(arr.mut(w.rindex));
+        return arr.mut_data() + w.rindex;
       }
 
       case index_object_key: {
         // Get the value with the given key.
         const auto& altr = this->m_stor.as<S_object_key>();
-        if(parent.is_null()) {
-          // Members of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_object())
           throw Runtime_Error(xtc_format,
                    "String subscript not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& obj = parent.mut_object();
+
         return obj.mut_ptr(altr.key);
       }
 
       case index_array_head: {
         // Get the first element.
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Head operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           return nullptr;
 
@@ -171,16 +163,15 @@ apply_write_opt(Value& parent) const
 
       case index_array_tail: {
         // Get the last element.
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Tail operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           return nullptr;
 
@@ -190,21 +181,20 @@ apply_write_opt(Value& parent) const
       case index_array_random: {
         // Get a random element.
         const auto& altr = this->m_stor.as<S_array_random>();
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullptr;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Random operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           return nullptr;
 
-        size_t r = ::rocket::probe_origin(arr.size(), altr.seed);
-        return arr.mut_data() + r;
+        size_t off = ::rocket::probe_origin(arr.size(), altr.seed);
+        return arr.mut_data() + off;
       }
 
       default:
@@ -220,10 +210,8 @@ apply_open(Value& parent) const
       case index_array_index: {
         // Get the element at the given index.
         const auto& altr = this->m_stor.as<S_array_index>();
-        if(parent.is_null()) {
-          // Empty arrays are created if null values are encountered.
+        if(parent.is_null())
           parent = V_array();
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Integer subscript not applicable (parent type `$1`)",
@@ -231,42 +219,42 @@ apply_open(Value& parent) const
 
         auto& arr = parent.mut_array();
         auto w = wrap_array_index(arr.ssize(), altr.index);
+
         if(w.nprepend)
           arr.insert(arr.begin(), ::rocket::clamp_cast<size_t>(w.nprepend, 0U, SIZE_MAX));
         else if(w.nappend)
           arr.append(::rocket::clamp_cast<size_t>(w.nappend, 0U, SIZE_MAX));
 
-        return arr.mut(w.rindex);
+        return *(arr.mut_data() + w.rindex);
       }
 
       case index_object_key: {
         // Get the value with the given key.
         const auto& altr = this->m_stor.as<S_object_key>();
-        if(parent.is_null()) {
-          // Empty objects are created if null values are encountered.
+        if(parent.is_null())
           parent = V_object();
-        }
         else if(!parent.is_object())
           throw Runtime_Error(xtc_format,
                    "String subscript not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& obj = parent.mut_object();
-        return obj.try_emplace(altr.key).first->second;
+
+        auto r = obj.try_emplace(altr.key);
+        return r.first->second;
       }
 
       case index_array_head: {
         // Get the first element.
-        if(parent.is_null()) {
-          // Empty arrays are created if null values are encountered.
+        if(parent.is_null())
           parent = V_array();
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Head operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           arr.insert(arr.begin(), nullopt);
         else
@@ -277,16 +265,15 @@ apply_open(Value& parent) const
 
       case index_array_tail: {
         // Get the last element.
-        if(parent.is_null()) {
-          // Empty arrays are created if null values are encountered.
+        if(parent.is_null())
           parent = V_array();
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Tail operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           arr.emplace_back();
         else
@@ -298,22 +285,21 @@ apply_open(Value& parent) const
       case index_array_random: {
         // Get a random element.
         const auto& altr = this->m_stor.as<S_array_random>();
-        if(parent.is_null()) {
-          // Empty arrays are created if null values are encountered.
+        if(parent.is_null())
           parent = V_array();
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Random operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           throw Runtime_Error(xtc_format,
                    "Cannot write to random element of an empty array");
 
-        size_t r = ::rocket::probe_origin(arr.size(), altr.seed);
-        return arr.mut(r);
+        size_t off = ::rocket::probe_origin(arr.size(), altr.seed);
+        return *(arr.mut_data() + off);
       }
 
       default:
@@ -329,10 +315,8 @@ apply_unset(Value& parent) const
       case index_array_index: {
         // Get the element at the given index.
         const auto& altr = this->m_stor.as<S_array_index>();
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullopt;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Integer subscript not applicable (parent type `$1`)",
@@ -340,27 +324,28 @@ apply_unset(Value& parent) const
 
         auto& arr = parent.mut_array();
         auto w = wrap_array_index(arr.ssize(), altr.index);
+
         if(w.nprepend | w.nappend)
           return nullopt;
 
-        auto val = arr.mut(w.rindex);
-        arr.erase(w.rindex, 1);
+        auto it = arr.mut_begin() + static_cast<ptrdiff_t>(w.rindex);
+        auto val = move(*it);
+        arr.erase(it);
         return val;
       }
 
       case index_object_key: {
         // Get the value with the given key.
         const auto& altr = this->m_stor.as<S_object_key>();
-        if(parent.is_null()) {
-          // Members of null values are also null values.
+        if(parent.is_null())
           return nullopt;
-        }
         else if(!parent.is_object())
           throw Runtime_Error(xtc_format,
                    "String subscript not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& obj = parent.mut_object();
+
         auto it = obj.mut_find(altr.key);
         if(it == obj.end())
           return nullopt;
@@ -372,63 +357,63 @@ apply_unset(Value& parent) const
 
       case index_array_head: {
         // Get the first element.
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullopt;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Head operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           return nullopt;
 
-        auto val = move(arr.mut_front());
-        arr.erase(arr.begin());
+        auto it = arr.mut_begin();
+        auto val = move(*it);
+        arr.erase(it);
         return val;
       }
 
       case index_array_tail: {
         // Get the last element.
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullopt;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Tail operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           return nullopt;
 
-        auto val = move(arr.mut_back());
-        arr.pop_back();
+        auto it = arr.mut_end() - 1;
+        auto val = move(*it);
+        arr.erase(it);
         return val;
       }
 
       case index_array_random: {
         // Get a random element.
         const auto& altr = this->m_stor.as<S_array_random>();
-        if(parent.is_null()) {
-          // Elements of null values are also null values.
+        if(parent.is_null())
           return nullopt;
-        }
         else if(!parent.is_array())
           throw Runtime_Error(xtc_format,
                    "Random operator not applicable (parent type `$1`)",
                    describe_type(parent.type()));
 
         auto& arr = parent.mut_array();
+
         if(arr.empty())
           return nullopt;
 
-        size_t r = ::rocket::probe_origin(arr.size(), altr.seed);
-        auto val = move(arr.mut(r));
-        arr.erase(r, 1);
+        size_t off = ::rocket::probe_origin(arr.size(), altr.seed);
+        auto it = arr.mut_begin() + static_cast<ptrdiff_t>(off);
+        auto val = move(*it);
+        arr.erase(it);
         return val;
       }
 
