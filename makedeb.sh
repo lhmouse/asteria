455d205d69791c1f6165c59e81f60d24cd8ae41f
LIU Hao <lh_mouse@126.com>
2024-01-28 14:31:30 +0800

    build: Strip binaries before installing

--- makedeb.sh
+++ makedeb.sh
@@ -3,22 +3,18 @@
 _pkgname=asteria
 _pkgversion=$(printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 _pkgarch=$(dpkg --print-architecture)
-
 _tempdir=$(readlink -f "./.makedeb")
-_debiandir=${_tempdir}/DEBIAN
 
 rm -rf ${_tempdir}
 mkdir -p ${_tempdir}
 cp -pr DEBIAN -t ${_tempdir}
 
 meson setup -Dbuildtype=release build_makedeb
-pushd build_makedeb
-ninja
-DESTDIR=${_tempdir} meson install
-popd
+meson compile -Cbuild_makedeb
+DESTDIR=${_tempdir} meson install --strip -Cbuild_makedeb
 
-sed -i "s/{_pkgname}/${_pkgname}/" ${_debiandir}/control
-sed -i "s/{_pkgversion}/${_pkgversion}/" ${_debiandir}/control
-sed -i "s/{_pkgarch}/${_pkgarch}/" ${_debiandir}/control
+sed -i "s/{_pkgname}/${_pkgname}/" ${_tempdir}/DEBIAN/control
+sed -i "s/{_pkgversion}/${_pkgversion}/" ${_tempdir}/DEBIAN/control
+sed -i "s/{_pkgarch}/${_pkgarch}/" ${_tempdir}/DEBIAN/control
 
 dpkg-deb --root-owner-group --build .makedeb "${_pkgname}_${_pkgversion}_${_pkgarch}.deb"
