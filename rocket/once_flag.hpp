3c9b2f4ed9ca7b20ad615aa29d2b431dac99ec6b
LIU Hao <lh_mouse@126.com>
2025-03-22 18:08:14 +0800

    rocket/once_flag: Use magic guard type for Darwin

--- rocket/once_flag.hpp
+++ rocket/once_flag.hpp
@@ -12,7 +12,33 @@ namespace rocket {
 class once_flag
   {
   private:
-    ::abi::__guard m_guard[1] = { };
+    union Guard
+      {
+        unsigned long long ull;
+        unsigned long ul;
+        unsigned int ui;
+        long long ll;
+        long l;
+        int i;
+        bool b;
+
+        operator unsigned long long*() noexcept { return &(this->ull);  }
+        operator unsigned long*() noexcept { return &(this->ul);  }
+        operator unsigned int*() noexcept { return &(this->ui);  }
+        operator long long*() noexcept { return &(this->ll);  }
+        operator long*() noexcept { return &(this->l);  }
+        operator int*() noexcept { return &(this->i);  }
+      };
+
+    struct Sentry
+      {
+        decltype(::abi::__cxa_guard_abort)* dtor;
+        Guard* guard;
+
+        ~Sentry() { (* this->dtor) (* this->guard);  }
+      };
+
+    Guard m_guard = { 0 };
 
   public:
     constexpr once_flag() noexcept = default;
@@ -24,27 +50,20 @@ class once_flag
     bool
     test() const noexcept
       {
-        return __atomic_load_n((const bool*) this->m_guard, __ATOMIC_RELAXED);
+        return __atomic_load_n(&(this->m_guard.b), __ATOMIC_RELAXED);
       }
 
     template<typename funcT, typename... paramsT>
     void
     call(funcT&& func, paramsT&&... params)
       {
-        // Try acquiring the guard. If `__atomic_load_n()` returns zero, another
-        // thread will have finished initialization.
-        if(__atomic_load_n((const bool*) this->m_guard, __ATOMIC_ACQUIRE)
+        // Try acquiring the guard. If `__atomic_load_n()` returns zero,
+        // another thread will have finished initialization.
+        if(__atomic_load_n(&(this->m_guard.b), __ATOMIC_ACQUIRE)
             || (::abi::__cxa_guard_acquire(this->m_guard) == 0))
           return;
 
-        struct Sentry
-          {
-            decltype(::abi::__cxa_guard_abort)* dtor;
-            ::abi::__guard* guard;
-            ~Sentry() { (* this->dtor) (this->guard);  }
-          };
-
-        Sentry sentry = { ::abi::__cxa_guard_abort, this->m_guard };
+        Sentry sentry = { ::abi::__cxa_guard_abort, &(this->m_guard) };
         forward<funcT>(func) (forward<paramsT>(params)...);
         sentry.dtor = ::abi::__cxa_guard_release;
       }
