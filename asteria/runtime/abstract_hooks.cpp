0a4bfcdb1a12cd4be5a6e5816a46cb512ab9302c
LIU Hao <lh_mouse@126.com>
2025-04-02 14:27:40 +0800

    runtime/abstract_hooks: Use `#pragma` to silence warnings

--- asteria/runtime/abstract_hooks.cpp
+++ asteria/runtime/abstract_hooks.cpp
@@ -4,61 +4,49 @@
 #include "../xprecompiled.hpp"
 #include "abstract_hooks.hpp"
 #include "../utils.hpp"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 namespace asteria {
 
 void
 Abstract_Hooks::
 on_trap(const Source_Location& sloc, Executive_Context& ctx)
   {
-    (void) sloc;
-    (void) ctx;
   }
 
 void
 Abstract_Hooks::
 on_call(const Source_Location& sloc, const cow_function& target)
   {
-    (void) sloc;
-    (void) target;
   }
 
 void
 Abstract_Hooks::
 on_declare(const Source_Location& sloc, const phsh_string& name)
   {
-    (void) sloc;
-    (void) name;
   }
 
 void
 Abstract_Hooks::
 on_return(const Source_Location& sloc, PTC_Aware ptc)
   {
-    (void) sloc;
-    (void) ptc;
   }
 
 void
 Abstract_Hooks::
 on_throw(const Source_Location& sloc, Value& except)
   {
-    (void) sloc;
-    (void) except;
   }
 
 void
 Abstract_Hooks::
 on_function_enter(Executive_Context& fctx, const Instantiated_Function& func)
   {
-    (void) fctx;
-    (void) func;
   }
 
 void
 Abstract_Hooks::
 on_function_leave(Executive_Context& fctx) noexcept
   {
-    (void) fctx;
   }
 
 }  // namespace asteria
