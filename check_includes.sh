71b6c6840cdc41812db33cb15bacceb806691e94
LIU Hao <lh_mouse@126.com>
2025-04-14 15:24:07 +0800

    doc: Don't use `sem` in 'check_includes.sh'

--- check_includes.sh
+++ check_includes.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
 
 # setup
-export CXX=${CXX:-"g++"}
-export CXXFLAGS="-D_FILE_OFFSET_BITS=64 -D_POSIX_C_SOURCE=200809
-                 -D_GNU_SOURCE -Ibuild_debug -std=c++17 -mavx"
+export CXXFLAGS="-D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -Ibuild_debug -std=c++17 -mavx"
 
-# note: `sem` is not always available
-_sem="parallel --will-cite --semaphore --halt soon,fail=1"
+if test -z ${CXX}
+then
+  CXX=g++
+fi
 
-for _f in $(find "rocket" "asteria" -name "*.[hc]pp")
+for _f in $(find ~+/"asteria" -name "*.[hc]pp")
 do
   echo "Checking includes:  ${_f}"
-  ${_sem} -j+0 -- ${CXX} ${CXXFLAGS} -x c++ ${_f} -S -o /dev/null
+  ${CXX} ${CXXFLAGS} -x c++ ${_f} -S -o /dev/null
 done
-${_sem} --wait
