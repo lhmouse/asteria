627ac79b9b47ec40d077849c19a35b7b20eee65d
LIU Hao <lh_mouse@126.com>
2025-04-14 15:58:40 +0800

    doc: Update 'check_includes.sh'

--- check_includes.sh
+++ check_includes.sh
@@ -3,6 +3,6 @@
 export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -Ibuild_debug"
 export CXX=${CXX:-"g++"}
 
-find ~+/"asteria" -name "*.[hc]pp"  \
+find ~+/"asteria" ~+/"rocket" -name "*.[hc]pp"  \
   | xargs -n 1 -P $(nproc) --  \
     sh -xec '${CXX} ${CPPFLAGS} -x c++ -std=c++17 $1 -S -o /dev/null' 0
