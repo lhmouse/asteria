5ca583d707be8d9619dd9c718ec017fa082e2c3d
LIU Hao <lh_mouse@126.com>
2025-04-19 20:05:26 +0800

    doc: Use `${PWD}` instead of `~+`

--- check_includes.sh
+++ check_includes.sh
@@ -3,6 +3,6 @@
 export CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -Ibuild_debug"
 export CXX=${CXX:-"g++"}
 
-find ~+/"asteria" ~+/"rocket" -name "*.[hc]pp"  \
+find ${PWD}/"asteria" ${PWD}/"rocket" -name "*.[hc]pp"  \
   | xargs -n 1 -P $(nproc) --  \
     sh -xec '${CXX} ${CPPFLAGS} -x c++ -std=c++17 $1 -S -o /dev/null' 0
