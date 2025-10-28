85f3d308f80a4482457daa10e405ebf9f246bc6f
LIU Hao <lh_mouse@126.com>
2025-10-28 16:49:10 +0800

    library/json: Reformat

--- asteria/library/json.cpp
+++ asteria/library/json.cpp
@@ -342,8 +342,9 @@ do_token(cow_string& token, Parser_Context& ctx, const Unified_Source& usrc)
         if(!is_within(ctx.c, '0', '9'))
           return do_err(ctx, "Invalid number");
 
-        do {  // fallthrough
+        // fallthrough
       case '0' ... '9':
+        do {
           token.push_back(static_cast<char>(ctx.c));
           do_load_next(ctx, usrc);
         }
